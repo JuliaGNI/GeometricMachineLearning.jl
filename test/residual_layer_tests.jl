ac36492488863ed55eee11bfc0757b071c044eb6
Michael Kraus <michael.kraus@ipp.mpg.de>
2022-07-08 13:13:08 +0200

    Add basic tests for layers.

---
+++ test/residual_layer_tests.jl
@@ -0,0 +1,10 @@
+using GeometricMachineLearning
+using Test
+
+
+l = ResidualLayer(x -> x, ones(2,2), ones(2), x -> 1)
+i = ones(2)
+o1 = zero(i)
+o2 = zero(i)
+
+@test l(o1, i) == apply!(o2, i, l) == 4*i
