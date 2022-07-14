816764c3ab28a86cc195ef9bdde508395d3c631c
benedict-96 <benedikt.brantner@icloud.com>
2022-07-14 11:24:25 +0200

    Fixed a Typo

--- src/layers/linear_symp_layer.jl
+++ src/layers/linear_symp_layer.jl
@@ -1,17 +1,17 @@
 #residual layer that changes p
-struct ResidualLayer_p{DT, N, M, WT <: AbstractMatrix{DT}, GT} <: NeuralNetworkLayer{DT,N,M}
+struct SympLinLayer_p{DT, N, M, WT <: AbstractMatrix{DT}, GT} <: NeuralNetworkLayer{DT,N,M}
     W::WT
     S::WT
     gradient::GT
 
-    function ResidualLayer(W::AbstractMatrix{DT}, gradient) where {DT}
+    function SympLinLayer_p(W::AbstractMatrix{DT}, gradient) where {DT}
         @assert length(axes(W,1)) == length(axes(W,2))
         S = W .+ W'
         new{DT, length(axes(W,2)), length(axes(W,1)), typeof(W), typeof(gradient)}(W,S,gradient)
     end
 end
 
-function apply!(output::AbstractVector, input::AbstractVector, layer::ResidualLayer)
+function apply!(output::AbstractVector, input::AbstractVector, layer::SympLinLayer_p)
     q = @view input[1:N÷2]
     p = @view input[N÷2+1:N]
     p_out = @view output[N÷2+1:N] 
@@ -21,19 +21,19 @@ end
 
 
 #residual layer that changes q
-struct ResidualLayer_q{DT, N, M, WT <: AbstractMatrix{DT}, GT} <: NeuralNetworkLayer{DT,N,M}
+struct SympLinLayer_q{DT, N, M, WT <: AbstractMatrix{DT}, GT} <: NeuralNetworkLayer{DT,N,M}
     W::WT
     S::WT
     gradient::GT
 
-    function ResidualLayer(W::AbstractMatrix{DT}, gradient) where {DT}
+    function SympLinLayer_q(W::AbstractMatrix{DT}, gradient) where {DT}
         @assert length(axes(W,1)) == length(axes(W,2))
         S = W .+ W'
         new{DT, length(axes(W,2)), length(axes(W,1)), typeof(W), typeof(gradient)}(W,S,gradient)
     end
 end
 
-function apply!(output::AbstractVector, input::AbstractVector, layer::ResidualLayer)
+function apply!(output::AbstractVector, input::AbstractVector, layer::SympLinLayer_q)
     q = @view input[1:N÷2]
     p = @view input[N÷2+1:N]
     q_out = @view output[N÷2+1:N] 
