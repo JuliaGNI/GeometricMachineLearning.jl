3b94b715c1b7b8478516da72332f35158041d68e
Michael Kraus <michael.kraus@ipp.mpg.de>
2022-07-18 08:43:58 +0200

    Add three-argument method for add!

--- src/arrays/add.jl
+++ src/arrays/add.jl
@@ -3,3 +3,8 @@ function add!(x::AbstractArray, b::AbstractArray)
     @assert shape(x) == shape(b)
     x .+= b
 end
+
+function add!(x::AbstractArray, a::AbstractArray, b::AbstractArray)
+    @assert shape(x) == shape(a) == shape(b)
+    x .= a .+ b
+end
