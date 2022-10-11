13bb1dba6a0fa201c2d8fb4ffd3cde7555655e04
Michael Kraus <michael.kraus@ipp.mpg.de>
2022-10-11 12:08:32 +0200

    Some refactoring.

--- test_lux/hnn_lux.jl
+++ test_lux/hnn_lux.jl
@@ -5,32 +5,44 @@ using Optimisers
 using ProgressMeter
 using Random
 
-#generate data
+# generate data
 include("../scripts/data.jl")
 
-data,target = get_data_set()
+data, target = get_data_set()
 data = reshape(data,100,1)
 target = reshape(target,100,1)
 
 ld = 10
+act = tanh
 
-model = Chain(Dense(2, ld, tanh),
-                Dense(ld,   ld, tanh),
-                Dense(ld,    1, tanh))
+model = Chain(Dense(2,  ld, act),
+              Dense(ld, ld, act),
+              Dense(ld,  1, act))
 
-#look at this in more detail; sets bias to zero for example
-ps,st = Lux.setup(Random.default_rng(),model)
+# look at this in more detail; sets bias to zero for example
+ps, st = Lux.setup(Random.default_rng(), model)
 
-#compute Vector fields associated with network
+# define Hamiltonian via evaluation of network
+function hnn(model, x, ps, st)
+    y, state = Lux.apply(model, x, ps, st)
+    return sum(y)
+end
 
-hnn_vf(model,x,ps,st) = [0 1; -1 0] * gradient(ξ -> sum(Lux.apply(model,ξ,ps,st)[1]),x)[1]
-#loss for a single datum
-loss_sing(model,x,y,ps,st) = sqeuclidean(hnn_vf(model,x,ps,st),y)
-loss(model,x,y,ps,st) = mapreduce(i -> loss_sing(model,x[i],y[i],ps,st),+,eachindex(x,y))
+# compute Vector fields associated with network
+grad_h(model,x,ps,st) = gradient(ξ -> hnn(model,ξ,ps,st), x)[1]
+hnn_vf(model,x,ps,st) = [0 1; -1 0] * grad_h(model,x,ps,st)
 
+# loss for a single datum
+# loss_sing(model,x,y,ps,st) = sqeuclidean(hnn_vf(model,x,ps,st), y)
+loss_sing(model,x,y,ps,st) = sqeuclidean(grad_h(model,x,ps,st), y)
 
-#This doesn't work!!!!!
-loss_gradient(model,x,y,ps,st) = gradient(Ω -> loss(model,x,y,Ω,st),ps)[1]
+# total loss
+loss(model,x,y,ps,st) = mapreduce(i -> loss_sing(model,x[i],y[i],ps,st), +, eachindex(x,y))
+# loss(model,x,y,ps,st) = mapreduce(z -> loss_sing(model, z..., ps, st), +, zip(x,y))
+
+# loss gradient
+# This doesn't work!!!!!
+loss_gradient(model,x,y,ps,st) = gradient(Ω -> loss(model,x,y,Ω,st), ps)[1]
 
 
 function train_flux_hnn(model,ps,st,data,target,runs)
@@ -40,8 +52,8 @@ function train_flux_hnn(model,ps,st,data,target,runs)
         #do a couple learning runs
         @showprogress 1 "Training..." for j in 1:runs
                 #gradient step
-                batch = ceil.(Int,rand(10))
-                step = loss_gradient(model,data[batch],target[batch],ps,st)
+                batch = ceil.(Int, rand(10))
+                step = loss_gradient(model, data[batch], target[batch], ps, st)
 
                 #make gradient steps for all the model parameters W & b
                 for Wb in ps
@@ -49,10 +61,10 @@ function train_flux_hnn(model,ps,st,data,target,runs)
                 end
 
                 #total loss i.e. loss computed over all data
-                total_loss[j] = loss(model,data,target,ps,st)
+                total_loss[j] = loss(model, data, target, ps, st)
         end
 
         return (model, data, target, ps, st, total_loss)
 end
 
-train_flux_hnn(model,ps,st,data,target,1)
+train_flux_hnn(model, ps, st, data, target, 1)
