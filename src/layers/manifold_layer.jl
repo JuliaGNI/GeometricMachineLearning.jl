a53e57b2feceb61058501b9d0fdbb31ff4b0ba07
Benedikt Brantner <benedikt.brantner@icloud.com>
2022-10-18 18:32:23 +0200

    Added optimization step as test.

--- src/layers/manifold_layer.jl
+++ src/layers/manifold_layer.jl
@@ -21,13 +21,13 @@ end
 make_sympl_mat(n) = hcat(vcat(zeros(n÷2,n÷2),-I(n÷2)),vcat(I(n÷2),zeros(n÷2,n÷2)))
 
 #maybe implement another random number generator
-function SymplecticStiefelLayer(dim_in::Int, dim_out::Int; inverse::Bool = false)
-    iseven(dim_in) && iseven(dim_out) || error("Dimension must be even.")
-    dim_in ≤ dim_out || error("Output dimension must be bigger than input dimension.")
-    M = Manifolds.SymplecticStiefel(dim_out, dim_in)
+function SymplecticStiefelLayer(dim_n::Int, dim_N::Int; inverse::Bool = false)
+    iseven(dim_n) && iseven(dim_N) || error("Dimension must be even.")
+    dim_n ≤ dim_N || error("Output dimension must be bigger than input dimension.")
+    M = Manifolds.SymplecticStiefel(dim_N, dim_n)
     init_weight = () -> rand(M)
-    SymplecticStiefelLayer{inverse, typeof(init_weight), typeof(M)}(dim_in, dim_out, M, init_weight,
-    make_sympl_mat(dim_in), make_sympl_mat(dim_out))
+    SymplecticStiefelLayer{inverse, typeof(init_weight), typeof(M)}(dim_n, dim_N, M, init_weight,
+    make_sympl_mat(dim_n), make_sympl_mat(dim_N))
 end
 
 function Lux.initialparameters(RNG::AbstractRNG, d::SymplecticStiefelLayer)
@@ -39,7 +39,7 @@ function Lux.initialstates(RNG::AbstractRNG, d::SymplecticStiefelLayer)
 end
 
 function Lux.parameterlength(d::SymplecticStiefelLayer)
-    (4 * d.dim_out - 2 * d.dim_in + 1) * d.dim_in
+    (4 * d.dim_N - 2 * d.dim_n + 1) * d.dim_n
 end
 
 Lux.statelength(d::SymplecticStiefelLayer) = 0
@@ -62,9 +62,12 @@ using Random
 
 model1 = SymplecticStiefelLayer(10,20;inverse=true)
 ps, st = Lux.setup(Random.default_rng(),model1)
-gradient(p -> sum(Lux.apply(model1, rand(20), p, st)[1]), ps)[1]
+g₁ =  gradient(p -> sum(Lux.apply(model1, rand(20), p, st)[1]), ps)[1]
 
 
 model2 = Chain(Gradient(200,1000),Gradient(200,500),SymplecticStiefelLayer(20,200;inverse=true),Gradient(20,50))
 ps, st = Lux.setup(Random.default_rng(),model2)
-gradient(p -> sum(Lux.apply(model2, rand(200), p, st)[1]), ps)[1]
+g₂ =  gradient(p -> sum(Lux.apply(model2, rand(200), p, st)[1]), ps)[1]
+
+o = StandardOptimizer()
+apply!(o, ps, g₂, st)
