285c31e76f07f3b7de237ba70d6ce915d27dae90
Benedikt Brantner <benedikt.brantner@icloud.com>
2022-10-18 18:31:31 +0200

    Implemented the simplest optimizer in a very naive way.

---
+++ src/optimizers/StandardOptimizer.jl
@@ -0,0 +1,25 @@
+"""
+Define the Standard optimizer, i.e. W ← W - η*∇f(W)
+Or the riemannian manifold equivalent, if applicable.
+"""
+
+struct StandardOptimizer{T} <: AbstractOptimizer
+    η::T
+end
+StandardOptimizer(η = 1f-2) = StandardOptimizer{typeof(η)}(η)
+
+function apply!(o::StandardOptimizer, x, dx, st::NamedTuple)
+    layer_names = keys(st)
+    i = 0
+    for layer in layer_names
+        i += 1
+        if isempty(st[layer])
+            for obj in keys(ps[layer])
+                x[layer][obj] .-= o.η * dx[layer][obj]
+            end 
+        else
+            Manifolds.retract_caley!(st[layer].ManifoldType(size(ps[layer].weight)...), 
+            x[layer].weight, x[layer].weight, -o.η * dx[layer].weight)
+        end
+    end
+end
