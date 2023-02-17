e52ee7712c4af27ad070b104904541927b96c7cb
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-02-17 11:14:33 +0100

    Minor fix.

--- src/optimizers/abstract_optimizer.jl
+++ src/optimizers/abstract_optimizer.jl
@@ -2,7 +2,7 @@
 abstract type AbstractOptimizer end 
 
 function apply!(o::AbstractOptimizer, state, model, x, dx)
-    for i in eachindex(model)
+    for i in eachindex(model,x,dx)
         update_layer!(o, state, model[i], x[i], dx[i])
     end
 end
