9d89fc66efb53a5ecc794cb888fbbea18196887d
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-04-25 11:39:05 +0200

    Add some docs.

--- src/architectures/architectures.jl
+++ src/architectures/architectures.jl
@@ -1,4 +1,11 @@
 
 abstract type AbstractArchitecture end
 
+"""
+The function `chain` returns a chain of a specific neural network architecture
+and a specific backend and is called by:
+```
+chain(::AbstractArchitecture, ::AbstractBackend)
+```
+"""
 function chain end
