19de35fccc5609ae6817d02a53b048206e28c264
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-04-25 13:33:05 +0200

    Fix linear layer test.

--- test/layers/linear_layer_tests.jl
+++ test/layers/linear_layer_tests.jl
@@ -9,4 +9,4 @@ Random.seed!(1234)
 dummy_model = Linear(4,change_q=false)
 ps,st = Lux.setup(Random.default_rng(), dummy_model)
 
-@test dummy_model(ones(4),ps,st)[1] ≈ [1.0, 1.0, 1.2091209590435028, 0.34127573668956757]  atol = 1e-14
+@test dummy_model(ones(4),ps,st)[1] ≈ [1.0, 1.0, 1.1045604944229126, 0.670637883245945]  atol = 1e-14
