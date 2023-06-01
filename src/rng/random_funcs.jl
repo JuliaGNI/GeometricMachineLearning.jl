a00814bf1eda3736a81cd447775d56639ac268a3
benedict-96 <benedikt.brantner@icloud.com>
2023-06-01 14:32:57 +0200

    Added a random routine for SymplecticStiefel.

--- src/rng/random_funcs.jl
+++ src/rng/random_funcs.jl
@@ -3,11 +3,16 @@ function Lux.glorot_uniform(rng::TrivialInitRNG, dims::Integer...; gain = 1)
     rand(rng, Float32, dims...)
 end
  
-function Lux.glorot_uniform(rng::TrivialInitRNG, ::Type{StiefelManifold}, N, n; gain = 1)
-    rand(rng, StiefelManifold{Float32}, N, n)
+function Lux.glorot_uniform(rng::TrivialInitRNG, T::Type{Manifold}, N, n; gain = 1)
+    rand(rng, T, N, n)
 end
 
 function Lux.glorot_uniform(rng::AbstractRNG, ::Type{StiefelManifold}, N, n; gain = 1)
     A = Lux.glorot_uniform(rng, N, n; gain=gain)
-    StiefelManifold(qr(A).Q[1:N, 1:n])
+    StiefelManifold(qr!(A).Q[1:N, 1:n])
+end
+
+function Lux.glorot_uniform(rng::AbstractRNG, ::Type{SymplecticStiefelManifold}, N2, n2; gain = 1)
+    A = Lux.glorot_uniform(rng, N2, n2; gain=gain)
+    StiefelManifold(sr!(A).S[1:N2, vcat(1:(n2÷2), (n2÷2 + 1):n2)])
 end
