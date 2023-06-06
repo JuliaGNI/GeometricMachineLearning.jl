14028c4ebab4946b4032c0f3e51f797b601ea37d
benedict-96 <benedikt.brantner@icloud.com>
2023-06-06 17:53:28 +0200

    Added a test for the global section property.

--- test/manifolds/grassmann_manifold.jl
+++ test/manifolds/grassmann_manifold.jl
@@ -1,6 +1,7 @@
 using Test 
 using LinearAlgebra
 using GeometricMachineLearning
+using GeometricMachineLearning: global_section
 
 function check_gradient(T, N::Integer, n::Integer)
     Y = rand(GrassmannManifold{T}, N, n)
@@ -12,4 +13,17 @@ function check_gradient(T, N::Integer, n::Integer)
     norm(tr(Δ'*A) - metric(Y, Δ, V))
 end
 
-display(check_gradient(Float32, 10, 5))
+function global_section_test(T, N::Integer, n::Integer)
+    Y = rand(GrassmannManifold{T}, N, n)
+    Q = global_section(Y)
+    πQ = Q[1:N, 1:n]
+    norm(Y - πQ*πQ'*Y)
+end
+
+
+function run_tests(T, N, n)
+    display(check_gradient(T, N, n))
+    display(global_section_test(T, N, n))
+end
+
+run_tests(Float32, 10, 4)
