1d2eb7143bff91cb0c440ee87ec0e8185987d7cd
benedict-96 <benedikt.brantner@icloud.com>
2023-06-20 15:01:39 +0200

    changed file name

--- test/performance_tests/adam_update_gpu_test.jl
+++ test/performance_tests/adam_update_gpu_test.jl
@@ -1,12 +1,13 @@
 using GeometricMachineLearning, CUDA
 
 
-function update_cache_test(dev, N, n)
+function adam_update_test(dev, N, n)
     map_to_dev(A::AbstractArray) = GeometricMachineLearning.convert_to_dev(CUDA.device(), A)
 
-    B₁ = rand(StiefelLieAlgHorMatrix{T}, N, n) |> gpu 
-    B₂ = rand(StiefelLieAlgHorMatrix{T}, N, n) |> gpu
+    B₁ = rand(StiefelLieAlgHorMatrix{T}, N, n) |> map_to_dev
+    B₂ = rand(StiefelLieAlgHorMatrix{T}, N, n) |> map_to_dev
 
     cache = AdamCache(B₁, B₂)
 
+    o 
 end
