715557cde9f02d21a11eb03ca47f1161766aefcd
benedict-96 <benedikt.brantner@icloud.com>
2023-06-20 16:24:39 +0200

    performance test for optimizer update for StiefelLieAlgHorMatrix

---
+++ test/performance_tests/optimizer_update_gpu_test.jl
@@ -0,0 +1,17 @@
+using GeometricMachineLearning, CUDA
+
+
+function adam_update_test(dev, N, n)
+    map_to_dev(A::AbstractArray) = GeometricMachineLearning.convert_to_dev(CUDA.device(), A)
+
+    B₁ = rand(StiefelLieAlgHorMatrix{T}, N, n) |> map_to_dev
+    B₂ = rand(StiefelLieAlgHorMatrix{T}, N, n) |> map_to_dev
+    cache = AdamCache(B₁, B₂)
+
+    B = rand(StiefelLieAlgHorMatrix{T}, N, n) |> map_to_dev
+
+    o = AdamOptimizer()
+
+    @time update!(o, cache, B)
+end
+
