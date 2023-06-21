88e95a3449ae266aee897d3cbd40111f5c9ba430
benedict-96 <benedikt.brantner@icloud.com>
2023-06-21 12:27:04 +0200

    moved file

--- test/performance_tests/zygote_specific/dense_gpu.jl
+++ test/performance_tests/zygote_specific/dense_gpu.jl
@@ -1,14 +1,14 @@
 using Lux, Plots, LinearAlgebra, ProgressMeter, CUDA
 using GeometricMachineLearning, Random, Zygote
 
-number_data_points = 1000
+number_data_points = 10000
 data_input = 2*π*(1:number_data_points)/number_data_points
 data_output = sin.(data_input)
 
 data_input = Tuple([data_input[i]] for i in 1:number_data_points)
 data_output = Tuple([data_output[i]] for i in 1:number_data_points)
 
-layer_width = 20
+layer_width = 100
 model = Chain(Dense(1, layer_width, tanh), Dense(layer_width, layer_width, tanh), Dense(layer_width, 1))
 
 function train(dev::Device, n_steps, batch_size)
@@ -30,8 +30,8 @@ function train(dev::Device, n_steps, batch_size)
         batch = Int.(ceil.(rand(batch_size)*number_data_points))
         dat_in₁ = data_input[batch[1]]
         dat_out₁   = data_output[batch[1]]
-        @time dp = Zygote.gradient(ps -> loss(dat_in₁, dat_out₁, ps, st), ps)[1]
-        for i in 2:batch_size
+        dp = Zygote.gradient(ps -> loss(dat_in₁, dat_out₁, ps, st), ps)[1]
+        Base.Threads.@threads for i in 2:batch_size
             dat_in = data_input[batch[i]]
             dat_out = data_input[batch[i]]
             dp = _add(dp, Zygote.gradient(ps -> loss(dat_in, dat_out, ps, st), ps)[1])
@@ -44,8 +44,8 @@ function train(dev::Device, n_steps, batch_size)
     training_error_array, ps, st
 end
 
-n_steps = 100
-batch_size = 10
+n_steps = 10
+batch_size = 10000
 
 training_error_cpu, ps_cpu, st = train(CPUDevice(), n_steps, batch_size)
 
