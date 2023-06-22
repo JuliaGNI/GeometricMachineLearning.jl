640992f690d3b4a8e2da46f2a92910ad3cce29cb
benedict-96 <benedikt.brantner@icloud.com>
2023-06-22 15:44:38 +0200

    moved the 'new bits' to the kernels section

--- test/performance_tests/zygote_specific/compute_loss.jl
+++ test/performance_tests/zygote_specific/compute_loss.jl
@@ -1,16 +1,19 @@
 using Lux, Zygote, CUDA, Random, GeometricMachineLearning, Test, GPUArrays
 
-model = Chain(Dense(10, 20, tanh), Dense(20,20, tanh), Dense(20,5, use_bias=false))
-ps_gpu, st_gpu = Lux.setup(CUDa.device(), Random.default_rng(), model)
+input_size = 10
 
-loss(x, ps, st) = sum(Lux.apply(model, x, ps, st)[1])
+model = Chain(Dense(input_size, 1, tanh), Dense(1,20, tanh), Dense(20,5, use_bias=false))
+ps_gpu, st_gpu = Lux.setup(CUDA.device(), Random.default_rng(), model)
 
-function kernel(x)
+loss(input, ps, st) = sum(Lux.apply(model, input, ps, st)[1])
+
+function kernel(x, ps, st, input)
     i = threadIdx().x
-    x[i] = loss(CUDA.rand(10), ps, st)
+    x[i] = loss(input, ps, st)
     return 
 end
 
-x = CUDA.zeros(100)
+x = CUDA.zeros(2)
 
-@cuda threads=length(x) kernel(x) 
+input = CUDA.rand(input_size)
+@cuda threads=length(x) kernel(x, ps_gpu, st_gpu, input) 
