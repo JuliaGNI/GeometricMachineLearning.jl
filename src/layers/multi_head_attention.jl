c452ff6de87a4d3ba48082eb7751e35053145aa0
benedict-96 <benedikt.brantner@icloud.com>
2023-06-30 19:58:41 +0200

    Added an additional option to specify if one would like to compute a ResNet connection.

--- src/layers/multi_head_attention.jl
+++ src/layers/multi_head_attention.jl
@@ -1,13 +1,13 @@
-struct MultiHeadAttention{Stiefel, Retraction, F1} <: Lux.AbstractExplicitLayer
+struct MultiHeadAttention{Stiefel, Retraction, add_connection, F1} <: Lux.AbstractExplicitLayer
     dim::Integer
     n_heads::Integer
     init_weight::F1
 end
 
 default_retr = Geodesic()
-function MultiHeadAttention(dim::Integer, n_heads::Integer; Stiefel::Bool=false, Retraction::AbstractRetraction=default_retr, init_weight=Lux.glorot_uniform)
+function MultiHeadAttention(dim::Integer, n_heads::Integer; Stiefel::Bool=false, Retraction::AbstractRetraction=default_retr add_connection=true, init_weight=Lux.glorot_uniform)
     @assert dim % n_heads == 0
-    MultiHeadAttention{Stiefel, typeof(Retraction), typeof(init_weight)}(dim, n_heads, init_weight)
+    MultiHeadAttention{Stiefel, typeof(Retraction), add_connection, typeof(init_weight)}(dim, n_heads, init_weight)
 end
 
 Lux.parameterlength(d::StiefelLayer{false}) = 3*d.dim^2
@@ -63,7 +63,7 @@ function Lux.initialparameters(rng::AbstractRNG, d::MultiHeadAttention{true})
     (PQ=PQ, PK=PK, PV=PV)
 end
 
-function Lux.apply(d::MultiHeadAttention, x::AbstractMatrix{T}, ps::NamedTuple, st::NamedTuple) where T
+function Lux.apply(d::MultiHeadAttention{Stiefel, Retraction, true}, x::AbstractMatrix{T}, ps::NamedTuple, st::NamedTuple) where {Stiefel, Retraction, T}
     Dₕ = d.dim ÷ d.n_heads
     dim, input_length = size(x)
     @assert dim == d.dim
@@ -75,3 +75,19 @@ function Lux.apply(d::MultiHeadAttention, x::AbstractMatrix{T}, ps::NamedTuple, 
     end
     x + output, st
 end
+
+function Lux.apply(d::MultiHeadAttention{Stiefel, Retraction, true}, x::AbstractArray{T, 3}, ps::NamedTuple, st::NamedTuple) where {Stiefel, Retraction, T} 
+    Dₕ = d.dim ÷ d.n_heads
+    dim, input_length, number_data = size(x)
+    @assert dim == d.dim 
+
+    output = typeof(x)(zeros(T, 0, input_length, number_data))
+    for i in 1:d.n_heads 
+        key = Symbol("head_"*string(i))
+        #use tensor_mat_mul for this computation
+        #output = vcat(output, ps.PV[key]'*x*Lux.softmax((ps.PQ[key]'*x)'*(ps.PK[key]'*x)))
+    end
+    x + output, st
+end
+
+    
