aae20e33b7a1311be63ffb566cfaf912a14db9b3
benedict-96 <benedikt.brantner@icloud.com>
2023-06-30 19:59:22 +0200

    Implementation of parallel evaluation of MHA

---
+++ custom_neural_networks/multi_head_attention_parallel.jl
@@ -0,0 +1,28 @@
+using GeometricMachineLearning, LinearAlgebra, ProgressMeter, Plots
+import Lux, Zygote, Random, MLDatasets, Flux, Lux.gpu
+
+#MNIST images are 28×28, so a sequence_length of 16 = 4² means the image patches are of size 7² = 49
+image_dim = 28
+patch_length = 7
+n_heads = 7
+n_layers = 5
+patch_number = (image_dim÷patch_length)^2
+
+train_x, train_y = MLDatasets.MNIST(split=:train)[:]
+test_x, test_y = MLDatasets.MNIST(split=:test)[:]
+
+train_x_reshaped = zeros(Float32, 49, 16, size(train_x, 3))
+test_x_reshaped = zeros(Float32, 49, 16, size(test_x, 3))
+
+#preprocessing steps 
+for i in 1:size(train_x, 3)
+    train_x_reshaped[:, :, i] = split_and_flatten(train_x[:, :, i], patch_length)
+end
+for i in 1:size(test_x, 3)
+    test_x_reshaped[:, :, i] = split_and_flatten(test_x[:, :, i], patch_length)
+end
+
+#implement this encoding yourself!
+train_y = Flux.onehotbatch(train_y, 0:9) 
+test_y = Flux.onehotbatch(test_y, 0:9)
+
