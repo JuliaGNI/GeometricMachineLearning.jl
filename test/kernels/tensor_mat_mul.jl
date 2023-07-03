2f2fc66310b9118a6923108f16377282358240e9
benedict-96 <benedikt.brantner@icloud.com>
2023-07-03 11:30:19 +0200

    Finished test for tensor matrix multiplication.

--- test/kernels/tensor_mat_mul.jl
+++ test/kernels/tensor_mat_mul.jl
@@ -1,4 +1,7 @@
 using GeometricMachineLearning: tensor_mat_mul
+using Random, Test
+
+backend = backend = KernelAbstractions.CPU()
 
 a = rand!(allocate(backend, Float32, 256, 123, 10))
 b = rand!(allocate(backend, Float32, 123, 45))
@@ -7,6 +10,9 @@ c = KernelAbstractions.zeros(backend, Float32, 256, 45, 10)
 tensor_mat_mul!(a,b,c)
 KernelAbstractions.synchronize(backend)
 
-#a_mul_b = 
+c_manual = zeros(Float32, dim1, dim3, num_data)
+@time for i in 1:num_data
+    c_manual[:,:,i] = a[:,:,i]*b 
+end
 
-#@test isapprox(c, a*b)
+@test isapprox(c, c_manual)
