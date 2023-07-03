faa94ff4fe416f6c9980e1c35f779080fefd063d
benedict-96 <benedikt.brantner@icloud.com>
2023-07-03 14:06:12 +0200

    Moved this into src from a different directory and removed tests that were part of the file.

--- src/kernels/tensor_mat_mul.jl
+++ src/kernels/tensor_mat_mul.jl
@@ -1,7 +1,4 @@
-using KernelAbstractions, Test, Random
-#include(joinpath(dirname(pathof(KernelAbstractions)), "../examples/utils.jl")) # Load backend
-
-# Simple kernel for matrix multiplication
+# Simple kernel for tensor-matrix multiplication (maybe you need to add a block index here!)
 @kernel function tensor_mat_mul_kernel!(c, a, b)
     i, j, k = @index(Global, NTuple)
 
@@ -22,29 +19,3 @@ function tensor_mat_mul!(c, a, b)
     kernel! = tensor_mat_mul_kernel!(backend)
     kernel!(c, a, b, ndrange=size(c)) 
 end
-
-backend = KernelAbstractions.CPU()
-
-#a = rand!(allocate(backend, Float32, 256, 123, 10))
-#b = rand!(allocate(backend, Float32, 123, 45))
-#c = KernelAbstractions.zeros(backend, Float32, 256, 45, 10)
-
-dim1 = 2^4
-dim2 = 2^1
-dim3 = 2^4
-num_data = 2^20
-
-a = rand(Float32, dim1, dim2, num_data)
-b = rand(Float32, dim2, dim3)
-c = zeros(Float32, dim1, dim3, num_data)
-
-@time tensor_mat_mul!(c,a,b)
-KernelAbstractions.synchronize(backend)
-
-c_manual = zeros(Float32, dim1, dim3, num_data)
-@time for i in 1:num_data
-    c_manual[:,:,i] = a[:,:,i]*b 
-end
-
-isapprox(c, c_manual)
-
