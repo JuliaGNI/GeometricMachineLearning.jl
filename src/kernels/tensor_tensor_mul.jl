05f575d7beb7efee9d88874dc03e34264faa8290
benedict-96 <benedikt.brantner@icloud.com>
2023-07-03 13:34:23 +0200

    Fixed typo.

--- src/kernels/tensor_tensor_mul.jl
+++ src/kernels/tensor_tensor_mul.jl
@@ -1,21 +1,22 @@
 # Simple kernel for tensor-matrix multiplication (maybe you need to add a block index here!)
-@kernel function tensor_mat_mul_kernel!(c, a, b)
+@kernel function tensor_tensor_mul_kernel!(c, a, b)
     i, j, k = @index(Global, NTuple)
 
     # creating a temporary sum variable for matrix multiplication
     tmp_sum = zero(eltype(c))
     for l = 1:size(a)[2]
-        tmp_sum += a[i, l, k] * b[l, j]
+        tmp_sum += a[i, l, k] * b[l, j, k]
     end
 
     c[i,j,k] = tmp_sum
 end
 
 # Creating a wrapper kernel for launching with error checks
-function tensor_mat_mul!(c, a, b)
+function tensor_tensor_mul!(c, a, b)
+    @assert size(a)[3] == size(b)[3]
     @assert size(a)[2] == size(b)[1]
 
     backend = KernelAbstractions.get_backend(a)
-    kernel! = tensor_mat_mul_kernel!(backend)
+    kernel! = tensor_tensor_mul_kernel!(backend)
     kernel!(c, a, b, ndrange=size(c)) 
 end
