c868fa1b10f24649d1b18f1f21ded52800ea0626
benedict-96 <benedikt.brantner@icloud.com>
2023-07-04 21:11:51 +0200

    Changed numbers to variables.

--- test/layers/multihead_attention_layer_parallel.jl
+++ test/layers/multihead_attention_layer_parallel.jl
@@ -1,4 +1,4 @@
-using GeometricMachineLearning, LinearAlgebra, ProgressMeter, Plots
+using GeometricMachineLearning, LinearAlgebra, ProgressMeter, Plots, Test
 import Lux, Zygote, Random, MLDatasets, Flux, Lux.gpu
 
 #MNIST images are 28×28, so a sequence_length of 16 = 4² means the image patches are of size 7² = 49
@@ -11,8 +11,8 @@ patch_number = (image_dim÷patch_length)^2
 train_x, train_y = MLDatasets.MNIST(split=:train)[:]
 test_x, test_y = MLDatasets.MNIST(split=:test)[:]
 
-train_x_reshaped = zeros(Float32, 49, 16, size(train_x, 3))
-test_x_reshaped = zeros(Float32, 49, 16, size(test_x, 3))
+train_x_reshaped = zeros(Float32, patch_length^2, patch_number, size(train_x, 3))
+test_x_reshaped = zeros(Float32, patch_length^2, patch_number, size(test_x, 3))
 
 #preprocessing steps 
 for i in 1:size(train_x, 3)
@@ -26,7 +26,17 @@ end
 train_y = Flux.onehotbatch(train_y, 0:9) 
 test_y = Flux.onehotbatch(test_y, 0:9)
 
-model = MultiHeadAttention(49, 7)
+model = MultiHeadAttention(patch_length^2, n_heads)
 ps, st = Lux.setup(Random.default_rng(), model)
 
-Lux.apply(model, train_x_reshaped[:,:,1:2], ps, st)
+n_data = 10000
+
+@time output1 = Lux.apply(model, train_x_reshaped[:,:,1:n_data], ps, st)[1]
+
+output2 = zeros(patch_length^2, patch_number, n_data)
+
+@time for i in 1:n_data
+    output2[:, :, i] = Lux.apply(model, train_x_reshaped[:, :, i], ps, st)[1]
+end
+
+@test isapprox(output1, output2)
