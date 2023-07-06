c97726b3d7be33fb18a100c7eed6aea7162e849a
benedict-96 <benedikt.brantner@icloud.com>
2023-07-06 23:21:49 +0200

    adjusted the tests

--- test/custom_ad_rules/kernel_pullbacks.jl
+++ test/custom_ad_rules/kernel_pullbacks.jl
@@ -1,6 +1,18 @@
 using GeometricMachineLearning: tensor_mat_mul, mat_tensor_mul
 using ChainRulesTestUtils
 
-test_rrule(tensor_mat_mul, rand(10, 3, 3), rand(3, 3))
-test_rrule(mat_tensor_mul, rand(3, 3), rand(3, 3, 3))
-#compute the derivative with FiniteDifferences.jl
+function main(first_dim, second_dim, third_dim)
+    test_rrule(tensor_mat_mul, rand(first_dim, second_dim, third_dim), rand(first_dim, second_dim))
+    test_rrule(mat_tensor_mul, rand(first_dim, second_dim), rand(first_dim, second_dim, third_dim))
+    #compute the derivative with FiniteDifferences.jl
+end
+
+dim_range = 10
+num_tests = 10
+for _ in 1:num_tests
+    first_dim = Int(ceil(dim_range*rand()))
+    second_dim = Int(ceil(dim_range*rand()))
+    third_dim = Int(ceil(dim_range*rand()))
+    print("dims are : (", first_dim, ", ", second_dim, ", ", third_dim, ")\n")
+    main(first_dim, second_dim, third_dim)
+end
