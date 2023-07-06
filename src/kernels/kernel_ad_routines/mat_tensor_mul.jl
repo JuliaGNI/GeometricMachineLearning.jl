c23301a29f3a33fd2445ea32d6e97765680fb035
benedict-96 <benedikt.brantner@icloud.com>
2023-07-06 18:19:00 +0200

    Fixed the derivative for the mat_tensor_mul kernel. Still have to check if this still works if I change dimension.

--- src/kernels/kernel_ad_routines/mat_tensor_mul.jl
+++ src/kernels/kernel_ad_routines/mat_tensor_mul.jl
@@ -3,22 +3,22 @@ This implements the custom pullback tor mat_tensor_mul
 """
 
 #the @thunk macro means that the computation is only performed in case it is needed
-    function ChainRulesCore.rrule(::typeof(mat_tensor_mul), B::AbstractMatrix{T}, A::AbstractArray{T, 3}) where T
-        @assert axes(A, 2) == axes(B, 1)
-        C = tensor_mat_mul(A, B)
-        function tensor_mat_mul_pullback(C_diff)
-            f̄ = NoTangent()
-            #tensor_transpose_mat_mul
-            B_diff = @thunk sum(tensor_tensor_transpose_mul(C_diff, A), dims=3)
-            A_diff = @thunk mat_tensor_mul(B', C_diff)
-            return f̄, B_diff, A_diff
-        end
-        return C, tensor_mat_mul_pullback
+function ChainRulesCore.rrule(::typeof(mat_tensor_mul), B::AbstractMatrix{T}, A::AbstractArray{T, 3}) where T
+    @assert axes(A, 2) == axes(B, 1)
+    C = mat_tensor_mul(B, A)
+    function mat_tensor_mul_pullback(C_diff)
+        f̄ = NoTangent()
+        #tensor_transpose_mat_mul
+        B_diff = @thunk sum(tensor_tensor_transpose_mul(C_diff, A), dims=3)
+        A_diff = @thunk mat_tensor_mul(B', C_diff)
+        return f̄, B_diff, A_diff
     end
+    return C, mat_tensor_mul_pullback
+end
+   
+mat_tensor_mul(B::AbstractMatrix, A::Thunk) = Thunk(() -> mat_tensor_mul(B, unthunk(A)))
     
-   mat_tensor_mul(A::Thunk, B::AbstractMatrix) = Thunk(() -> mat_tensor_mul(unthunk(A), B))
-    
-    function tensor_tensor_transpose_mul(A::AbstractArray{T, 3}, B::Thunk) where T 
-        Thunk(() -> tensor_tensor_transpose_mul(A, unthunk(B)))
-    end
+function tensor_tensor_transpose_mul(B::Thunk, A::AbstractArray{T, 3}) where T 
+    Thunk(() -> tensor_tensor_transpose_mul(unthunk(B), A))
+end
 
