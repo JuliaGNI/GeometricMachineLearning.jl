5305f18119ec5da6dbe4179df4d57636a9bada0d
benedict-96 <benedikt.brantner@icloud.com>
2023-07-06 17:51:04 +0200

    Added new test for mat_tensor_mul.

--- test/custom_ad_rules/tensor_matrix_multiplication.jl
+++ test/custom_ad_rules/tensor_matrix_multiplication.jl
@@ -1,4 +1,6 @@
-using GeometricMachineLearning: tensor_mat_mul
+using GeometricMachineLearning: tensor_mat_mul, mat_tensor_mul
 using ChainRulesTestUtils
+
 test_rrule(tensor_mat_mul, rand(3, 3, 3), rand(3, 3))
+test_rrule(mat_tensor_mul, rand(3, 3), rand(3, 3, 3))
 #compute the derivative with FiniteDifferences.jl
