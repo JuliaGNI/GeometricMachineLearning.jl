537bb287c5a7f35dd4d80f3c66d5ad1a65a23d94
Duez Theo <theo.duez@student-cs.fr>
2023-07-11 23:35:53 +0200

    corrections

--- scripts/test/runtests.jl
+++ scripts/test/runtests.jl
@@ -1,4 +1,9 @@
+using GeometricMachineLearning
+using Test
 using SafeTestsets
+
+include("generation_of_data.jl")
+include("macro_testerror.jl")
 
 @safetestset "Architecture                                                                    " begin include("test_architecture.jl") end
 @safetestset "Data                                                                            " begin include("test_data.jl") end
@@ -6,5 +11,5 @@ using SafeTestsets
 @safetestset "TrainingSet                                                                     " begin include("test_trainingSet.jl") end
 @safetestset "Training                                                                        " begin include("test_training.jl") end
 @safetestset "NeuralNetSolution                                                               " begin include("test_neuralnet_solution.jl") end
-@safetestset "Integrators                                                                     " begin include("test_integrator.jl") end
+@safetestset "Problem & Integrators                                                           " begin include("test_integrator.jl") end
 
