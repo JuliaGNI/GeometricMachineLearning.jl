068bdd53b60489effc43b97fe18dfc10b0a21e69
Michael Kraus <michael.kraus@ipp.mpg.de>
2023-07-12 14:11:17 +0200

    Make optimizer structs immutable.

--- src/optimizers/Method_Optimizer/gradient_optimizer.jl
+++ src/optimizers/Method_Optimizer/gradient_optimizer.jl
@@ -2,7 +2,7 @@
 Define the Gradient optimizer, i.e. W ← W - η*∇f(W)
 Or the riemannian manifold equivalent, if applicable.
 """
-mutable struct GradientOptimizer{T<:Real} <: AbstractMethodOptimiser
+struct GradientOptimizer{T<:Real} <: AbstractMethodOptimiser
     η::T
     t::Integer
     GradientOptimizer(η = 1e-2) = new{typeof(η)}(η,0)
