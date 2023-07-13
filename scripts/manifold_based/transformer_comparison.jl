d3d4ab586dec29a8dc57c8e252bfe852d59e3fbb
benedict-96 <benedikt.brantner@icloud.com>
2023-07-13 19:00:52 +0200

    Cleaned up the transformer mnist script and added a Jupiter notebook to better explain what we are doing.

--- scripts/manifold_based/transformer_comparison.jl
+++ scripts/manifold_based/transformer_comparison.jl
@@ -1,35 +1,45 @@
-using GeometricMachineLearning, LinearAlgebra, ProgressMeter
+using GeometricMachineLearning
 using GeometricMachineLearning: ResNet
-import Zygote, Random, MLDatasets, Flux 
-
-using GeometricMachineLearning, LinearAlgebra, ProgressMeter, CUDA #, Plots
-import Lux, Zygote, Random, MLDatasets, Flux
+using LinearAlgebra: norm
+using ProgressMeter: @showprogress
+using Zygote: gradient 
+import MLDatasets
+import Lux
+import Random
 
 image_dim = 28
 patch_length = 7
 n_heads = 7
-n_layers = 16
 patch_number = (image_dim÷patch_length)^2
 
 train_x, train_y = MLDatasets.MNIST(split=:train)[:]
 test_x, test_y = MLDatasets.MNIST(split=:test)[:]
 
-train_x_reshaped = zeros(Float32, patch_length^2, patch_number, size(train_x, 3))
-test_x_reshaped = zeros(Float32, patch_length^2, patch_number, size(test_x, 3))
-
 # preprocessing steps (also perform rescaling so that the images have values between 0 and 1)
-for i in axes(train_x, 3)
-    train_x_reshaped[:, :, i] = split_and_flatten(train_x[:, :, i], patch_length)/255
-end
-for i in axes(test_x, 3)
-    test_x_reshaped[:, :, i] = split_and_flatten(test_x[:, :, i], patch_length)/255
+function preprocess_x(x)
+    x_reshaped = zeros(Float32, patch_length^2, patch_number, size(x, 3))
+    for i in axes(x, 3)
+        x_reshaped[:, :, i] = split_and_flatten(x[:, :, i], patch_length)/255
+    end
+    x_reshaped
 end
 
-# implement this encoding yourself!
-train_y = Flux.onehotbatch(train_y, 0:9) 
-test_y = Flux.onehotbatch(test_y, 0:9)
+train_x_reshaped = preprocess_x(train_x)
+test_x_reshaped = preprocess_x(test_x)
 
-L = 2
+# preprocessing/encoding for y
+function encode_y(y)
+    y_encoded = zeros(Bool, 10, length(y))
+    for i in axes(y,1)
+        y_encoded[y[i]+1,i] = 1
+    end
+    y_encoded
+end
+
+train_y_encoded = encode_y(train_y)
+test_y_encoded = encode_y(test_y)
+
+L = 4
 
 model₀ = Lux.Chain(Tuple(map(_ -> ResNet(49, tanh), 1:L))..., Classification(patch_length^2, 10, use_bias=false))
 model₁ = Lux.Chain( Transformer(patch_length^2, n_heads, L, add_connection=false, Stiefel=false),
@@ -42,46 +52,66 @@ model₄ = Lux.Chain(Transformer(patch_length^2, n_heads, L, add_connection=true
                     Classification(patch_length^2, 10, use_bias=false))
 
 const num = 60000
-function training(model::Lux.Chain, batch_size=1024, n_epochs=10, o=AdamOptimizer())
-    ps, st = Lux.setup(CUDA.device(), Random.default_rng(), model)
-
+function training(model::Lux.Chain, batch_size=32, n_epochs=.01, o=AdamOptimizer(), enable_cuda=false, give_training_error=false)
+    ps, st = enable_cuda ? Lux.setup(CUDA.device(), Random.default_rng(), model) : Lux.setup(Random.default_rng(), model)
+                    
     function loss(ps, x, y)
-        x_eval = Lux.apply(model, x |> cu, ps, st)[1]
-        norm(x_eval - (y |> cu))/sqrt(size(y, 2))
+        x_eval = enable_cuda ? Lux.apply(model, x |> cu, ps, st)[1] : Lux.apply(model, x, ps, st)[1]
+        enable_cuda ? norm(x_eval - (y |> cu))/sqrt(size(y, 2)) : norm(x_eval - (y))/sqrt(size(y, 2))
     end
-
+                    
+    # the number of training steps is calculated based on the number of epochs and the batch size
     training_steps = Int(ceil(n_epochs*num/batch_size))
+    # this records the training error
     loss_array = zeros(training_steps + 1)
-    loss_array[1] = loss(ps, train_x_reshaped |> cu, train_y |> cu)
-    println("initial loss: ", loss_array[1])
-
-    optimizer_instance = Optimizer(CUDA.device(), o, model)
-
+    loss_array[1] = give_training_error ? (enable_cuda ? loss(ps, train_x_reshaped |> cu, train_y_encoded |> cu) : loss(ps, train_x_reshaped, train_y_encoded)) : 0.
+                    
+    give_training_error ? println("initial loss: ", loss_array[1]) : nothing
+                    
+    # initialize the optimizer cache
+    optimizer_instance = enable_cuda ? Optimizer(CUDA.device(), o, model) : Optimizer(o, model)
+                    
     @showprogress "Training network ..." for i in 1:training_steps
+        # draw a mini batch 
         indices = Int.(ceil.(rand(batch_size)*num))
-        x_batch = train_x_reshaped[:, :, indices] |> cu 
-        y_batch = train_y[:, indices] |> cu
-
-        dp = Zygote.gradient(ps -> loss(ps, x_batch, y_batch), ps)[1]
-
+        x_batch = enable_cuda ? (train_x_reshaped[:, :, indices] |> cu) : train_x_reshaped[:, :, indices]
+        y_batch = enable_cuda ? (train_y_encoded[:, indices] |> cu) : train_y_encoded[:, indices]
+                    
+        # compute the gradient using Zygote
+        dp = gradient(ps -> loss(ps, x_batch, y_batch), ps)[1]
+                    
+        #update the cache of the optimizer and the parameter
         optimization_step!(optimizer_instance, model, ps, dp)    
-        loss_array[1+i] = loss(ps, train_x_reshaped |> cu, train_y |> cu)
+                    
+        # compute the loss at the current step
+        loss_array[1+i] = give_training_error ? (enable_cuda ? loss(ps, train_x_reshaped |> cu, train_y_encoded |> cu) : loss(ps, train_x_reshaped, train_y_encoded)) : 0.
+                    
     end
-    println("final loss: ", loss_array[end])
-    println("final test loss: ", loss(ps, test_x_reshaped |> cu, test_y |> cu),"\n")
-
+    #println("final loss: ", loss_array[end])
+    enable_cuda ? println("final test loss: ", loss(ps, test_x_reshaped |> cu, test_y_encoded |> cu),"\n") : println("final test loss: ", loss(ps, test_x_reshaped, test_y_encoded),"\n")
+                    
     loss_array
 end
 
-loss_array₀ = training(model₀)
-loss_array₁ = training(model₁)
-loss_array₂ = training(model₂)
-loss_array₃ = training(model₃)
-loss_array₄ = training(model₄)
+batch_size = 512
+n_epochs = 1
+o = AdamOptimizer(0.001f0, 0.9f0, 0.99f0, 1.0f-8)
+enable_cuda = false
+give_training_error = false
 
-using Plots 
-p = plot(loss_array₀, label="0")
-plot!(p, loss_array₁, label="1")
-plot!(p, loss_array₂, label="2")
-plot!(p, loss_array₃, label="3")
-plot!(p, loss_array₄, label="4")
+loss_array₀ = training(model₀, batch_size, n_epochs, o, enable_cuda, give_training_error)
+loss_array₁ = training(model₁, batch_size, n_epochs, o, enable_cuda, give_training_error)
+loss_array₂ = training(model₂, batch_size, n_epochs, o, enable_cuda, give_training_error)
+loss_array₃ = training(model₃, batch_size, n_epochs, o, enable_cuda, give_training_error)
+loss_array₄ = training(model₄, batch_size, n_epochs, o, enable_cuda, give_training_error)
+
+function plot_stuff()
+    using Plots 
+    p = plot(loss_array₀, label="0")
+    plot!(p, loss_array₁, label="1")
+    plot!(p, loss_array₂, label="2")
+    plot!(p, loss_array₃, label="3")
+    plot!(p, loss_array₄, label="4")
+end
+
+give_training_error ? plot_stuff : nothing 
