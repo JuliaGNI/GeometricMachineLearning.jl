bc0bec86f55f53ce44a3b6faf67acddbc98f2b91
benedict-96 <benedikt.brantner@icloud.com>
2023-07-13 20:20:02 +0200

    Added new option to take first vector instead of average.

--- src/layers/classification.jl
+++ src/layers/classification.jl
@@ -1,9 +1,9 @@
-struct Classification{use_bias} <: Lux.AbstractExplicitLayer
+struct Classification{use_bias， use_average} <: Lux.AbstractExplicitLayer
     n_labels::Integer 
     input_dim::Integer
 
-    function Classification(n_labels::Integer, input_dim::Integer; use_bias::Bool=true)
-        new{use_bias}(n_labels, input_dim)
+    function Classification(n_labels::Integer, input_dim::Integer; use_bias::Bool=true, use_average::Bool=true)
+        new{use_bias, use_average}(n_labels, input_dim)
     end
 end
 
@@ -24,20 +24,32 @@ function Lux.apply(d::Classification{false}, x::AbstractVector, ps::NamedTuple, 
     Lux.softmax(ps.weight*x), st
 end
 
-function Lux.apply(d::Classification{true}, x::AbstractMatrix, ps::NamedTuple, st::NamedTuple)
+function Lux.apply(d::Classification{true, true}, x::AbstractMatrix, ps::NamedTuple, st::NamedTuple)
     Lux.softmax(sum(ps.weight*x, dims=2)[:, 1]/size(x, 2) + ps.bias), st
 end
 
-function Lux.apply(d::Classification{false}, x::AbstractMatrix, ps::NamedTuple, st::NamedTuple)
-    Lux.softmax(sum(ps.weight*x, dims=2)[:, 1]/size(x, 2)), st
+function Lux.apply(d::Classification{true, false}, x::AbstractMatrix, ps::NamedTuple, st::NamedTuple)
+    Lux.softmax(ps.weight*(x[:,1]) + ps.bias), st
 end
 
-function Lux.apply(d::Classification{true}, x::AbstractArray{T, 3}, ps::NamedTuple, st::NamedTuple) where T
+function Lux.apply(d::Classification{false, false}, x::AbstractMatrix, ps::NamedTuple, st::NamedTuple)
+    Lux.softmax(ps.weight*(x[:,1])), st
+end
+
+function Lux.apply(d::Classification{true, true}, x::AbstractArray{T, 3}, ps::NamedTuple, st::NamedTuple) where T
     Lux.softmax(sum(mat_tensor_mul(ps.weight, x), dims=2)[:,1,:]/size(x, 2) .+ ps.bias), st
 end
 
-function Lux.apply(d::Classification{false}, x::AbstractArray{T, 3}, ps::NamedTuple, st::NamedTuple) where T
+function Lux.apply(d::Classification{false, true}, x::AbstractArray{T, 3}, ps::NamedTuple, st::NamedTuple) where T
     Lux.softmax(sum(mat_tensor_mul(ps.weight, x), dims=2)[:,1,:]/size(x, 2)), st
+end
+
+function Lux.apply(d::Classification{true, false}, x::AbstractArray{T, 3}, ps::NamedTuple, st::NamedTuple) where T
+    Lux.softmax(mat_tensor_mul(ps.weight, x[:,1,:]) .+ ps.bias), st
+end
+
+function Lux.apply(d::Classification{false, false}, x::AbstractArray{T, 3}, ps::NamedTuple, st::NamedTuple) where T
+    Lux.softmax(mat_tensor_mul(ps.weight, x[:,1,:])), st
 end
 
 (d::Classification)(x::AbstractArray, ps::NamedTuple, st::NamedTuple) = Lux.apply(d, x, ps, st)
