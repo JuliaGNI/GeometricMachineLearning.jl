6fec817d51bee92966f6503c95add3570843b79d
Theozeud <theozeud@gmail.com>
2023-07-18 16:53:06 +0200

    almost finished harmonic ensemble test

--- scripts/ensemblesolution/harmonic_oscillator.jl
+++ scripts/ensemblesolution/harmonic_oscillator.jl
@@ -1,43 +1,59 @@
 using GeometricMachineLearning
 using GeometricSolutions
+using GeometricEquations
 using Test
 
 using GeometricProblems.HarmonicOscillator
-using GeometricProblems.HarmonicOscillator: harmonic_oscillator_hode_ensemble
+using GeometricProblems.HarmonicOscillator: harmonic_oscillator_hode_ensemble, exact_solution, exact_solution_q, exact_solution_p
 
 
 #create the object ensemble_solution
-ensemble_solution = harmonic_oscillator_hode_ensemble()
+ensemble_problem = harmonic_oscillator_hode_ensemble()
+ensemble_solution =  EnsembleSolution(ensemble_problem)
+
+
+for i in eachindex(ensemble_solution.s)
+    ensemble_solution.s[i]= exact_solution(GeometricEquations.problem(ensemble_problem,i)) 
+end
+
 
 #create the data associated
-data_training = TrainingData(ensemble_solution)
+training_data = TrainingData(ensemble_solution)
 
-@test problem(training_data)               == UnknownProblem()
+@test GeometricMachineLearning.problem(training_data)               == ensemble_problem
 @test typeof(shape(training_data))         == TrajectoryData
 @test type(data_symbols(training_data))    == PhaseSpaceSymbol
 @test symbols(training_data)               == (:q,:p)
 @test dim(training_data)                   == 2
 @test noisemaker(training_data)            == NothingFunction()    
 
-@test get_Δt(training_data)                == 0.1
-@test get_nb_trajectory(training_data)     == 10
-@test get_length_trajectory(training_data) == 11
+#@test get_Δt(training_data)                == 0.1
+@test get_nb_trajectory(training_data)     == 100
+#@test get_length_trajectory(training_data) == 11
 @test get_nb_point(training_data)         === nothing
 
-@test Tuple(keys(GeometricMachineLearning.get(training_data))) ==(:q, :p)
+@test Tuple(keys(GeometricMachineLearning.get_data(training_data))) ==(:p, :q)
 
 #creating a training sets
-sympnet = GSympNet(dim(data_training); nhidden = 3)
-nruns = 1000
+sympnet = NeuralNetwork(GSympNet(dim(training_data); nhidden = 3))
+nruns = 10
 method = BasicSympNet()
 mopt = MomentumOptimizer()
-training_paramters = TrainingParameters(nruns, method, mopt)
+training_parameters = TrainingParameters(nruns, method, mopt)
 
 training_set = TrainingSet(sympnet, training_parameters, training_data)
 
 #training of the neural network
-neural_net_solution = train!(training_set)
+neural_net_solution = train!(training_set; showprogress = true)
 
+
+include("../plots.jl")
+
+H(x) = hamiltonian([x[2]],0.0, [x[1]],(k =0.5, ω = sqrt(0.5)))
+
+
+
+#plot_hnn(H, sympnet, loss(neural_net_solution); filename="harmonic_oscillator_ensemble.png", xmin=-1.2, xmax=+1.2, ymin=-1.2, ymax=+1.2)
 
 
 
