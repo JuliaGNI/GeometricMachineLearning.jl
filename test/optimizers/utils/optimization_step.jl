918da8671b8b15f8798d0cdea385032079a33580
benedict-96 <benedikt.brantner@icloud.com>
2023-07-29 13:18:30 +0200

    Got rid of the StiefelTestLayer. StiefelLayer does the same.

--- test/optimizers/utils/optimization_step.jl
+++ test/optimizers/utils/optimization_step.jl
@@ -1,44 +1,31 @@
-using GeometricMachineLearning
-using Test
-using LinearAlgebra
-import Lux
+using GeometricMachineLearning, Test, LinearAlgebra, KernelAbstractions
+using AbstractNeuralNetworks: AbstractExplicitLayer
+import GeometricMachineLearning: initialparameters
 import Random
 
-struct StiefelTestLayer <: Lux.AbstractExplicitLayer
-    N::Integer 
-    n::Integer
-end 
-
-function Lux.initialparameters(rng::Random.AbstractRNG, d::StiefelTestLayer)
-    (Y=rand(rng, StiefelManifold{Float32}, d.N, d.n), )
-end
-
-function GeometricMachineLearning.retraction(::StiefelTestLayer, B::NamedTuple)
-    GeometricMachineLearning.geodesic(B)
-end
-
-function optimization_step(N, n)
-    model = Lux.Chain(StiefelTestLayer(N, n), Lux.Dense(n, n, tanh))
-    ps, _ = Lux.setup(Random.default_rng(), model)
+function optimization_step_test(N, n, T)
+    model = Chain(StiefelLayer(N, n), Dense(N, N, tanh))
+    ps = initialparameters(KernelAbstractions.CPU(), T, model)
     # gradient 
-    dx = (layer_1=(Y=rand(Float32, N, n),), layer_2=(weight=rand(Float32, n, n), bias=rand(Float32, n, 1)))
+    dx = ((weight=rand(Float32, N, n),), (W=rand(Float32, N, N), b=rand(Float32, N)))
     m = AdamOptimizer()
     # randomize the cache!
     o = Optimizer(m, ps)
 
     ps2 = deepcopy(ps)
     optimization_step!(o, model, ps, dx)
-    @test typeof(ps.layer_1.Y) <: StiefelManifold
-    for key1 in keys(ps)
-        for key2 in keys(ps[key1])
-            @test norm(ps[key1][key2] - ps2[key1][key2]) > 1f-6
+    @test typeof(ps[1].weight) <: StiefelManifold
+    for (layers1, layers2) in zip(ps, ps2)
+        for key in keys(layers1)
+            @test norm(layers1[key] - layers2[key]) > T(1f-6)
         end
     end
 end
 
 N_max = 10
+T = Float32
 for N = 4:N_max
     for n = 1:N
-        optimization_step(N, n)
+        optimization_step_test(N, n, T)
     end
 end
