0b2568d19f6a44fa0657fbe14d878bb70661c0de
Theozeud <theozeud@gmail.com>
2023-08-03 16:19:31 +0200

    delete files

--- symbolic/symbolize_fun/est.jl
+++
@@ -1,3 +0,0 @@
-function (sinput, M_1, M_2, M_3, M_4, M_5, M_6)
-    (broadcast)(AbstractNeuralNetworks.IdentityActivation(), (*)(M_5, (broadcast)(tanh, (broadcast)(+, (*)(M_3, (broadcast)(tanh, (broadcast)(+, (*)(M_1, sinput), M_2))), M_4))))
-end
--- symbolic/symbolize_fun/field.jl
+++
@@ -1,5 +0,0 @@
-function (sinput, M_1, M_2, M_3, M_4)
-    begin
-        (SymbolicUtils.Code.create_array)(Array, nothing, Val{1}(), Val{(2,)}(), 0, 0)
-    end
-end
