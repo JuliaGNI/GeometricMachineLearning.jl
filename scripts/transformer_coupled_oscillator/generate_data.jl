3e99c491814e6bea44468fec24b24a48f1b82f60
benedict-96 <benedikt.brantner@icloud.com>
2023-08-09 17:24:55 +0200

    Added a warning because the data generation still doesn't work for Float32.

--- scripts/transformer_coupled_oscillator/generate_data.jl
+++ scripts/transformer_coupled_oscillator/generate_data.jl
@@ -1,4 +1,4 @@
-using GeometricIntegrators, KernelAbstractions, HDF5
+using GeometricIntegrators, KernelAbstractions, JLD2
 
 T = Float64
 
@@ -45,7 +45,8 @@ function assign_tensor(data_tensor, sols)
     assign_p!(data_tensor, sols, ndrange=dims)
 end
 
-function generate_data(params_collection=params_collection, initial_conditions=initial_conditions, t_integration=t_integration)
+function generate_data(params_collection=params_collection, initial_conditions=initial_conditions, t_integration=t_integration, Float32_not_working_yet=true)
+    Float32_not_working_yet ? (@warn "Float32 not used in integration!!") : nothing
     sols = []
     for params in params_collection
         for initial_conditions in initial_conditions_collection
@@ -58,8 +59,10 @@ function generate_data(params_collection=params_collection, initial_conditions=i
     time_steps = length(sols[1].q)
     data_tensor = zeros(4, length(sols), time_steps)
     assign_tensor(data_tensor, sols)
-    data_tensor
+    Float32_not_working_yet ? Float32.(data_tensor) : data_tensor
 end
 
-data = generate_data()
-#file = h5open("data", "w")
+data_tensor = generate_data()
+
+jldsave("data", tensor=data_tensor, params=params_collection, initial_conditions=initial_conditions_collection, 
+        time_interval=(0, t_integration), time_step=time_step)
