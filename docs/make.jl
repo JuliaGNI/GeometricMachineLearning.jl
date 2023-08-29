b8d38066618d84ab6c4616e569c6116d5d595a04
benedict-96 <benedikt.brantner@icloud.com>
2023-08-29 15:01:18 +0200

    Updated documentation to include new files.

--- docs/make.jl
+++ docs/make.jl
@@ -21,6 +21,17 @@ makedocs(;
             "Grassmann" => "manifolds/grassmann_manifold.md",
             "Stiefel" => "manifolds/stiefel_manifold.md",
             ],
+        "Arrays" => [
+            "Global Tangent Space" => "arrays/stiefel_lie_alg_horizontal.md",
+        ]
+        "Optimizers" => [
+            "Global Sections" => "manifold_related/global_sections.md",
+            "Retractions" => "manifold_related/retractions.md",
+            "Adam Optimizer" => "adam_optimizer.md",
+            ],
+        "Special Neural Network Layers" => [
+            "Attention" => "layers/attention_layer.md"
+        ],
         "Library" => "library.md",
     ],
 )
