297c9489ef5ae64f1931c45eb5a7b59d87e4c420
benedict-96 <benedikt.brantner@icloud.com>
2023-08-31 12:54:10 +0200

    Test that checks if attention and mha do the same thing.

---
+++ test/attention_layer/apply_multi_head_attention.jl
@@ -0,0 +1,23 @@
+using GeometricMachineLearning, Test 
+using NNlib: softmax
+using LinearAlgebra: I
+
+function compare_attention_to_mha(N, batch_size=10, T=Float32)
+    model₁ = MultiHeadAttention(N, 1, add_connection=false)
+    model₂ = Attention(N, softmax, add_connection=false)
+    model₃ = MultiHeadAttention(N, 1, add_connection=true)
+    model₄ = Attention(N, softmax, add_connection=true)
+
+    ps₂ = initialparameters(CPU(), T, model₂)
+    ps₁ = (PQ=(head_1=ps₂.PQ,), PK=(head_1=ps₂.PK,), PV=(head_1=typeof(ps₂.PK)(I(N)),))
+
+    mat = rand(T, N, N)
+    ten = rand(T, N, N, batch_size)
+    @test isapprox(model₁(mat, ps₁), model₂(mat, ps₂))
+    @test isapprox(model₁(ten, ps₁), model₂(ten, ps₂))
+
+    @test isapprox(model₃(mat, ps₁), model₄(mat, ps₂))
+    @test isapprox(model₃(ten, ps₁), model₄(ten, ps₂))
+end
+
+compare_attention_to_mha(10)
