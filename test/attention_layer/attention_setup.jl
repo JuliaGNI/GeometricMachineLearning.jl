e9f2428d003cb5d0d8077eda592c07d2e5c4a0ab
benedict-96 <benedikt.brantner@icloud.com>
2023-08-31 11:36:52 +0200

    Added new test for add_connection.}

--- test/attention_layer/attention_setup.jl
+++ test/attention_layer/attention_setup.jl
@@ -1,22 +1,31 @@
 using GeometricMachineLearning, Test
 using GeometricMachineLearning: upper_triangular_asymmetrize
+using GeometricMachineLearning: orthonormal_activation
 using LinearAlgebra: det
 
 function attention_tests(N, T=Float32)
     model₁ = Attention(N, Stiefel=false)
     model₂ = Attention(N, Stiefel=true)
+    model₃ = Attention(N, Stiefel=false, activation=orthonormal_activation)
+    # same as model₁, but with the add connection
+    model₄ = Attention(N, Stiefel=false, add_connection=true)
 
     ps₁ = initialparameters(CPU(), T, model₁)
     ps₂ = initialparameters(CPU(), T, model₂)
+    ps₃ = initialparameters(CPU(), T, model₃)
     @test typeof(ps₂.PQ) <: StiefelManifold 
-    @test typeof(ps₂.PQ) <: StiefelManifold 
+    @test typeof(ps₂.PK) <: StiefelManifold 
 
     A = randn(N, N)
     det₁ = det(A)
     det₂ = det(model₁(A, ps₁))
     det₃ = det(model₂(A, ps₂))
+    det₄ = det(model₃(A, ps₃))
     @test isapprox(det₁, det₂)
     @test isapprox(det₂, det₃)
+    @test isapprox(det₃, det₄)
+
+    @test isapprox(model₁(A, ps₁), model₄(A, ps₁))
 
     A = reshape(rand(SkewSymMatrix{T}, N), N, N, 1)
     @test isapprox(A, upper_triangular_asymmetrize(A))
