677dba4249a6ee05d038379d80b411f41b7dc6c5
benedict-96 <benedikt.brantner@icloud.com>
2023-09-03 21:00:13 +0100

    Cleaned up the routine calling the data loader.

--- src/data_loader/mnist_utils.jl
+++ src/data_loader/mnist_utils.jl
@@ -8,32 +8,33 @@ function onehotbatch(target::AbstractVector{T}) where {T<:Integer}
     output = KernelAbstractions.zeros(backend, T, 10, length(target))
     assign_val! = assign_val_kernel!(backend)
     assign_val!(output, target, ndrange=length(target))
+    reshape(output, 10, 1, length(target))
 end
 
-# second argumen pl is "patch length"
-# this splits the image into patches of size pl×pl and then arranges them into a matrix,
-# the columns of the matrix give the patch number.
-
-function flatten(image_patch::AbstractMatrix)
-    n, m = size(image_patch)
-    reshape(image_patch, n*m)
+"""
+Based on coordinates i,j this returns the batch index (for MNIST data set for now).
+"""
+function patch_index(i::T, j::T, patch_length::T, number_of_patches::T) where {T<:Integer}
+    opt_i = i % patch_length == 0 ? 1 : 0
+    opt_j = j % patch_length == 0 ? 1 : 0 
+    (j÷(patch_length)-opt_j)*(Int(√number_of_patches)) + (i÷(patch_length)-opt_i) + 1
 end
 
-function split_and_flatten(image::AbstractMatrix, pl)
-    n, m = size(image)
-    @assert n == m
-    @assert n%pl == 0
-    #square root of patch number
-    pnsq = n ÷ pl
-    hcat(Tuple(vcat(map(j -> map(i -> flatten(image[pl*(i-1)+1:pl*i,pl*(j-1)+1:pl*j,1]), 1:pnsq),1:pnsq)...))...)
+""" 
+Based on coordinates i,j this returns the index within the batch
+"""
+function within_patch_index(i::T, j::T, patch_length::T) where {T<:Integer}
+    opt_i = i % patch_length == 0 ? 1 : 0
+    opt_j = j % patch_length == 0 ? 1 : 0 
+    i_red, j_red = i%1 + opt_i, j%patch_length + opt_j
+    j_red*patch_length + i_red
 end
 
 @kernel function split_and_flatten_kernel!(output::AbstractArray{T, 3}, input::AbstractArray{T, 3}, patch_length::Integer, number_of_patches::Integer) where T
     i,j,k = @index(Global, NTuple)
-    patch_index = (j÷patch_length)*(Int(√number_of_patches)) + i÷patch_length + 1
-    within_patch_i = i%patch_length+1
-    within_patch_j = j%patch_length+1
-    output[within_patch_i+(within_patch_j-1)*patch_length,patch_index,k] = input[i,j,k]
+    patch_index₁ = patch_index(i, j, patch_length, number_of_patches)
+    patch_index₂ = within_patch_index(i, j, patch_length)
+    output[patch_index₂, patch_index₁, k] = input[i, j, k]
 end
 
 function split_and_flatten(input::AbstractArray{T, 3}, patch_length::Integer=7, number_of_patches::Integer=16) where T 
