5310d4339661d3e91361ab6e29dd34383b910036
benedict-96 <benedikt.brantner@icloud.com>
2023-09-03 20:59:13 +0100

    Fixed problems with initializing data loader.

--- src/data_loader/data_loader.jl
+++ src/data_loader/data_loader.jl
@@ -6,10 +6,11 @@ If the data loader is called with a single tensor, a batch_size and an output_si
 
 TODO: Implement DataLoader that works well with GeometricEnsembles etc. 
 """
-struct DataLoader{T, AT<:AbstractArray{T}}
+struct DataLoader{T, AT<:AbstractArray{T}, TT<:Union{AbstractArray, Nothing}, OT<:AbstractArray}
     data::AT
     batch::AT
-    output::AT
+    target_tensor::TT
+    output::OT
     sys_dim::Integer
     seq_length::Integer 
     batch_size::Integer
@@ -25,11 +26,29 @@ function DataLoader(data::AbstractArray{T, 3}, seq_length=10, batch_size=32, out
     batch = KernelAbstractions.allocate(backend, T, sys_dim, seq_length, batch_size)
     output = KernelAbstractions.allocate(backend, T, sys_dim, output_size, batch_size)
     draw_batch!(batch, output, data, seq_length, batch_size, output_size, n_params, n_time_steps)
-    DataLoader{T, typeof(data)}(data, batch, output, sys_dim, seq_length, batch_size, output_size, n_params, n_time_steps)
+    DataLoader{T, typeof(data), Nothing, typeof(output)}(data, batch, nothing, output, sys_dim, seq_length, batch_size, output_size, n_params, n_time_steps)
 end
 
-function redraw_batch(dl::DataLoader)
-    draw_batch!(dl.batch, dl.output, dl.data, dl.seq_length, dl.batch_size, dl.output_size, dl.n_params, dl.n_time_steps)
+# T and T1 are not the same because T1 is of Integer type
+function DataLoader(data::AbstractArray{T, 3}, target::AbstractVector{T1}, batch_size=32, patch_length=7) where {T, T1} 
+    @info "You provided a tensor and a vector as input. This will be treated as a classification problem. Tensor axes: (i) & (ii) image axes and (iii) batch dimesnion."
+    im_dim₁, im_dim₂, batch_size = size(data)
+    @assert length(target) == batch_size 
+    number_of_patches = (im_dim₁÷patch_length)*(im_dim₂÷patch_length) 
+    target = onehotbatch(target)
+    data_preprocessed = split_and_flatten(data, patch_length, number_of_patches)
+    backend = KernelAbstractions.get_backend(data)
+    batch_input = KernelAbstractions.allocate(backend, T, patch_length^2, number_of_patches, batch_size)
+    batch_output = KernelAbstractions.allocate(backend, T1, 10, 1, batch_size)
+    DataLoader{T, typeof(data), typeof(target), typeof(batch_output)}(data_preprocessed, batch_input, target, batch_output, patch_length^2, number_of_patches, batch_size, 1, length(target), number_of_patches)
+end
+
+function redraw_batch(dl::DataLoader{T, AT, BT}) where {T, AT<:AbstractArray{T}, BT<:AbstractArray{<:Integer}}
+    draw_batch!(dl.batch, dl.output, dl.data, dl.target_tensor, dl.batch_size, dl.n_params)
+end
+
+function redraw_batch(dl::DataLoader{T, AT, Nothing}) where {T, AT<:AbstractArray{T}}
+    draw_batch!(dl.batch, dl.output, dl.data, dl.target, dl.seq_length, dl.batch_size, dl.n_params)
 end
 
 function loss(model::Union{Chain, AbstractExplicitLayer}, ps::Union{Tuple, NamedTuple}, dl::DataLoader{T}) where T
