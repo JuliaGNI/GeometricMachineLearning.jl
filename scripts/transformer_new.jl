7276c268bcb3acda5b52aa3ea38405f77b9b74a6
benedict-96 <benedikt.brantner@icloud.com>
2023-09-04 12:41:49 +0100

    (Hopefully) finished updating script to new format.

--- scripts/transformer_new.jl
+++ scripts/transformer_new.jl
@@ -11,78 +11,53 @@ patch_length = 7
 n_heads = 7
 n_layers = 5
 patch_number = (image_dim÷patch_length)^2
+batch_size = 128
 
 train_x, train_y = MLDatasets.MNIST(split=:train)[:]
 test_x, test_y = MLDatasets.MNIST(split=:test)[:]
 
 # call data loader
-dl = DataLoader(train_x, train_y, batch_size=128)
+dl = DataLoader(train_x, train_y, batch_size=batch_size)
 dl_test = DataLoader(test_x, test_y, batch_size=length(test_y))
-
-# Implement classification layer!!!
 
 #encoder layer - final layer has to be added for evaluation purposes!
 Ψᵉ₁ = Lux.Chain(
     Transformer(patch_length^2, n_heads, n_layers, Stiefel=false),
-    Lux.Dense(patch_length^2, 10, Lux.σ, use_bias=false)
+    Classification(patch_length^2, 10, σ)
 )
 
 Ψᵉ₂ = Lux.Chain(
     #Embedding(patch_length^2, patch_number),
     Transformer(patch_length^2, n_heads, n_layers, Stiefel=true),
-    Lux.Dense(patch_length^2, 10, Lux.σ, use_bias=false)
+    Classification(patch_length^2, 10, σ)
 )
 
-function loss_sing(Ψᵉ, ps, st, x, y)
-    norm(sum(Lux.apply(Ψᵉ, x, ps, st)[1], dims=2)/size(x, 2) - y)
-end
 
-function loss_sing(Ψᵉ, ps, st, train_x, train_y, index)
-    loss_sing(Ψᵉ, ps, st, train_x[index], train_y[index])    
-end
-    
-function full_loss(Ψᵉ, ps, st, train_x, train_y)
-    num = length(train_x)
-    mapreduce(index -> loss_sing(Ψᵉ, ps, st, train_x, train_y, index), +, 1:num)    
-end
-
-
-#err_freq is the frequency with which the error is computed (e.g. every 100 steps)
-function transformer_training(Ψᵉ::Lux.Chain, batch_size=64, training_steps=10000, o=AdamOptimizer())
-    ps, st = Lux.setup(CUDA.device(), Random.default_rng(), Ψᵉ) 
-
-    num = length(train_x)
+# err_freq is the frequency with which the error is computed (e.g. every 100 steps)
+function transformer_training(Ψᵉ::Lux.Chain; backend=CPU(), training_steps=10000, o=AdamOptimizer())
+    ps = initialparameters(backend, eltype(dl.data), Ψᵉ) 
 
     optimizer_instance = Optimizer(o, ps)
 
-    println("initial test loss: ", full_loss(Ψᵉ, ps, st, test_x, test_y)/length(test_x), "\n")
+    draw_batch!(dl_test)
+    println("initial test loss: ", loss(Ψᵉ, ps, dl_test), "\n")
 
     @showprogress "Training network ..." for i in 1:training_steps
-        index₁ = Int(ceil(rand()*num))
-        x = train_x[index₁]
-        y = train_y[index₁] 
-        l, pb = Zygote.pullback(ps -> loss_sing(Ψᵉ, ps, st, x, y), ps)
+        draw_batch!(dl)
+        l, pb = Zygote.pullback(ps -> loss(Ψᵉ, ps, dl), ps)
         dp = pb(one(l))[1]
-
-        indices = Int.(ceil.(rand(batch_size -1)*num))
-        for index in indices
-            x = train_x[index] 
-            y = train_y[index] 
-            l, pb = Zygote.pullback(ps -> loss_sing(Ψᵉ, ps, st, x, y), ps)
-            dp = _add(dp, pb(one(l))[1])
-        end
 
         optimization_step!(optimizer_instance, Ψᵉ, ps, dp)    
     end
 
-    println("final test loss: ", full_loss(Ψᵉ, ps, st, test_x, test_y)/length(test_x), "\n")
+    println("final test loss: ", loss(Ψᵉ, ps, dl_test), "\n")
 
     ps
 end
 
-batch_size = 100
-epochs = 10
-training_steps = Int(ceil(length(train_x)*epochs/batch_size))
+n_epochs = 10
+# calculate number of epochs
+training_steps = Int(ceil(length(train_x)*n_epochs/batch_size))
 
 ps₁ = transformer_training(Ψᵉ₁, batch_size, training_steps, AdamOptimizer())
 ps₂ = transformer_training(Ψᵉ₂, batch_size, training_steps, AdamOptimizer())
