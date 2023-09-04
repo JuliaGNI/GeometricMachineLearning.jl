a1ca3c4f50d8b14d6cdcda9cf001ab64c175043b
benedict-96 <benedikt.brantner@icloud.com>
2023-09-04 22:42:23 +0100

    Made script work with new layer setup and DataLoader.

--- scripts/transformer_new.jl
+++ scripts/transformer_new.jl
@@ -3,51 +3,57 @@ TODO: Add a better predictor at the end! It should set the biggest value of the 
 """
 
 using GeometricMachineLearning, LinearAlgebra, ProgressMeter, Plots, CUDA
+using AbstractNeuralNetworks
 import Zygote, MLDatasets
+
+# remove this after AbstractNeuralNetworks PR has been merged 
+GeometricMachineLearning.Chain(model::Chain, d::AbstractNeuralNetworks.AbstractExplicitLayer) = Chain(model.layers..., d)
 
 # MNIST images are 28×28, so a sequence_length of 16 = 4² means the image patches are of size 7² = 49
 image_dim = 28
 patch_length = 7
 n_heads = 7
-n_layers = 5
+n_layers = 10
 patch_number = (image_dim÷patch_length)^2
 batch_size = 128
 
 train_x, train_y = MLDatasets.MNIST(split=:train)[:]
 test_x, test_y = MLDatasets.MNIST(split=:test)[:]
 
-# call data loader
-dl = DataLoader(train_x, train_y, batch_size=batch_size)
-dl_test = DataLoader(test_x, test_y, batch_size=length(test_y))
-
 #encoder layer - final layer has to be added for evaluation purposes!
-Ψᵉ₁ = Lux.Chain(
+Ψᵉ₁ = Chain(
     Transformer(patch_length^2, n_heads, n_layers, Stiefel=false),
-    Classification(patch_length^2, 10, σ)
+    Classification(patch_length^2, 10, softmax)
 )
 
-Ψᵉ₂ = Lux.Chain(
+Ψᵉ₂ = Chain(
     #Embedding(patch_length^2, patch_number),
     Transformer(patch_length^2, n_heads, n_layers, Stiefel=true),
-    Classification(patch_length^2, 10, σ)
+    Classification(patch_length^2, 10, softmax)
 )
 
 
 # err_freq is the frequency with which the error is computed (e.g. every 100 steps)
-function transformer_training(Ψᵉ::Lux.Chain; backend=CPU(), training_steps=10000, o=AdamOptimizer())
+function transformer_training(Ψᵉ::Chain; backend=CPU(), n_training_steps=10000, o=AdamOptimizer())
+    # call data loader
+    dl = DataLoader(train_x, train_y, batch_size=batch_size)
+    dl_test = DataLoader(test_x, test_y, batch_size=length(test_y))
+
     ps = initialparameters(backend, eltype(dl.data), Ψᵉ) 
 
     optimizer_instance = Optimizer(o, ps)
 
-    draw_batch!(dl_test)
     println("initial test loss: ", loss(Ψᵉ, ps, dl_test), "\n")
 
-    @showprogress "Training network ..." for i in 1:training_steps
-        draw_batch!(dl)
-        l, pb = Zygote.pullback(ps -> loss(Ψᵉ, ps, dl), ps)
-        dp = pb(one(l))[1]
+    progress_object = Progress(n_training_steps; enabled=true)
 
-        optimization_step!(optimizer_instance, Ψᵉ, ps, dp)    
+    for i in 1:n_training_steps
+        redraw_batch!(dl)
+        loss_val, pb = Zygote.pullback(ps -> loss(Ψᵉ, ps, dl), ps)
+        dp = pb(one(loss_val))[1]
+
+        optimization_step!(optimizer_instance, Ψᵉ, ps, dp)
+        ProgressMeter.next!(progress_object; showvalues = [(:TrainingLoss, loss_val)])   
     end
 
     println("final test loss: ", loss(Ψᵉ, ps, dl_test), "\n")
@@ -55,12 +61,12 @@ function transformer_training(Ψᵉ::Lux.Chain; backend=CPU(), training_steps=10
     ps
 end
 
-n_epochs = 10
+n_epochs = 3
 # calculate number of epochs
-training_steps = Int(ceil(length(train_x)*n_epochs/batch_size))
+n_training_steps = Int(ceil(length(train_y)*n_epochs/batch_size))
 
-ps₁ = transformer_training(Ψᵉ₁, batch_size, training_steps, AdamOptimizer())
-ps₂ = transformer_training(Ψᵉ₂, batch_size, training_steps, AdamOptimizer())
+ps₁ = transformer_training(Ψᵉ₁, n_training_steps=n_training_steps)
+ps₂ = transformer_training(Ψᵉ₂, n_training_steps=n_training_steps)
 
 #loss_array₃ = transformer_training(Ψᵉ₂, batch_size, training_steps, err_freq, StandardOptimizer(0.001))
 #loss_array₄ = transformer_training(Ψᵉ₂, batch_size, training_steps, err_freq, MomentumOptimizer(0.001, 0.5))
