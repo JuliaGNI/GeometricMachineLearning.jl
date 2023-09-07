7d47d8c085e5a31681d4e8c140482d9daac9bdf8
benedict-96 <benedikt.brantner@icloud.com>
2023-09-07 10:00:24 +0100

    Testing for both classification options now.

--- test/layers/classification.jl
+++ test/layers/classification.jl
@@ -1,7 +1,7 @@
 using GeometricMachineLearning, Test 
 
-function test_set_up_and_application(T=Float32, sys_dim=49, output_dim=10, seq_length=16, batch_size=32)
-    d = Classification(sys_dim, output_dim, σ)
+function test_set_up_and_application(T=Float32, sys_dim=49, output_dim=10, seq_length=16, batch_size=32; average=false)
+    d = Classification(sys_dim, output_dim, σ, average=average)
     ps = initialparameters(CPU(), T, d)
     output₁ = d(rand(T, sys_dim, seq_length), ps)
     output₂ = d(rand(T, sys_dim, seq_length, batch_size), ps)
@@ -9,5 +9,5 @@ function test_set_up_and_application(T=Float32, sys_dim=49, output_dim=10, seq_l
     @test size(output₂) == (10, 1, batch_size)
 end
 
-test_set_up_and_application()
-    
+test_set_up_and_application(average=false)
+test_set_up_and_application(average=true)
