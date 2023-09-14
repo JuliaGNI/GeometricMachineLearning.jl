2c4a71b2f360c3a8e285d461770123655a957118
benedict-96 <benedikt.brantner@icloud.com>
2023-09-14 13:56:44 +0200

    Added routine for assigning a batch in the amtrix case.

---
+++ src/data_loader/matrix_assign.jl
@@ -0,0 +1,9 @@
+"""
+This assigns the batch if the data are in form of a matrix.
+"""
+function draw_batch!(batch::AbstractMatrix{T}, data::AbstractMatrix{T}) where T
+    sys_dim, batch_size = size(batch)
+    n_params = size(data, 2)
+    param_indices = Int.(ceil.(rand(T, batch_size)*n_params))
+    batch .= data[:, param_indices]
+end
