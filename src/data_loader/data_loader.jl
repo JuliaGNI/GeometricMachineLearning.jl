b84b8ba16e65d5676bbd0b19c4e850ab7785d9fc
benedict-96 <benedikt.brantner@icloud.com>
2023-09-14 13:57:22 +0200

    Added new constructor for matrix input.

--- src/data_loader/data_loader.jl
+++ src/data_loader/data_loader.jl
@@ -1,39 +1,39 @@
 """
-Data Loader is a struct that creates an instance based on a tensor (or different input format) and is designed to make training convenient.
-
-Implemented: 
-If the data loader is called with a single tensor, a batch_size and an output_size, then the batch is drawn randomly in the relevant range and the output is assigned accordingly.
-
-The fields of the struct are the following: 
-    - data: The input data with axes (i) system dimension, (ii) number of parameters and (iii) number of time steps.
-    - batch: A tensor in which the current batch is stored. 
-    - target_tensor: A tensor in which the current target is stored. 
-    - output: The tensor from which the output is drawn - this may be of type Nothing if the constructor is only called with one tensor.
-    - sys_dim: The ``dimension'' of the system, i.e. what is taken as input by a regular neural network. 
-    - seq_length: The length batches should have. 
-    - batch_size: 
-    - output_size: The size of the second axis of the output tensor (prediction_window, output_size=1 in most cases)
-    - n_params: The number of parameters that are present in the data set (length of second axis). 
-    - n_time_steps: Number of time steps (length of third axis).
-
-For drawing the batch, the sampling is done over n_params and n_time_steps (here seq_length and output_size are also taken into account).
-
-TODO: Implement DataLoader that works well with GeometricEnsembles etc. 
+Data Loader is a struct that creates an instance based on a tensor (or different input format) and is designed to make training convenient. \n
+\n
+Implemented: \n
+If the data loader is called with a single tensor, a batch_size and an output_size, then the batch is drawn randomly in the relevant range and the output is assigned accordingly.\n
+\n
+The fields of the struct are the following: \n
+\t   - data: The input data with axes (i) system dimension, (ii) number of parameters and (iii) number of time steps.\n
+\t   - batch: A tensor in which the current batch is stored.\n 
+\t   - target_tensor: A tensor in which the current target is stored.\n
+\t   - output: The tensor from which the output is drawn - this may be of type Nothing if the constructor is only called with one tensor.\n
+\t   - sys_dim: The ``dimension'' of the system, i.e. what is taken as input by a regular neural network.\n 
+\t   - seq_length: The length batches should have.\n
+\t   - batch_size:\n
+\t   - output_size: The size of the second axis of the output tensor (prediction_window, output_size=1 in most cases)\n
+\t   - n_params: The number of parameters that are present in the data set (length of second axis)\n
+\t   - n_time_steps: Number of time steps (length of third axis)\n
+\n
+For drawing the batch, the sampling is done over n_params and n_time_steps (here seq_length and output_size are also taken into account).\n
+\n
+TODO: Implement DataLoader that works well with GeometricEnsembles etc.\n
 """
-struct DataLoader{T, AT<:AbstractArray{T}, TT<:Union{AbstractArray, Nothing}, OT<:AbstractArray}
+struct DataLoader{T, AT<:AbstractArray{T}, TT<:Union{AbstractArray, Nothing}, OT<:Union{AbstractArray, Nothing}}
     data::AT
     batch::AT
     target_tensor::TT
     output::OT
     sys_dim::Integer
-    seq_length::Integer 
+    seq_length::Union{Integer, Nothing} 
     batch_size::Integer
-    output_size::Integer 
+    output_size::Union{Integer, Nothing} 
     n_params::Integer 
-    n_time_steps::Integer 
+    n_time_steps::Union{Integer, Nothing}
 end
 
-function DataLoader(data::AbstractArray{T, 3}, seq_length=10, batch_size=32, output_size=1) where T
+function DataLoader(data::AbstractArray{T, 3}; seq_length=10, batch_size=32, output_size=1) where T
     @info "You have provided a tensor with three axes as input. They will be interpreted as \n (i) system dimension, (ii) number of parameters and (iii) number of time steps."
     sys_dim,n_params,n_time_steps = size(data)
     backend = KernelAbstractions.get_backend(data)
@@ -41,6 +41,15 @@ function DataLoader(data::AbstractArray{T, 3}, seq_length=10, batch_size=32, out
     output = KernelAbstractions.allocate(backend, T, sys_dim, output_size, batch_size)
     draw_batch!(batch, output, data)
     DataLoader{T, typeof(data), Nothing, typeof(output)}(data, batch, nothing, output, sys_dim, seq_length, batch_size, output_size, n_params, n_time_steps)
+end
+
+function DataLoader(data::AbstractMatrix{T}; batch_size=32) where T 
+    @info "You have provided a matrix as input. The axes will be interpreted as (i) system dimension and (ii) number of parameters."
+    sys_dim, n_params = size(data)
+    backend = KernelAbstractions.get_backend(data)
+    batch = KernelAbstractions.allocate(backend, T, sys_dim, batch_size)
+    draw_batch!(batch, data)
+    DataLoader{T, typeof(data), Nothing, Nothing}(data, batch, nothing, nothing, sys_dim, nothing, batch_size, nothing, n_params, nothing)
 end
 
 # T and T1 are not the same because T1 is of Integer type
