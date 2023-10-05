56c48312bafd4c0ead659fec72d5692c150c197c
benedict-96 <benedikt.brantner@icloud.com>
2023-10-05 12:10:19 +0800

    Made script work and changed architecture of autoencoder.

--- scripts/symplectic_autoencoders/training.jl
+++ scripts/symplectic_autoencoders/training.jl
@@ -17,8 +17,6 @@ include("initial_condition.jl")
 
 T = Float64
 #μ_collection=T(5/12):T(.1):T(5/6)
-n = 5
-n_epochs = 500
 
 backend, data = 
 try 
@@ -36,9 +34,56 @@ function get_psd_encoder_decoder(; n=5)
     Φ = svd(hcat(data[1:N,:], data[(N+1):2*N,:])).U[:,1:n]
     PSD = hcat(vcat(Φ, zero(Φ)), vcat(zero(Φ), Φ))
 
+    PSD_cpu = _cpu_convert(PSD)
     psd_encoder(z) = PSD_cpu'*z 
     psd_decoder(ξ) = PSD_cpu*ξ
     psd_encoder, psd_decoder
+end
+
+function get_nn_encoder_decoder(; n=5, n_epochs=500, activation=tanh, opt=AdamOptimizer(), T=T)
+    Ψᵉ = Chain(
+        GradientQ(2*N, 10*N, activation), 
+        GradientP(2*N, 10*N, activation),
+        GradientQ(2*N, 10*N, activation), 
+        GradientP(2*N, 10*N, activation),
+        PSDLayer(2*N, 2*n)
+    )
+
+    Ψᵈ = Chain(
+        GradientQ(2*n, 10*n, activation), 
+        GradientP(2*n, 10*n, activation),
+        PSDLayer(2*n, 2*N),
+        GradientQ(2*N, 2*N, activation)
+        )
+    model = Chain(  
+                    Ψᵉ.layers..., 
+                    Ψᵈ.layers...
+    )
+
+    ps = initialparameters(backend, T, model)
+    loss(model, ps, dl)
+
+    optimizer_instance = Optimizer(opt, ps)
+    n_training_iterations = Int(ceil(n_epochs*dl.n_params/dl.batch_size))
+    progress_object = Progress(n_training_iterations; enabled=true)
+
+    for _ in 1:n_training_iterations
+        redraw_batch!(dl)
+        loss_val, pb = Zygote.pullback(ps -> loss(model, ps, dl), ps)
+        dp = pb(one(loss_val))[1]
+
+        optimization_step!(optimizer_instance, model, ps, dp)
+        ProgressMeter.next!(progress_object; showvalues=[(:TrainingLoss, loss_val)])
+    end
+
+    n_layers = length(model.layers)
+    psᵉ = _cpu_convert(ps[1:length(Ψᵉ.layers)])
+    psᵈ = _cpu_convert(ps[(length(Ψᵉ.layers)+1):end])   
+
+    nn_encoder(z) = Ψᵉ(z, psᵉ)
+    nn_decoder(ξ) = Ψᵈ(ξ, psᵈ)
+
+    nn_encoder, nn_decoder
 end
 
 function _cpu_convert(ps::Tuple)
@@ -59,51 +104,6 @@ end
 
 _cpu_convert(A::AbstractArray) = Array(A)
 
-function get_nn_encoder_decoder(; n=5, n_epcohs=500, activation=tanh, opt=AdamOptimizer(), T=T)
-    model = Chain(  GradientQ(2*N, 2*N, activation), 
-                    GradientP(2*N, 2*N, activation),
-                    PSDLayer(2*N, 2*n),
-                    PSDLayer(2*n, 2*N),
-                    GradientQ(2*N, 2*N, activation),
-                    GradientP(2*N, 2*N, activation)
-    )
-
-    ps = initialparameters(backend, T, model)
-    loss(model, ps, dl)
-
-    optimizer_instance = Optimizer(opt, ps)
-    n_training_iterations = Int(ceil(n_epochs*dl.n_params/dl.batch_size))
-    progress_object = Progress(n_training_iterations; enabled=true)
-
-    for _ in 1:n_training_iterations
-        redraw_batch!(dl)
-        loss_val, pb = Zygote.pullback(ps -> loss(model, ps, dl), ps)
-        dp = pb(one(loss_val))[1]
-
-        optimization_step!(optimizer_instance, model, ps, dp)
-        ProgressMeter.next!(progress_object; showvalues=[(:TrainingLoss, loss_val)])
-    end
-
-    Ψᵉ = Chain(model.layers[1:3]...)
-    psᵉ = _cpu_convert(ps[1:3])
-    Ψᵈ = Chain(model.layers[4:end]...)
-    psᵈ = _cpu_convert(ps[4:end])   
-
-    PSD_cpu = _cpu_convert(PSD)
-
-    nn_encoder(z) = Ψᵉ(z, psᵉ)
-    nn_decoder(ξ) = Ψᵈ(ξ, psᵈ)
-
-    nn_encoder, nn_decoder    
-end
-
-
-data_cpu = _cpu_convert(data)
-psd_error = norm(data_cpu - psd_decoder(psd_encoder(data_cpu)))/norm(data_cpu)
-nn_error = norm(data_cpu - nn_decoder(nn_encoder(data_cpu)))/norm(data_cpu)
-
-μ_test_vals = (T(0.51), T(0.625), T(0.74))
-
 # make sure you also store tspan and tstep in the future (in the HDF5 file!!!)
 function reduced_systems_for_wave_equation(μ_val, Ñ=(N-2), n=n, n_time_steps=n_time_steps; T=Float64, integrator=ImplicitMidpoint(), system_type=GeometricMachineLearning.Symplectic())
     params = (μ=μ_val, Ñ=Ñ, Δx=T(1/(Ñ-1)))
@@ -118,7 +118,7 @@ function reduced_systems_for_wave_equation(μ_val, Ñ=(N-2), n=n, n_time_steps=
     nn_rs, psd_rs
 end
 
-function compute_reduction_errors(μ_val=T(0.51), n_time_steps=size(data,2)/8)
+function compute_reduction_errors(μ_val=T(0.51), n_time_steps=size(data,2)/8; n=5)
     nn_rs, psd_rs = reduced_systems_for_wave_equation(μ_val, N-2, n, n_time_steps)
     (psd=compute_reduction_error(psd_rs), nn=compute_reduction_error(nn_rs))
 end
@@ -143,3 +143,16 @@ function plot_comparison_for_reconstructed_trajectories(trajectories, t_step=0)
     plot!(plot_object, trajectories.nn.q[t_step_index][1:N], label="NN")
     png(plot_object, "plots/comparison_for_time_step_"*string(t_step))
 end
+
+
+psd_encoder, psd_decoder = get_psd_encoder_decoder(n=5)
+nn_encoder, nn_decoder = get_nn_encoder_decoder(n=5, n_epochs=2000)
+
+data_cpu = _cpu_convert(data)
+
+psd_error = norm(data_cpu - psd_decoder(psd_encoder(data_cpu)))/norm(data_cpu)
+nn_error = norm(data_cpu - nn_decoder(nn_encoder(data_cpu)))/norm(data_cpu)
+
+μ_test_vals = (T(0.51), T(0.625), T(0.74))
+
+reduction_errors = compute_reduction_errors(n=5)
