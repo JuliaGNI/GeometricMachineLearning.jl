901593c7b0fb028241c100a28f960ebe4ccdae84
benedict-96 <benedikt.brantner@icloud.com>
2023-10-09 12:32:48 +0800

    Changed initial condition to be consistent with weakly symplectic paper.

--- scripts/symplectic_autoencoders/initial_condition.jl
+++ scripts/symplectic_autoencoders/initial_condition.jl
@@ -31,10 +31,24 @@ function u₀(ξ::AbstractVector{T}, μ::T) where T
     h.(s(ξ, μ))
 end
 
+function p₀(ξ::T, μ::T) where T 
+    - μ * ForwardDiff.derivative(ξ -> u₀(ξ, μ), ξ)
+end
+
+function p₀(ξ::AbstractVector{T}, μ::T) where T 
+    p₀_closure(ξ) = p₀(ξ, μ)
+    p₀_closure.(ξ)
+end
+
 function get_initial_condition(μ::T, Δx::T) where T
     Ω = T(-.5):Δx:T(.5)
-    (q=OffsetArray(u₀(Ω,μ), OffsetArrays.Origin(0)), p=OffsetArray(p(T(0),Ω,μ), OffsetArrays.Origin(0)))
+    (q=OffsetArray(u₀(Ω,μ), OffsetArrays.Origin(0)), p=OffsetArray(p₀(Ω,μ), OffsetArrays.Origin(0)))
 end
+
+function get_initial_condition2(μ::T, Δx::T) where T 
+    Ω = T(-.5):Δx:T(0.5)
+    (q=OffsetArray(u₀(Ω,μ), OffsetArrays.Origin(0)), p=OffsetArray(zero(Ω), OffsetArrays.Origin(0)))
+end 
 
 """
 If you call this function with a Float and an Integer, then the integer will be interpreted as the number of nodes, i.e. degrees of freedom.
@@ -43,7 +57,16 @@ function get_initial_condition(μ::T, Ñ::Integer) where T
     get_initial_condition(μ, T(1/(Ñ+1)))
 end
 
+function get_initial_condition2(μ::T, Ñ::Integer) where T 
+    get_initial_condition2(μ, T(1/(Ñ+1)))
+end
+
 function get_initial_condition_vector(μ::T, Ñ::Integer) where T 
     ics_offset = get_initial_condition(μ, Ñ)
+    vcat(ics_offset.q.parent, ics_offset.p.parent)
+end
+
+function get_initial_condition_vector2(μ::T, Ñ::Integer) where T
+    ics_offset = get_initial_condition2(μ, Ñ)
     vcat(ics_offset.q.parent, ics_offset.p.parent)
 end
