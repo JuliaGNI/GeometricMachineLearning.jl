e266bb14500cd7c4ea0e9d6cd990332d1244a915
benedict-96 <benedikt.brantner@icloud.com>
2023-10-20 12:56:01 +0800

    Now not exporting loss anymore, has to be taken care of in the test!

--- test/data_loader/data_loader.jl
+++ test/data_loader/data_loader.jl
@@ -3,14 +3,11 @@ using GeometricMachineLearning, Test, Zygote
 function test_data_loader(sys_dim, n_time_steps, n_params, T=Float32)
     data = randn(T, sys_dim, n_time_steps, n_params)
     dl = DataLoader(data)
-    dl_copy = deepcopy(dl)
-    redraw_batch!(dl)
-    @test dl !== dl_copy
 
     # first argument is sys_dim, second is number of heads, third is number of units
     model = Transformer(dl.sys_dim, 2, 1)
     ps = initialparameters(CPU(), T, model)
-    dx = Zygote.gradient(ps -> loss(model, ps, dl), ps)[1]
+    dx = Zygote.gradient(ps -> GeometricMachineLearning.loss(model, ps, dl), ps)[1]
     ps_copy = deepcopy(ps)
     o = Optimizer(GradientOptimizer(), ps)
     optimization_step!(o, model, ps, dx)
