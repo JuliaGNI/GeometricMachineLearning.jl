366231a5583a604cc8fcabdd4e6251fd5c2583ac
benedict-96 <benedikt.brantner@icloud.com>
2023-11-22 14:59:42 +0100

    Updated SympNetLayers to save an extra struct and an abstract type. Stream-lined all the array routines.

--- src/layers/sympnets.jl
+++ src/layers/sympnets.jl
@@ -6,11 +6,8 @@ For the linear layer, the activation and the bias are left out, and for the acti
 abstract type SympNetLayer{M, N} <: AbstractExplicitLayer{M, N} end
 
 @doc raw"""
-Super type of `GradientQ` and `GradientP`.
+`GradientLayer` is the `struct` corresponding to the constructors `GradientLayerQ` and `GradientLayerP`. See those for more information.
 """ 
-abstract type Gradient{M, N, TA} <: SympNetLayer{M, N} end
-
-############ new layout, saves a struct!!! 
 struct GradientLayer{M, N, TA, C} <: SympNetLayer{M, N}
         second_dim::Integer
         activation::TA
@@ -19,15 +16,24 @@ end
 const GradientLayerQ{M, N, TA} = GradientLayer{M, N, TA, :Q}
 const GradientLayerP{M, N, TA} = GradientLayer{M, N, TA, :P}
 
-function GradientLayerQ(M, upscaling_dimension, activation)
-        GradientLayer{M, M, typeof(activation), :Q}(upscaling_dimension, activation)
+@doc raw"""
+`LinearLayer` is the `struct` corresponding to the constructors `LinearLayerQ` and `LinearLayerP`. See those for more information.
+"""
+struct LinearLayer{M, N, C} <: SympNetLayer{M, N}
 end
 
-function GradientLayerP(M, upscaling_dimension, activation)
-        GradientLayer{M, M, typeof(activation), :P}(upscaling_dimension, activation)
+const LinearLayerQ{M, N, TA} = LinearLayer{M, N, :Q}
+const LinearLayerP{M, N, TA} = LinearLayer{M, N, :P}
+
+@doc raw"""
+`ActivationLayer` is the `struct` corresponding to the constructors `ActivationLayerQ` and `ActivationLayerP`. See those for more information.
+"""
+struct  ActivationLayer{M, N, TA, C} <: SympNetLayer{M, N}
+        activation::TA
 end
 
-
+const ActivationLayerQ{M, N, TA} = ActivationLayer{M, N, TA, :Q}
+const ActivationLayerP{M, N, TA} = ActivationLayer{M, N, TA, :P}
 
 @doc raw"""
 The gradient layer that changes the $q$ component. It is of the form: 
@@ -40,13 +46,8 @@ The gradient layer that changes the $q$ component. It is of the form:
 
 with $V(p) = \sum_{i=1}^Ma_i\Sigma(\sum_jk_{ij}p_j+b_i)$, where $\Sigma$ is the antiderivative of the activation function $\sigma$ (one-layer neural network). We refer to $M$ as the *upscaling dimension*. Such layers are by construction symplectic.
 """
-struct GradientQ{M, N, TA} <: Gradient{M, N, TA}
-        second_dim::Integer
-        activation::TA
-end
-
-function GradientQ(M, upscaling_dimension, activation)
-        GradientQ{M, M, typeof(activation)}(upscaling_dimension, activation)
+function GradientLayerQ(M, upscaling_dimension, activation)
+        GradientLayer{M, M, typeof(activation), :Q}(upscaling_dimension, activation)
 end
 
 @doc raw"""
@@ -60,19 +61,9 @@ The gradient layer that changes the $q$ component. It is of the form:
 
 with $V(p) = \sum_{i=1}^Ma_i\Sigma(\sum_jk_{ij}p_j+b_i)$, where $\Sigma$ is the antiderivative of the activation function $\sigma$ (one-layer neural network). We refer to $M$ as the *upscaling dimension*. Such layers are by construction symplectic.
 """
-struct GradientP{M, N, TA} <: Gradient{M, N, TA}
-        second_dim::Integer
-        activation::TA
+function GradientLayerP(M, upscaling_dimension, activation)
+        GradientLayer{M, M, typeof(activation), :P}(upscaling_dimension, activation)
 end
-
-function GradientP(M, upscaling_dimension, activation)
-        GradientP{M, M, typeof(activation)}(upscaling_dimension, activation)
-end
-
-@doc raw"""
-Super type of `ActivationQ` and `ActivationP`.
-"""
-abstract type Activation{M, N, TA} <: SympNetLayer{M, N} end
 
 @doc raw"""
 Performs:
@@ -87,11 +78,8 @@ Performs:
 ```
 
 """
-struct ActivationQ{M, N, TA} <: Activation{M, N, TA} 
-        activation::TA
-end
-function ActivationQ(M, activation)
-        ActivationQ{M, M, typeof(activation)}(activation)
+function ActivationLayerQ(M, activation)
+        ActivationLayerQ{M, M, typeof(activation)}(activation)
 end
 
 @doc raw"""
@@ -107,17 +95,10 @@ Performs:
 ```
 
 """
-struct ActivationP{M, N, TA} <: Activation{M, N, TA} 
-        activation::TA
-end
-function ActivationP(M, activation)
-        ActivationP{M, M, typeof(activation)}(activation)
+function ActivationLayerP(M, activation)
+        ActivationLayerP{M, M, typeof(activation)}(activation)
 end
 
-@doc raw"""
-Super type of `LinearQ` and `LinearP`.
-"""
-abstract type LinearSympNetLayer{M, N} <: SympNetLayer{M, N} end
 
 @doc raw"""
 Equivalent to a left multiplication by the matrix:
@@ -129,11 +110,8 @@ Equivalent to a left multiplication by the matrix:
 ```
 where $B$ is a symmetric matrix.
 """
-struct LinearQ{M, N} <: LinearSympNetLayer{M, N} 
-end 
-
-function LinearQ(M)
-        LinearQ{M, M}()
+function LinearLayerQ(M)
+        LinearLayerQ{M, M}()
 end
 
 @doc raw"""
@@ -146,15 +124,14 @@ B & \mathbb{I}
 ```
 where $B$ is a symmetric matrix.
 """
-struct LinearP{M, N} <: LinearSympNetLayer{M, N}
-end 
-
-function LinearP(M)
-        LinearP{M, M}()
+function LinearLayerP(M)
+        LinearLayerP{M, M}()
 end
 
-"""
-This is an old constructor and will be depricated. For `change_q=true` it is equivalent to `GradientQ`; for `change_q=false` it is equivalent to `GradientP`.
+@doc raw"""
+This is an old constructor and will be depricated. For `change_q=true` it is equivalent to `GradientLayerQ`; for `change_q=false` it is equivalent to `GradientLayerP`.
+
+If `full_grad=false` then `ActivationLayer` is called
 """
 function Gradient(dim::Int, dim2::Int=dim, activation = identity; full_grad::Bool=true, change_q::Bool=true, allow_fast_activation::Bool=true)
         @warn "You are calling the old constructor. This will be deprecated."
@@ -165,13 +142,13 @@ function Gradient(dim::Int, dim2::Int=dim, activation = identity; full_grad::Boo
         dim2 ≥ dim || error("Second dimension should be bigger than the first!")
 
         if change_q
-                return full_grad ? GradientQ{dim, dim, typeof(activation)}(dim2, activation) : ActivationQ{dim, dim, typeof(activation)}(activation)
+                return full_grad ? GradientLayerQ{dim, dim, typeof(activation)}(dim2, activation) : ActivationLayerQ{dim, dim, typeof(activation)}(activation)
         else
-                return full_grad ? GradientP{dim, dim, typeof(activation)}(dim2, activation) : ActivationP{dim, dim, typeof(activation)}(activation)
+                return full_grad ? GradientLayerP{dim, dim, typeof(activation)}(dim2, activation) : ActivationLayerP{dim, dim, typeof(activation)}(activation)
         end
 end
 
-function initialparameters(backend::Backend, ::Type{T}, d::Gradient{M, M}; rng::AbstractRNG = Random.default_rng(), init_weight = GlorotUniform(), init_bias = ZeroInitializer(), init_scale = GlorotUniform()) where {M, T}
+function initialparameters(backend::Backend, ::Type{T}, d::GradientLayer{M, M}; rng::AbstractRNG = Random.default_rng(), init_weight = GlorotUniform(), init_bias = ZeroInitializer(), init_scale = GlorotUniform()) where {M, T}
         K = KernelAbstractions.allocate(backend, T, d.second_dim÷2, M÷2)
         b = KernelAbstractions.allocate(backend, T, d.second_dim÷2)
         a = KernelAbstractions.allocate(backend, T, d.second_dim÷2)
@@ -181,27 +158,27 @@ function initialparameters(backend::Backend, ::Type{T}, d::Gradient{M, M}; rng::
         return (weight=K, bias=b, scale=a)
 end
 
-function initialparameters(backend::Backend, ::Type{T}, ::Activation{M, M}; rng::AbstractRNG = Random.default_rng(), init_scale = GlorotUniform()) where {M, T}
+function initialparameters(backend::Backend, ::Type{T}, ::ActivationLayer{M, M}; rng::AbstractRNG = Random.default_rng(), init_scale = GlorotUniform()) where {M, T}
         a = KernelAbstractions.zeros(backend, T, M÷2)
         init_scale(rng, a)
         return (scale = a,)
 end
 
-function initialparameters(backend::Backend, ::Type{T}, ::LinearSympNetLayer{M, M}; rng::AbstractRNG = Random.default_rng(), init_weight = GlorotUniform()) where {M, T}
+function initialparameters(backend::Backend, ::Type{T}, ::LinearLayer{M, M}; rng::AbstractRNG = Random.default_rng(), init_weight = GlorotUniform()) where {M, T}
         S = KernelAbstractions.allocate(backend, T, (M÷2)*(M÷2+1)÷2)
         init_weight(rng, S)
         (weight=SymmetricMatrix(S, M÷2), )
 end
 
-function parameterlength(d::Gradient{M, M}) where {M}
+function parameterlength(d::GradientLayer{M, M}) where {M}
         d.second_dim÷2 * (M÷2 + 2)
 end
 
-function parameterlength(::Activation{M, M}) where {M}
+function parameterlength(::ActivationLayer{M, M}) where {M}
         M÷2
 end
 
-function parameterlength(::Linear{M, M}) where {M}
+function parameterlength(::LinearLayer{M, M}) where {M}
         (M÷2)*(M÷2+1)÷2
 end
 
@@ -218,7 +195,9 @@ function custom_vec_mul(scale::AbstractVector{T}, x::AbstractArray{T, 3}) where 
 end
 
 @doc raw"""
-This function is used in the wrappers where the input to the SympNet layers is not a `NamedTuple` (as it should be) but an `Array`.
+This function is used in the wrappers where the input to the SympNet layers is not a `NamedTuple` (as it should be) but an `AbstractArray`.
+
+It converts the Array to a `NamedTuple` (via `assign_q_and_p`), then calls the SympNet routine(s) and converts back to an `AbstractArray` (with `vcat`).
 """
 function apply_layer_to_nt_and_return_array(d::SympNetLayer{M, M}, ps) where {M}
         q, p = assign_q_and_p(x, N2)
@@ -226,56 +205,44 @@ function apply_layer_to_nt_and_return_array(d::SympNetLayer{M, M}, ps) where {M}
         return vcat(output.q, output.p)
 end
 
-@inline function (d::ActivationQ{M, M})(x::NamedTuple, ps) where {M}
+@inline function (d::ActivationLayerQ{M, M})(x::NamedTuple, ps) where {M}
         size(x.q, 1) == M÷2 || error("Dimension mismatch.")
         return (q = x.q + custom_vec_mul(ps.scale, d.activation.(x.p)), p = x.p)
 end
 
-@inline function (d::ActivationQ)(x::AbstractArray, ps)
-        apply_layer_to_nt_and_return_array(d, ps)
-end
-
-@inline function (d::ActivationP{M, M})(x::NamedTuple, ps) where {M}
+@inline function (d::ActivationLayerP{M, M})(x::NamedTuple, ps) where {M}
         size(x.q, 1) == M÷2 || error("Dimension mismatch.")
         return (q = x.q, p = x.p + custom_vec_mul(ps.scale, d.activation.(x.q)))
 end
 
-@inline function (d::ActivationP)(x::AbstractArray, ps)
-        apply_layer_to_nt_and_return_array(d, ps)
-end
 
-@inline function (d::GradientQ{M, M})(x::NamedTuple, ps) where {M}
+@inline function (d::GradientLayerQ{M, M})(x::NamedTuple, ps) where {M}
         size(x.q, 1) == M÷2 || error("Dimension mismatch.")
         (q = x.q + custom_mat_mul(ps.weight', (custom_vec_mul(ps.scale, d.activation.(custom_mat_mul(ps.weight, x.p) .+ ps.bias)))), p = x.p)     
 end
 
-@inline function (d::GradientQ)(x::AbstractArray, ps) 
-        apply_layer_to_nt_and_return_array(d, ps)
-end
 
-@inline function(d::GradientP{M, M})(x::NamedTuple, ps) where {M}
+@inline function(d::GradientLayerP{M, M})(x::NamedTuple, ps) where {M}
         size(x.q, 1) == M÷2 || error("Dimension mismatch.")
         (q = x.q, p = x.p + custom_mat_mul(ps.weight', (custom_vec_mul(ps.scale, d.activation.(custom_mat_mul(ps.weight, x.q) .+ ps.bias)))))
 end
 
-@inline function(d::GradientP{M, M})(x::AbstractArray, ps) where {M}
-        apply_layer_to_nt_and_return_array(d, ps)
-end
 
-@inline function(d::LinearQ{M, M})(x::NamedTuple, ps) where {M}
+@inline function(d::LinearLayerQ{M, M})(x::NamedTuple, ps) where {M}
         size(x.q, 1) == M÷2 || error("Dimension mismatch.")
         (q = x.q + custom_mat_mul(ps.weight, x.p), p = x.p)
 end
 
-@inline function (d::LinearQ)(x::AbstractArray, ps)
-        apply_layer_to_nt_and_return_array(d, ps)
-end
 
-@inline function(d::LinearP{M, M})(x::NamedTuple, ps) where {M}
+@inline function(d::LinearLayerP{M, M})(x::NamedTuple, ps) where {M}
         size(x.q, 1) == M÷2 || error("Dimension mismatch.")
         (q = x.q, p = x.p + custom_mat_mul(ps.weight, x.q))
 end
 
-@inline function (d::LinearP)(x::AbstractArray, ps)
+
+@doc raw"""
+This is called when a SympnetLayer is applied to a `NamedTuple`. It calls `apply_layer_to_nt_and_return_array`.
+"""
+@inline function (d::SympNetLayer)(x::AbstractArray, ps)
         apply_layer_to_nt_and_return_array(d, ps)
 end
