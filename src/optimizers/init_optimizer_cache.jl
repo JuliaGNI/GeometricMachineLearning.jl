fd5854e4322a83bad6453bd11228fd11333e4265
benedict-96 <benedikt.brantner@icloud.com>
2023-11-28 14:02:09 +0100

    Added routine for bfgs

--- src/optimizers/init_optimizer_cache.jl
+++ src/optimizers/init_optimizer_cache.jl
@@ -1,7 +1,8 @@
-"""
-Wrapper for the functions setup_adam_cache, setup_momentum_cache, setup_gradient_cache.
-These appear outside of optimizer_caches.jl because the OptimizerMethods first have to be defined.
+@doc raw"""
+Wrapper for the functions `setup_adam_cache`, `setup_momentum_cache`, `setup_gradient_cache`, `setup_bfgs_cache`.
+These appear outside of `optimizer_caches.jl` because the `OptimizerMethods` first have to be defined.
 """
 init_optimizer_cache(::GradientOptimizer, x) = setup_gradient_cache(x)
 init_optimizer_cache(::MomentumOptimizer, x) = setup_momentum_cache(x)
 init_optimizer_cache(::AdamOptimizer, x) = setup_adam_cache(x)
+init_optimizer_cache(::BFGSOptimizer, x) = setup_bfgs_cache(x)
