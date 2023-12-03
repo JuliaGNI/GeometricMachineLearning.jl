4bff014f779cb72d9531a0e84d627ca97bcbe19a
benedict-96 <benedikt.brantner@icloud.com>
2023-12-03 17:00:03 +0100

    Added a test for bfgs on the Stiefel manifold.

--- test/optimizers/bfgs_optimizer.jl
+++ test/optimizers/bfgs_optimizer.jl
@@ -1,7 +1,7 @@
 using GeometricMachineLearning
 using Zygote
 using Test
-using LinearAlgebra: norm
+using LinearAlgebra: norm, svd
 
 @doc raw"""
 This tests the BFGS optimizer.
@@ -48,3 +48,61 @@ function bfgs_optimizer2(N, n_iterates=10)
 end
 
 # bfgs_optimizer2(10)
+
+function stiefel_optimizer_test(N, n; T=Float32, n_iterates=10, η=1f-2)
+    opt=BFGSOptimizer(T(η))
+    A = rand(StiefelManifold{T}, N, n)
+    B = T(10)*randn(T, N, N)
+    ideal_A = svd(B).U[:, 1:n]
+    loss(A) = norm(B - A * A' * B)
+    ideal_error = norm(ideal_A)
+    losses = zeros(n_iterates+2)
+    losses[1] = loss(A)
+    optimizer_instance = Optimizer(opt, A)
+    λA = GlobalSection(A)
+    grad_loss = global_rep(λA, rgrad(A, gradient(loss, A)[1]))
+    GeometricMachineLearning.bfgs_initialization!(optimizer_instance, optimizer_instance.cache, grad_loss)
+    # geodesic for `grad_loss`
+    exp_grad_loss = GeometricMachineLearning.geodesic(grad_loss)
+    GeometricMachineLearning.apply_section!(A, λA, exp_grad_loss)
+    losses[2] = loss(A)
+    for i in 1:n_iterates
+        λA = GlobalSection(A)
+        grad_loss = global_rep(λA, rgrad(A, gradient(loss, A)[1]))
+        GeometricMachineLearning.bfgs_update!(optimizer_instance, optimizer_instance.cache, grad_loss)
+        # geodesic for `grad_loss`
+        exp_grad_loss = GeometricMachineLearning.geodesic(grad_loss)
+        GeometricMachineLearning.apply_section!(A, λA, exp_grad_loss)
+        losses[i+2] = loss(A)
+    end
+    losses, ideal_error, check(A)
+end
+
+function stiefel_adam_test(N, n; T=Float32, n_iterates=10)
+    opt=AdamOptimizer()
+    A = rand(StiefelManifold{T}, N, n)
+    B = T(10)*randn(T, N, N)
+    ideal_A = svd(B).U[:, 1:n]
+    loss(A) = norm(B - A * A' * B)
+    ideal_error = norm(ideal_A)
+    losses = zeros(n_iterates+2)
+    losses[1] = loss(A)
+    optimizer_instance = Optimizer(opt, A)
+    λA = GlobalSection(A)
+    grad_loss = global_rep(λA, rgrad(A, gradient(loss, A)[1]))
+    GeometricMachineLearning.update!(optimizer_instance, optimizer_instance.cache, grad_loss)
+    # geodesic for `grad_loss`
+    exp_grad_loss = GeometricMachineLearning.geodesic(grad_loss)
+    GeometricMachineLearning.apply_section!(A, λA, exp_grad_loss)
+    losses[2] = loss(A)
+    for i in 1:n_iterates
+        λA = GlobalSection(A)
+        grad_loss = global_rep(λA, rgrad(A, gradient(loss, A)[1]))
+        GeometricMachineLearning.update!(optimizer_instance, optimizer_instance.cache, grad_loss)
+        # geodesic for `grad_loss`
+        exp_grad_loss = GeometricMachineLearning.geodesic(grad_loss)
+        GeometricMachineLearning.apply_section!(A, λA, exp_grad_loss)
+        losses[i+2] = loss(A)
+    end
+    losses, ideal_error, check(A)
+end
