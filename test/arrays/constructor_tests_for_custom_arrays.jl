1f04af95a82d187b4ca90da28bccb67c917daf83
benedict-96 <benedikt.brantner@icloud.com>
2023-12-13 17:14:04 +0100

    Added tests for the various constructors for custom arrays.

---
+++ test/arrays/constructor_tests_for_custom_arrays.jl
@@ -0,0 +1,37 @@
+using GeometricMachineLearning, Test
+using LinearAlgebra: I
+
+@doc raw"""
+This tests various constructor for custom arrays, e.g. if calling `SymmetricMatrix` on a matrix ``A`` does 
+```math 
+A \mapsto \frac{1}{2}(A + A^T).
+```
+"""
+function test_constructors_for_custom_arrays(n::Int, N::Int, T::Type)
+    A = rand(T, n, n)
+    B = rand(T, N, N)
+
+    # SymmetricMatrix 
+    @test Matrix{T}(SymmetricMatrix(A)) ≈ T(.5) * (A + A')
+
+    # SkewSymMatrix 
+    @test Matrix{T}(SkewSymMatrix(A)) ≈ T(.5) * (A - A')
+
+    # StiefelLieAlgHorMatrix 
+    B_shor = StiefelLieAlgHorMatrix(SkewSymMatrix(B), n)
+    B_shor2 = Matrix{T}(SkewSymMatrix(B))
+    B_shor2[(n+1):N, (n+1):N] .= zero(T)
+    @test Matrix{T}(B_shor) ≈ B_shor2
+
+    # GrassmannLieAlgHorMatrix 
+    B_ghor = GrassmannLieAlgHorMatrix(SkewSymMatrix(B), n)
+    B_ghor2 = copy(B_shor2)
+    B_ghor2[1:n, 1:n] .= zero(T)
+    @test Matrix{T}(B_ghor) ≈ B_ghor2 
+
+    # StiefelProjection
+    E = StiefelProjection(T, N, n)
+    @test Matrix{T}(E) ≈ vcat(I(n), zeros(T, (N-n), n))
+end
+
+test_constructors_for_custom_arrays(5, 10, Float32)
