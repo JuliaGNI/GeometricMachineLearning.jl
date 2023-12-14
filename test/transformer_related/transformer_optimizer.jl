4c82474c49e6bc5a1c97bac4e5128af4968f17ba
benedict-96 <benedikt.brantner@icloud.com>
2023-12-14 15:15:19 +0100

    Refactored all the transformer tests and gave each of them a name.

--- test/transformer_related/transformer_optimizer.jl
+++ test/transformer_related/transformer_optimizer.jl
@@ -1,5 +1,8 @@
 using Test, KernelAbstractions, GeometricMachineLearning, Zygote, LinearAlgebra
 
+@doc raw"""
+This function tests if the `GradientOptimzier`, `MomentumOptimizer`, `AdamOptimizer` and `BFGSOptimizer` act on the neural network weights via `optimization_step!`.
+"""
 function transformer_gradient_test(T, dim, n_heads, L, seq_length=8, batch_size=10)
     model = Chain(Transformer(dim, n_heads, L, Stiefel=true), ResNet(dim))
     model = Transformer(dim, n_heads, L, Stiefel=true)
@@ -14,15 +17,19 @@ function transformer_gradient_test(T, dim, n_heads, L, seq_length=8, batch_size=
     o₁ = Optimizer(GradientOptimizer(), ps)
     o₂ = Optimizer(MomentumOptimizer(), ps)
     o₃ = Optimizer(AdamOptimizer(), ps)
+    o₄ = Optimizer(BFGSOptimizer(), ps)
 
     ps₁ = deepcopy(ps)
     ps₂ = deepcopy(ps)
     ps₃ = deepcopy(ps)
+    ps₄ = deepcopy(ps)
 
     optimization_step!(o₁, model, ps₁, dx)
     optimization_step!(o₂, model, ps₂, dx)
     optimization_step!(o₃, model, ps₃, dx)
-    @test typeof(ps₁) == typeof(ps₂) == typeof(ps₃) == typeof(ps)
+    optimization_step!(o₄, model, ps₄, dx)
+    @test typeof(ps₁) == typeof(ps₂) == typeof(ps₃) == typeof(ps₄) == typeof(ps)
+    @test ps₁[1].PQ.head_1 ≉ ps₂[1].PQ.head_1 ≉ ps₃[1].PQ.head_1 ≉ ps₄[1].PQ.head_1 ≉ ps[1].PQ.head_1
 end
 
 transformer_gradient_test(Float32, 10, 5, 4)
--- test/transformer_related/transformer_setup.jl
+++ test/transformer_related/transformer_setup.jl
@@ -1,5 +1,8 @@
 using Test, KernelAbstractions, GeometricMachineLearning
 
+@doc raw"""
+This function tests the setup of the transformer with Stiefel weights.
+"""
 function transformer_setup_test(dim, n_heads, L, T)
     model = Transformer(dim, n_heads, L, Stiefel=true)
     ps = initialparameters(KernelAbstractions.CPU(), T, model)
