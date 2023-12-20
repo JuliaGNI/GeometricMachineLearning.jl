da77a967fa26d914cbb62031ceb7f81cc3548d08
benedict-96 <benedikt.brantner@icloud.com>
2023-12-20 08:45:03 +0100

    Added sampling test for custom arrays (resolved conflict).

--- test/transformer_related/multi_head_attention_stiefel_retraction.jl
+++ test/transformer_related/multi_head_attention_stiefel_retraction.jl
@@ -1,7 +1,3 @@
-"""
-This is a test for that checks if the retractions (geodesic and Cayley for now) map from StiefelLieAlgHorMatrix to StiefelManifold when used with MultiHeadAttention.
-"""
-
 import Random, Test, Lux, LinearAlgebra, KernelAbstractions
 
 using GeometricMachineLearning, Test
@@ -9,37 +5,40 @@ using GeometricMachineLearning: geodesic
 using GeometricMachineLearning: cayley
 using GeometricMachineLearning: init_optimizer_cache
 
-dim = 64
-n_heads = 8
-Dₕ = dim÷8
-tol = eps(Float32)
-T = Float32
-backend = KernelAbstractions.CPU()
-
-model = MultiHeadAttention(dim, n_heads, Stiefel=true)
-
-ps = initialparameters(backend, T, model)
-
-cache = init_optimizer_cache(MomentumOptimizer(), ps)
-
-E = StiefelProjection(dim, Dₕ, T)
-function check_retraction_geodesic(A::AbstractMatrix) 
+@doc raw"""
+This function computes the geodesic retraction of an element of `StiefelLieAlgHorMatrix` and then checks if the resulting element is `StiefelProjection`.
+"""
+function check_retraction_geodesic(A::AbstractMatrix{T}, tol=eps(T)) where T
     A_retracted = geodesic(A)
     @test typeof(A_retracted) <: StiefelManifold
-    @test LinearAlgebra.norm(A_retracted - E) < tol
+    @test LinearAlgebra.norm(A_retracted - StiefelProjection(A_retracted)) < tol
 end
 check_retraction_geodesic(cache::NamedTuple) = apply_toNT(check_retraction_geodesic, cache)
 check_retraction_geodesic(B::MomentumCache) = check_retraction_geodesic(B.B)
 
-check_retraction_geodesic(cache)
-
-E = StiefelProjection(dim, Dₕ)
-function check_retraction_cayley(A::AbstractMatrix) 
+@doc raw"""
+This function computes the cayley retraction of an element of `StiefelLieAlgHorMatrix` and then checks if the resulting element is `StiefelProjection`.
+"""
+function check_retraction_cayley(A::AbstractMatrix{T}, tol=eps(T)) where T
     A_retracted = cayley(A)
     @test typeof(A_retracted) <: StiefelManifold
-    @test LinearAlgebra.norm(A_retracted - E) < tol
+    @test LinearAlgebra.norm(A_retracted - StiefelProjection(A_retracted)) < tol
 end
 check_retraction_cayley(cache::NamedTuple) = apply_toNT(check_retraction_cayley, cache)
 check_retraction_cayley(B::MomentumCache) = check_retraction_cayley(B.B)
 
-check_retraction_cayley(cache)
+@doc raw"""
+This is a test for that checks if the retractions (geodesic and Cayley for now) map from `StiefelLieAlgHorMatrix` to `StiefelManifold` when used with `MultiHeadAttention`.
+"""
+function test_multi_head_attention_retraction(T::Type, dim, n_heads, tol=eps(T), backend=KernelAbstractions.CPU())
+    model = MultiHeadAttention(dim, n_heads, Stiefel=true)
+
+    ps = initialparameters(backend, T, model)
+    cache = init_optimizer_cache(MomentumOptimizer(), ps)
+
+    check_retraction_geodesic(cache)
+
+    check_retraction_cayley(cache)
+end
+
+test_multi_head_attention_retraction(Float32, 64, 8)
--- test/transformer_related/multi_head_attention_stiefel_setup.jl
+++ test/transformer_related/multi_head_attention_stiefel_setup.jl
@@ -3,25 +3,36 @@ import Random, Test, Lux, LinearAlgebra, KernelAbstractions
 using GeometricMachineLearning, Test
 using GeometricMachineLearning: init_optimizer_cache
 
-T = Float32
-model = MultiHeadAttention(64, 8, Stiefel=true)
-ps = initialparameters(KernelAbstractions.CPU(), T, model)
-tol = 10*eps(T)
-
-function check_setup(A::AbstractMatrix)
+@doc raw"""
+This checks for an arbitrary matrix ``A\in\mathbb{R}^{N\times{}n}`` if ``A\in{}St(n,N)``.
+"""
+function check_setup(A::AbstractMatrix{T}, tol=T(10)*eps(T)) where T
     @test typeof(A) <: StiefelManifold
     @test check(A) < tol
 end
 check_setup(ps::NamedTuple) = apply_toNT(check_setup, ps)
-check_setup(ps)
 
-######## check if the gradients are set up the correct way 
-function check_grad_setup(B::AbstractMatrix)
+@doc raw"""
+This checks for an arbitrary matrix ``B\in\mathbb{R}^{N\times{}N}`` if ``B\in\mathfrak{g}^\mathrm{hor}``.
+"""
+function check_grad_setup(B::AbstractMatrix{T}, tol=T(10)*eps(T)) where T
     @test typeof(B) <: StiefelLieAlgHorMatrix
     @test LinearAlgebra.norm(B) < tol
 end
 check_grad_setup(gx::NamedTuple) = apply_toNT(check_grad_setup, gx)
 check_grad_setup(B::MomentumCache) = check_grad_setup(B.B)
 
-gx = init_optimizer_cache(MomentumOptimizer(), ps)
-check_grad_setup(gx)
+@doc raw"""
+Check if `initialparameters` and `init_optimizer_cache` do the right thing for `MultiHeadAttentionLayer`.
+"""
+function check_multi_head_attention_stiefel_setup(T::Type, N::Int, n::Int)
+    model = MultiHeadAttention(N, n, Stiefel=true)
+    ps = initialparameters(KernelAbstractions.CPU(), T, model)
+
+    check_setup(ps)
+
+    gx = init_optimizer_cache(MomentumOptimizer(), ps)
+    check_grad_setup(gx)
+end
+
+check_multi_head_attention_stiefel_setup(Float32, 64, 8)
