4bbfabb696427fdbd2f8817d27be358b018a618f
benedict-96 <benedikt.brantner@icloud.com>
2024-01-09 17:25:25 +0100

    Added a constructor for when the Adam optimizer is called with a type only.

--- src/optimizers/adam_optimizer.jl
+++ src/optimizers/adam_optimizer.jl
@@ -11,6 +11,10 @@ struct AdamOptimizer{T<:Real} <: OptimizerMethod
     AdamOptimizer(η = 1f-3, ρ₁ = 9f-1, ρ₂ = 9.9f-1, δ = 3f-7; T=typeof(η)) = new{T}(T(η), T(ρ₁), T(ρ₂), T(δ))
 end
 
+function AdamOptimizer(T::Type)
+    AdamOptimizer(T(1f-3))
+end
+
 function update!(o::Optimizer{<:AdamOptimizer{T}}, C::AdamCache, B::AbstractArray) where T
     add!(C.B₁, ((o.method.ρ₁ - o.method.ρ₁^o.step)/(T(1.) - o.method.ρ₁^o.step))*C.B₁, ((T(1.) - o.method.ρ₁)/(T(1.) - o.method.ρ₁^o.step))*B)
     add!(C.B₂, ((o.method.ρ₂ - o.method.ρ₂^o.step)/(T(1.) - o.method.ρ₂^o.step))*C.B₂, ((T(1.) - o.method.ρ₂)/(T(1.) - o.method.ρ₂^o.step))*⊙²(B))
