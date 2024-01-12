be7368c6898d24d40e33af4eb323b96593833195
benedict-96 <benedikt.brantner@icloud.com>
2024-01-12 19:03:30 +0100

    Combined the rand routines of Grassmann and Stiefel manifolds.

--- src/manifolds/abstract_manifold.jl
+++ src/manifolds/abstract_manifold.jl
@@ -1,5 +1,54 @@
+@doc raw"""
+`rand` is implemented for manifolds that use the initialization of the `StiefelManifold` and the `GrassmannManifold` by default. 
+"""
 abstract type Manifold{T} <: AbstractMatrix{T} end
 
+@kernel function assign_columns_kernel!(Y::AbstractMatrix{T}, A::AbstractMatrix{T}) where T
+    i,j = @index(Global, NTuple)
+    Y[i,j] = A[i,j]
+end
+
+function assign_columns(Q::AbstractMatrix{T}, N::Integer, n::Integer) where T
+    backend = KernelAbstractions.get_backend(Q)
+    Y = KernelAbstractions.allocate(backend, T, N, n)
+    assign_columns! = assign_columns_kernel!(backend)
+    assign_columns!(Y, Q, ndrange=size(Y))
+    Y
+end
+
+# TODO: check the distribution this is coming from - related to the Haar measure ???
+function Base.rand(::CPU, rng::Random.AbstractRNG, ::Type{MT}, N::Integer, n::Integer) where {T, MT<:Manifold{T}} 
+    @assert N ≥ n 
+    A = randn(rng, T, N, n)
+    MT{typeof(A)}(assign_columns(typeof(A)(qr!(A).Q), N, n))
+end
+
+function Base.rand(backend::GPU, rng::Random.AbstractRNG, ::Type{MT}, N::Integer, n::Integer) where {T, MT<:Manifold{T}} 
+    @assert N ≥ n 
+    A = KernelAbstractions.allocate(backend, T, N, n)
+    Random.randn!(rng, A)    
+    MT{typeof(A)}(assign_columns(typeof(A)(qr!(A).Q), N, n))
+end
+
+function Base.rand(backend::CPU, rng::Random.AbstractRNG, ::Type{MT}, N::Integer, n::Integer) where MT <: Manifold
+    rand(backend, rng, MT{Float64}, N, n)
+end
+
+function Base.rand(backend::GPU, rng::Random.AbstractRNG, ::Type{MT}, N::Integer, n::Integer) where MT <: Manifold
+    rand(backend, rng, MT{Float32}, N, n)
+end
+
+function Base.rand(rng::Random.AbstractRNG, manifold_type::Type{MT}, N::Integer, n::Integer) where MT <: Manifold
+    rand(CPU(), rng, manifold_type, N, n)
+end
+
+function Base.rand(backend::KernelAbstractions.Backend, manifold_type::Type{MT}, N::Integer, n::Integer) where MT <: Manifold 
+    rand(backend, Random.default_rng(), manifold_type, N, n)
+end
+
+function Base.rand(manifold_type::Type{MT}, N::Integer, n::Integer) where MT <: Manifold
+    rand(Random.default_rng(), manifold_type, N, n)
+end
 
 Base.size(A::Manifold) = size(A.A)
 Base.parent(A::Manifold) = A.A 
--- src/manifolds/stiefel_manifold.jl
+++ src/manifolds/stiefel_manifold.jl
@@ -7,53 +7,6 @@ An implementation of the Stiefel manifold. It has various convenience functions 
 """
 mutable struct StiefelManifold{T, AT <: AbstractMatrix{T}} <: Manifold{T}
     A::AT
-    function StiefelManifold(A::AbstractMatrix)
-        @assert size(A)[1] ≥ size(A)[2]
-        new{eltype(A), typeof(A)}(A)
-    end
-end
-
-@kernel function assign_columns_kernel!(Y::AbstractMatrix{T}, A::AbstractMatrix{T}) where T
-    i,j = @index(Global, NTuple)
-    Y[i,j] = A[i,j]
-end
-
-function assign_columns(Q::AbstractMatrix{T}, N::Integer, n::Integer) where T
-    backend = KernelAbstractions.get_backend(Q)
-    Y = KernelAbstractions.allocate(backend, T, N, n)
-    assign_columns! = assign_columns_kernel!(backend)
-    assign_columns!(Y, Q, ndrange=size(Y))
-    Y
-end
-
-# TODO: check the distribution this is coming from - related to the Haar measure ???
-function Base.rand(rng::Random.AbstractRNG, ::Type{StiefelManifold{T}}, N::Integer, n::Integer) where T
-    @assert N ≥ n
-    A = randn(rng, T, N, n)
-    StiefelManifold(assign_columns(typeof(A)(qr!(A).Q), N, n))
-end
-
-function Base.rand(rng::Random.AbstractRNG, ::Type{StiefelManifold}, N::Integer, n::Integer)
-    rand(rng, StiefelManifold{Float64}, N, n)
-end
-
-function Base.rand(manifold_type::Type{StiefelManifold{T}}, N::Integer, n::Integer) where T
-    rand(Random.default_rng(), manifold_type, N, n)
-end
-
-function Base.rand(::Type{StiefelManifold}, N::Integer, n::Integer)
-    rand(StiefelManifold{Float64}, N, n)
-end
-
-function Base.rand(backend::KernelAbstractions.Backend, rng::Random.AbstractRNG, ::Type{StiefelManifold{T}}, N::Integer, n::Integer) where T 
-    @assert N ≥ n 
-    A = KernelAbstractions.allocate(backend, T, N, n)
-    Random.rand!(rng, A)
-    StiefelManifold(assign_columns(typeof(A)(qr!(A).Q), N, n))
-end
-
-function Base.rand(backend::KernelAbstractions.Backend, manifold_type::Type{StiefelManifold{T}}, N::Integer, n::Integer) where T 
-    rand(backend, Random.default_rng(), manifold_type, N, n)
 end
 
 Base.:*(Y::StiefelManifold, B::AbstractMatrix) = Y.A*B
@@ -61,6 +14,10 @@ Base.:*(B::AbstractMatrix, Y::StiefelManifold) = B*Y.A
 
 function Base.:*(Y::Adjoint{T, StiefelManifold{T, AT}}, B::AbstractMatrix) where {T, AT<:AbstractMatrix{T}}
     Y.parent.A'*B 
+end
+
+function Base.:*(Y::Adjoint{T, ST}, B::ST) where {T, AT<:AbstractMatrix{T}, ST<:StiefelManifold{T, AT}}
+    Y.parent.A' * B.A
 end
 
 @doc raw"""
@@ -100,6 +57,6 @@ function global_section(Y::StiefelManifold{T}) where T
     backend = KernelAbstractions.get_backend(Y)
     A = KernelAbstractions.allocate(backend, T, N, N-n)
     randn!(A)
-    A = A - Y.A*Y.A'*A
+    A = A - Y.A * (Y.A' * A)
     qr!(A).Q
 end
