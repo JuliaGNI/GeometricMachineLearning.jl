a5e29273a38805e761ca772f5aa926e6d6536154
benedict-96 <benedikt.brantner@icloud.com>
2024-01-23 10:18:56 +0100

    Also testing the output array now (for dimension).

--- test/data_loader/draw_batch_for_tensor_test.jl
+++ test/data_loader/draw_batch_for_tensor_test.jl
@@ -7,6 +7,12 @@ function test_data_loader_for_qp_tensor(system_dim2::Int, input_time_steps::Int,
     dl = DataLoader(dummy_data)
     batch = Batch(batch_size, seq_length)
 
-    batch_indices = batch(dl)
-    batched_array = convert_input_and_batch_indices_to_array(dl, batch, batch_indices)
+    batch_indices_all = batch(dl)
+    for batch_indices in batch_indices_all
+        batched_array = convert_input_and_batch_indices_to_array(dl, batch, batch_indices)
+        @test size(batched_array[1].q) == (system_dim2, seq_length, batch_size)
+        @test size(batched_array[2].q) == (system_dim2, 1,          batch_size)
+    end
 end
+
+test_data_loader_for_qp_tensor(5, 50, 20, 10, 16)
