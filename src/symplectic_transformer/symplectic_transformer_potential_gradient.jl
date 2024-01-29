97b334d09129ed22f1b4c3568e85e3b6a0d6fff3
benedict-96 <benedikt.brantner@icloud.com>
2024-01-29 18:05:14 +0100

    Rewrote the program such that all computations are done with two independent kernels.

--- src/symplectic_transformer/symplectic_transformer_potential_gradient.jl
+++ src/symplectic_transformer/symplectic_transformer_potential_gradient.jl
@@ -22,7 +22,7 @@ function symplectic_transformer_potential_gradient(Z::AbstractArray{T}, A::Abstr
     T(.5) * _mul(
                 Z, 
                 (S + _transpose(S))
-                ) + SDerArrayC(Z, A) 
+                ) + s_der_array(Z, A, S) 
 end 
 
 @doc raw"""
@@ -45,7 +45,27 @@ _mul(Z::AbstractArray{T, 3}, Ẑ::AbstractArray{T, 3}) where T = tensor_tensor_m
 _mul(Z::AbstractMatrix, Ẑ::AbstractMatrix) = Z * Ẑ
 _mul(A::AbstractMatrix{T}, Z::AbstractArray{T, 3}) where T = mat_tensor_mul(A, Z)
 _transpose(S::AbstractArray{T, 3}) where T = tensor_transpose(S)
-_transpose(S::AbstractMatrix) = S' 
+_transpose(S::AbstractMatrix) = matrix_transpose(S)
+
+@kernel function s_derivative_kernel_1!(C::AbstractArray{T, 5}, Z::AT, S::AT, AZ::AT, ATZ::AT) where {T, AT<:AbstractArray{T, 3}}
+    m, l, f, i, k = @index(Global, NTuple)
+
+    C[m, l, f, i, k] = Z[i, k, f] * (
+        compute_first_term(Z, S, AZ, i, k, m, l, f) +
+        compute_third_term(Z, S, ATZ, i, k, m, l, f) )
+
+    nothing
+end
+
+@kernel function s_derivative_kernel_2!(C::AbstractArray{T, 6}, Z::AT, S::AT, AZ::AT, ATZ::AT) where {T, AT<:AbstractArray{T, 3}}
+    m, l, f, i, k, j = @index(Global, NTuple)
+
+    C[m, l, f, i, k, j] = Z[i, k, f] * (
+        compute_second_term(Z, S, AZ, i, k, m, l, f, j) + 
+        compute_fourth_term(Z, S, ATZ, i, k, m, l, f, j) )
+
+    nothing
+end
 
 @doc raw"""
 This computes the term
@@ -54,20 +74,7 @@ This computes the term
 \sum_{ijk}z_{ij}\frac{\partial{}s_{jk}}{\partial{}z_{m\ell}}z_{ik}
 ```
 """
-function s_derivative_term(Z::AbstractArray{T, 3}, A::AbstractMatrix{T}, S::AbstractArray{T, 3}, m::Int, l::Int, f::Int) where {T} 
-    n, time_steps = size(Z)
-    AZ = _mul(A, Z) 
-    ATZ = _mul(A', Z)
-
-    T(.5) * sum(Z[i, k, f] * (
-                            compute_first_term(Z, S, AZ, i, k, m, l, f) - 
-                            compute_second_term(Z, S, AZ, i, k, m, l, f) + 
-                            compute_third_term(Z, S, ATZ, i, k, m, l, f) - 
-                            compute_fourth_term(Z, S, ATZ, i, k, m, l, f) ) for i=1:n, k=1:time_steps)
-end 
-
 function s_derivative_term(Z::AbstractMatrix{T}, A::AbstractMatrix{T}, S::AbstractMatrix{T}, m::Int, l::Int) where {T} 
-    n, time_steps = size(Z)
     AZ = _mul(A, Z) 
     ATZ = _mul(A', Z)
 
@@ -75,7 +82,7 @@ function s_derivative_term(Z::AbstractMatrix{T}, A::AbstractMatrix{T}, S::Abstra
                             compute_first_term(Z, S, AZ, i, k, m, l) - 
                             compute_second_term(Z, S, AZ, i, k, m, l) + 
                             compute_third_term(Z, S, ATZ, i, k, m, l) - 
-                            compute_fourth_term(Z, S, ATZ, i, k, m, l) ) for i=1:n, k=1:time_steps)
+                            compute_fourth_term(Z, S, ATZ, i, k, m, l) ) for i in axes(Z, 1), k in axes(Z, 2))
 end 
 
 @doc raw"""
@@ -91,34 +98,20 @@ struct SDerMatrix{T, AT, BT} <: AbstractMatrix{T}
     S::AT 
 end
 
-struct SDerTensor{T, AT, BT} <: AbstractArray{T, 3} 
-    Z::AT 
-    A::BT 
-    S::AT 
-end
-
-const SDerArray = Union{SDerMatrix, SDerTensor}
-
 function SDerMatrix(Z::AT, A::BT, S::AT) where {T, AT<:AbstractMatrix{T}, BT<:AbstractMatrix{T}} 
     SDerMatrix{T, AT, BT}(Z, A, S)
 end 
 
 SDerMatrix(Z, A) = SDerMatrix(Z, A, _compute_softmax(Z, A))
 
-function SDerTensor(Z::AT, A::BT, S::AT) where {T, AT<:AbstractArray{T, 3}, BT<:AbstractMatrix{T}}
-    SDerTensor{T, AT, BT}(Z, A, S)
-end
-
-SDerTensor(Z, A) = SDerTensor(Z, A, _compute_softmax(Z, A))
-
 SDerArrayC(Z::AT, A::BT, S::AT) where {T, AT<:AbstractMatrix{T}, BT<:AbstractMatrix{T}} = SDerMatrix(Z, A, S)
-SDerArrayC(Z::AT, A::BT, S::AT) where {T, AT<:AbstractArray{T, 3}, BT<:AbstractMatrix{T}} = SDerTensor(Z, A, S)
 SDerArrayC(Z, A) = SDerArrayC(Z, A, _compute_softmax(Z, A))
 
 Base.getindex(ZSZ::SDerMatrix, m::Int, l::Int) = s_derivative_term(ZSZ.Z, ZSZ.A, ZSZ.S, m, l)
-Base.getindex(ZSZ::SDerTensor, m::Int, l::Int, f::Int) = s_derivative_term(ZSZ.Z, ZSZ.A, ZSZ.S, m, l, f)
 
-Base.size(ZSZ::SDerArray) = size(ZSZ.Z)
+Base.size(ZSZ::SDerMatrix) = size(ZSZ.Z)
+
+s_der_array(Z, A, S) = SDerArrayC(Z, A, S)
 
 @doc raw"""
 This computes the term 
@@ -147,9 +140,8 @@ function compute_second_term(Z::AbstractMatrix, S::AbstractMatrix, AZ::AbstractM
     sum(Z[i, j] * S[j, k] * S[l, k] * AZ[m, k] for j=1:time_steps)
 end
 
-function compute_second_term(Z::AbstractArray{T, 3}, S::AbstractArray{T, 3}, AZ::AbstractArray{T, 3}, i::Int, k::Int, m::Int, l::Int, f::Int) where T
-    time_steps = size(Z, 2)
-    sum(Z[i, j, f] * S[j, k, f] * S[l, k, f] * AZ[m, k, f] for j=1:time_steps)
+function compute_second_term(Z::AbstractArray{T, 3}, S::AbstractArray{T, 3}, AZ::AbstractArray{T, 3}, i::Int, k::Int, m::Int, l::Int, f::Int, j::Int) where T
+    Z[i, j, f] * S[j, k, f] * S[l, k, f] * AZ[m, k, f]
 end
 
 @doc raw"""
@@ -179,7 +171,55 @@ function compute_fourth_term(Z::AbstractMatrix, S::AbstractMatrix, ATZ::Abstract
     sum(Z[i, l] * S[k, l] * S[j, l] * ATZ[m, j] for j=1:time_steps)
 end
 
-function compute_fourth_term(Z::AbstractArray{T, 3}, S::AbstractArray{T, 3}, ATZ::AbstractArray{T, 3}, i::Int, k::Int, m::Int, l::Int, f::Int) where T 
-    time_steps = size(Z, 2)
-    sum(Z[i, l, f] * S[k, l, f] * S[j, l, f] * ATZ[m, j, f] for j=1:time_steps)
+function compute_fourth_term(Z::AbstractArray{T, 3}, S::AbstractArray{T, 3}, ATZ::AbstractArray{T, 3}, i::Int, k::Int, m::Int, l::Int, f::Int, j̃::Int) where T 
+    Z[i, l, f] * S[k, l, f] * S[j̃, l, f] * ATZ[m, j̃, f] 
 end
+
+#=
+function z_derivative_of_tensor(C::SDerTensor, C_diff)
+    sum(Zygote.gradient(Z -> s_derivative_term(Z, C.A, C.S, i, j, k), C.Z)[1] * C_diff[i, j, k] for i in axes(C_diff, 1), j in axes(C_diff, 2), k in axes(C_diff, 3))
+end
+
+function a_derivative_of_tensor(C::SDerTensor, C_diff)
+    sum(Zygote.gradient(A -> s_derivative_term(C.Z, A, C.S, i, j, k), C.A)[1] * C_diff[i, j, k] for i in axes(C_diff, 1), j in axes(C_diff, 2), k in axes(C_diff, 3))
+end
+
+function s_derivative_of_tensor(C::SDerTensor, C_diff)
+    sum(Zygote.gradient(S -> s_derivative_term(C.Z, C.A, S, i, j, k), C.S)[1] * C_diff[i, j, k] for i in axes(C_diff, 1), j in axes(C_diff, 2), k in axes(C_diff, 3))
+end
+
+function ChainRulesCore.rrule(::typeof(s_der_array), Z::AT, A::BT, S::AT) where {T, AT<:AbstractArray{T, 3}, BT<:AbstractMatrix{T}}
+    C = SDerTensor(Z, A, S)
+    function SDerTensor_pullback(C_diff)
+        f̃ = NoTangent()
+        Z_diff = @thunk z_derivative_of_tensor(C, C_diff)
+        A_diff = @thunk a_derivative_of_tensor(C, C_diff)
+        S_diff = @thunk s_derivative_of_tensor(C, C_diff)
+        return f̃, Z_diff, A_diff, S_diff 
+    end
+    return C, SDerTensor_pullback
+end
+=#
+
+function s_der_array(Z::AT, A::BT, S::AT) where {T, AT<:AbstractArray{T, 3}, BT<:AbstractArray{T, 2}}
+    AZ = _mul(A, Z) 
+    ATZ = _mul(_transpose(A), Z)
+
+    s_der_array(Z, S, AZ, ATZ)
+end
+
+function s_der_array(Z::AT, S::AT, AZ::AT, ATZ::AT) where {T, AT<:AbstractArray{T, 3}}
+    backend = KernelAbstractions.get_backend(Z)
+
+    C₁ = KernelAbstractions.allocate(backend, T, size(Z, 1), size(Z, 2), size(Z, 3), size(Z, 1), size(Z, 2))
+    C₂ = KernelAbstractions.allocate(backend, T, size(Z, 1), size(Z, 2), size(Z, 3), size(Z, 1), size(Z, 2), size(Z, 2))
+
+    s_derivative_1! = s_derivative_kernel_1!(backend)
+    s_derivative_2! = s_derivative_kernel_2!(backend)
+
+    s_derivative_1!(C₁, Z, S, AZ, ATZ, ndrange=size(C₁))
+    s_derivative_2!(C₂, Z, S, AZ, ATZ, ndrange=size(C₂))
+
+    T(.5) * ( (@view sum(C₁, dims=(4, 5))[:, :, :, 1, 1]) - (@view sum(C₂, dims=(4, 5, 6))[:, :, :, 1, 1, 1]) )
+end
+
