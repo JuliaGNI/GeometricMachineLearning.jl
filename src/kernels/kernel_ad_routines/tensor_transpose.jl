fad148de72772685949d9fc102146f653a5b106c
benedict-96 <benedikt.brantner@icloud.com>
2024-01-29 17:58:59 +0100

    Removed unnecessary comment.

--- src/kernels/kernel_ad_routines/tensor_transpose.jl
+++ src/kernels/kernel_ad_routines/tensor_transpose.jl
@@ -1,8 +1,6 @@
 """
 This implements the custom pullback tor tensor_transpose
 """
-
-#the @thunk macro means that the computation is only performed in case it is needed
 function ChainRulesCore.rrule(::typeof(tensor_transpose), A::AbstractArray{T, 3}) where T
     C = tensor_transpose(A)
     function tensor_transpose_pullback(C_diff)
