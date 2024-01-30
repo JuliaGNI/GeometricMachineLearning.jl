5f99b80d7af803796272629a8976c02b9c0dc54d
benedict-96 <benedikt.brantner@icloud.com>
2024-01-30 15:19:38 +0100

    Increased the number of initial conditions and switched to CUDA backend.

--- scripts/symplectic_transformer/double_pendulum.jl
+++ scripts/symplectic_transformer/double_pendulum.jl
@@ -1,28 +1,60 @@
-using GeometricMachineLearning: DataLoader, LinearSymplecticTransformer, NeuralNetwork, CPU, Batch, AdamOptimizer, Optimizer, transformer_loss
+using GeometricMachineLearning: DataLoader, LinearSymplecticTransformer, NeuralNetwork, CPU, Batch, AdamOptimizer, Optimizer, transformer_loss, GSympNet
 using GeometricProblems.DoublePendulum: tspan, tstep, default_parameters, hodeproblem
 using GeometricIntegrators: integrate, ImplicitMidpoint
 using GeometricEquations: EnsembleProblem
+using CUDA
 
 initial_conditions = [
-        (q=[π / 4, π / 2], p=[0.0, π / 8]), 
-        (q=[π / 4, π / 1], p=[0.0, 0.0  ]),
-        (q=[π / 1, π / 2], p=[0.0, π    ])
+    (q = [π / i, π / j], p = [0.0, π / k]) for i=1:10, j=1:10, k=1:5
 ]
+initial_conditions = reshape(initial_conditions, length(initial_conditions))
 
-ensemble_problem = EnsembleProblem(hodeproblem().equation, (tspan[1], 10*tspan[2]), tstep, initial_conditions, default_parameters)
+ensemble_problem = EnsembleProblem(hodeproblem().equation, (tspan[1], 20*tspan[2]), tstep, initial_conditions, default_parameters)
 
 ensemble_solution = integrate(ensemble_problem, ImplicitMidpoint())
 
 dl = DataLoader(ensemble_solution)
 
 const seq_length = 5
+const nhidden = 4
+const depth = 1 
+const backend = CUDABackend()
+const T = backend == CUDABackend() ? Float32 : Float64 
+const batch_size = 1000
+const n_epochs = 100
+const opt_method = AdamOptimizer(T)
 
-arch = LinearSymplecticTransformer(dl, nhidden=2, depth=1, seq_length=seq_length)
+dl = backend == CUDABackend() ? DataLoader(dl.input |> cu) : dl
 
-nn = NeuralNetwork(arch, CPU(), Float64)
+function train_linear_symplectic_transformer()
 
-opt = Optimizer(AdamOptimizer(), nn)
+    arch = LinearSymplecticTransformer(dl, nhidden = nhidden, depth = depth, seq_length = seq_length)
 
-batch = Batch(1000, seq_length)
+    nn = NeuralNetwork(arch, backend, T)
 
-loss_array = opt(nn, dl, batch, 1000, transformer_loss)
+    opt = Optimizer(opt_method, nn)
+
+    batch = Batch(batch_size, seq_length)
+
+    loss_array = opt(nn, dl, batch, n_epochs, transformer_loss)
+
+    loss_array, nn
+end 
+
+function train_sympnet()
+
+    arch = GSympNet(dl, nhidden = nhidden * depth)
+
+    nn = NeuralNetwork(arch, backend, T)
+
+    opt = Optimizer(opt_method, nn)
+
+    batch = Batch(batch_size, 1)
+
+    loss_array = opt(nn, dl, batch, n_epochs)
+
+    loss_array, nn
+end
+
+sympnet_loss_array, sympnet = train_sympnet()
+transformer_loss_array, transformer = train_linear_symplectic_transformer()
