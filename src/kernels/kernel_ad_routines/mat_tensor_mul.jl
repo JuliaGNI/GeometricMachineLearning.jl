882193536cdc16aa8aa4bcbc3c46ea173d96f70e
benedict-96 <benedikt.brantner@icloud.com>
2024-02-16 17:56:13 +0800

    Added special pullback routines for mat_tensor_mul with SymmetricMatrix and SkewSymMatrix.

--- src/kernels/kernel_ad_routines/mat_tensor_mul.jl
+++ src/kernels/kernel_ad_routines/mat_tensor_mul.jl
@@ -16,6 +16,44 @@ function ChainRulesCore.rrule(::typeof(mat_tensor_mul), B::AbstractMatrix{T}, A:
     return C, mat_tensor_mul_pullback
 end
    
+# get a piece of paper and wirte out these rules! if it's too difficult you can do it with Enzyme
+function ChainRulesCore.rrule(::typeof(mat_tensor_mul), B::SkewSymMatrix{T}, A::AbstractArray{T, 3}) where T 
+    @assert size(A, 1) == B.n 
+    C = mat_tensor_mul(B, A)
+    function skew_sym_mul_pullback(C_diff::AbstractArray{T, 3})
+        f̄ = NoTangent()
+        S_diff = zero(B.S)
+        A_diff = zero(C_diff)
+        C_diff_copy = copy(C_diff)
+        S_copy = copy(B.S)
+        A_copy = copy(A)
+        C_copy = copy(C)
+        Enzyme.autodiff(Enzyme.Reverse, skew_mat_mul!, Enzyme.Const, Enzyme.Duplicated(C_copy, C_diff_copy), Enzyme.Duplicated(S_copy, S_diff), Enzyme.Duplicated(A_copy, A_diff), Enzyme.Const(B.n))
+
+        return f̄, SkewSymMatrix(S_diff, B.n), A_diff 
+    end 
+    return C, skew_sym_mul_pullback
+end
+
+function ChainRulesCore.rrule(::typeof(mat_tensor_mul), B::SymmetricMatrix{T}, A::AbstractArray{T, 3}) where T 
+    @assert size(A, 1) == B.n 
+    C = mat_tensor_mul(B, A)
+    function symmetric_mul_pullback(C_diff::AbstractArray{T, 3})
+        f̄ = NoTangent()
+        S_diff = zero(B.S)
+        A_diff = zero(C_diff)
+        C_diff_copy = copy(C_diff)
+        S_copy = copy(B.S)
+        A_copy = copy(A)
+        C_copy = copy(C)
+        Enzyme.autodiff(Enzyme.Reverse, symmetric_mat_mul!, Enzyme.Const, Enzyme.Duplicated(C_copy, C_diff_copy), Enzyme.Duplicated(S_copy, S_diff), Enzyme.Duplicated(A_copy, A_diff), Enzyme.Const(B.n))
+
+        return f̄, SymmetricMatrix(S_diff, B.n), A_diff 
+    end 
+    return C, skew_sym_mul_pullback
+end
+
+
 mat_tensor_mul(B::AbstractMatrix, A::Thunk) = Thunk(() -> mat_tensor_mul(B, unthunk(A)))
     
 function tensor_tensor_transpose_mul(B::Thunk, A::AbstractArray{T, 3}) where T 
