b04da1a1186bcc3d560fbeb8004a82dac01b5200
benedict-96 <benedikt.brantner@icloud.com>
2024-02-23 10:48:30 +0100

    Updated description.

--- src/layers/volume_preserving_attention.jl
+++ src/layers/volume_preserving_attention.jl
@@ -1,13 +1,20 @@
-"""
-Volume-preserving attention (single heaad attention)
+@doc raw"""
+# Volume-preserving attention (single head attention)
 
 Drawbacks: 
 - only works on CPU for now (there is an issue with Enzyme atm)
 - the super-fast activation is only implemented for sequence lengths of 4, 5 and 6. We also dispatch over this. 
 
+## Constructor 
 The constructor is called with: 
 - `dim::Int`: The system dimension 
 - `seq_length::Int`: The number of time steps that are considered in the attention mechanism. 
+
+## Functor 
+Applying a layer of type `VolumePreservingAttention` does the following: 
+- First we perform the operation ``X \mapsto X^T A X =: C``, where ``X\in'mathbb{R}^{N\times\mathtt{seq\_length}}`` is a vector containing time series data and ``A`` is the skew symmetric matrix associated with the layer. 
+- In a second step we compute the Cayley transform of ``C``; ``\Lambda = \mathrm{Cayley}(C)``.
+- The output of the layer is then ``X\Lambda``.
 """
 struct VolumePreservingAttention{M, N, SL} <: AbstractExplicitLayer{M, N}
 
@@ -42,15 +49,6 @@ function initialparameters(backend::KernelAbstractions.Backend, T::Type, d::Volu
     (A = SkewSymMatrix(V, M), )
 end
 
-@doc raw"""
-Here we fist perform the operation 
-```math
-X \mapsto X^T A X =: C,
-```
-where ``X\in'mathbb{R}^{N\times\mathtt{seq\_length}}`` is a vector containing time series data and ``A`` is the skew symmetric matrix associated with the layer. 
-
-In a second step we compute the Cayley transform of ``C``. This is the output. 
-"""
 function (d::VolumePreservingAttention)(x::AbstractArray{T, 3}, ps::NamedTuple) where {T}
     tensor_tensor_mul(  x, 
                         orthonormal_activation_cayley(d, 
