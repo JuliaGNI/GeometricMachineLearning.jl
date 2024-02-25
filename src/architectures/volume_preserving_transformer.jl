e5e01624c18f83f478c4f40ae80472dd522fa419
benedict-96 <benedikt.brantner@icloud.com>
2024-02-25 18:13:44 +0100

    Updated architecture for volume-preserving transformer. depth can't be set anymore. Instead work with n_blocks and n_linear!

--- src/architectures/volume_preserving_transformer.jl
+++ src/architectures/volume_preserving_transformer.jl
@@ -1,75 +1,34 @@
-struct VolumePreservingTransformer{AT, InitUpper, Upscaling} <: TransformerIntegrator 
-    sys_dim::Int 
-    transformer_dim::Int 
-    seq_length::Int
-    depth::Int
-    L::Int 
-    activation::AT
-end
-
 @doc raw"""
 The volume-preserving transformer with the Cayley activation function and built-in upscaling. The arguments for the constructor are: 
 - `sys_dim::Int`
 - `seq_length::Int`: The sequence length of the data fed into the transformer.
-- `depth::Int`: The number of volume-preserving feedforward layers in one transformer block.
-- `transformer_dim::Int`: by default equal to `sys_dim`
-- `L::Int`: The number of transformer blocks (default is 2). 
+- `n_blocks::Int`: The number of blocks in one transformer unit (containing linear layers and nonlinear layers). Default is `1`.
+- `n_linear::Int`: The number of linear `VolumePreservingLowerLayer`s and `VolumePreservingUpperLayer`s in one block. Default is `1`.
+- `L::Int`: The number of transformer units (default is 2). 
 - `activation`: The activation function (`tanh` by default).
 - `init_upper::Bool`: Specifies if the network first acts on the ``q`` component. 
 """
-function VolumePreservingTransformer(sys_dim::Int, seq_length::Int, depth::Int = 2, transformer_dim::Int = sys_dim, L::Int = 2, activation = tanh, init_upper::Bool=true)
-    upscaling = sys_dim == transformer_dim ? false : true
-    VolumePreservingTransformer{typeof(tanh), init_upper, upscaling}(sys_dim, transformer_dim, seq_length, depth, L, activation)
-end
+struct VolumePreservingTransformer{AT} <: TransformerIntegrator 
+    sys_dim::Int 
+    seq_length::Int
+    n_blocks::Int
+    n_linear::Int
+    L::Int 
+    activation::AT
+    init_upper::Bool
 
-function Chain(arch::VolumePreservingTransformer{AT, true, true}) where {AT}
-    layers = (StiefelLayer(arch.sys_dim, arch.transformer_dim), )
-    for _ in 1:arch.L 
-        layers = (layers..., VolumePreservingAttention(arch.transformer_dim, arch.seq_length))
-        for __ in 1:arch.depth
-            layers = (layers..., VolumePreservingLowerLayer(arch.transformer_dim, arch.activation))
-            layers = (layers..., VolumePreservingUpperLayer(arch.transformer_dim, arch.activation))
-        end
+    function VolumePreservingTransformer(sys_dim::Int, seq_length::Int, n_blocks::Int=1, n_linear::Int=1, L::Int=2, activation=tanh, init_upper::Bool=false)
+        return new{typeof(tanh)}(sys_dim, seq_length, n_blocks, n_linear, L, activation, init_upper)
     end
-    layers = (layers..., StiefelLayer(arch.transformer_dim, arch.sys_dim))
-
-    Chain(layers...)
 end
 
-function Chain(arch::VolumePreservingTransformer{AT, false, true}) where {AT}
-    layers = (StiefelLayer(arch.sys_dim, arch.transformer_dim), )
-    for _ in 1:arch.L 
-        layers = (layers..., VolumePreservingAttention(arch.transformer_dim, arch.seq_length))
-        for __ in 1:arch.depth
-            layers = (layers..., VolumePreservingUpperLayer(arch.transformer_dim, arch.activation))
-            layers = (layers..., VolumePreservingLowerLayer(arch.transformer_dim, arch.activation))
-        end
-    end
-    layers = (layers..., StiefelLayer(arch.transformer_dim, arch.sys_dim))
-    Chain(layers...)
-end
-
-function Chain(arch::VolumePreservingTransformer{AT, true, false}) where {AT}
+function Chain(arch::VolumePreservingTransformer)
     layers = ()
-    for _ in 1:arch.L 
-        layers = (layers..., VolumePreservingAttention(arch.transformer_dim, arch.seq_length))
-        for __ in 1:arch.depth
-            layers = (layers..., VolumePreservingLowerLayer(arch.transformer_dim, arch.activation))
-            layers = (layers..., VolumePreservingUpperLayer(arch.transformer_dim, arch.activation))
-        end
+    
+    for _ in 1:arch:L 
+        layers = (layers..., VolumePreservingAttention(arch.sys_dim, arch.seq_length))
+        layers = (layers..., Chain(VolumePreservingFeedForward(arch.sys_dim, arch.n_blocks, arch.n_linear, arch.tanh; init_upper=init_upper)).layers...)
     end
 
-    Chain(layers...)
-end
-
-function Chain(arch::VolumePreservingTransformer{AT, false, false}) where {AT}
-    layers = ()
-    for _ in 1:arch.L 
-        layers = (layers..., VolumePreservingAttention(arch.transformer_dim, arch.seq_length))
-        for __ in 1:arch.depth
-            layers = (layers..., VolumePreservingUpperLayer(arch.transformer_dim, arch.activation))
-            layers = (layers..., VolumePreservingLowerLayer(arch.transformer_dim, arch.activation))
-        end
-    end
     Chain(layers...)
 end
