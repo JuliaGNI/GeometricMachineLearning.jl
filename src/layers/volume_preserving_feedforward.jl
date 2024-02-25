448095ee8f911c8e86b241e5c551efbf9b236be2
benedict-96 <benedikt.brantner@icloud.com>
2024-02-25 18:06:37 +0100

    Made bias optional.

--- src/layers/volume_preserving_feedforward.jl
+++ src/layers/volume_preserving_feedforward.jl
@@ -1,24 +1,33 @@
-abstract type VolumePreservingFeedForwardLayer{M, N} <: AbstractExplicitLayer{M, N} end 
+abstract type VolumePreservingFeedForwardLayer{M, N, bias} <: AbstractExplicitLayer{M, N} end 
 
-struct VolumePreservingLowerLayer{M, N, AT} <: VolumePreservingFeedForwardLayer{M, N}
+struct VolumePreservingLowerLayer{M, N, bias, AT} <: VolumePreservingFeedForwardLayer{M, N, bias}
     activation::AT
 
-    function VolumePreservingLowerLayer(sys_dim::Int, activation=tanh)
-        new{sys_dim, sys_dim, typeof(activation)}(activation)
+    function VolumePreservingLowerLayer(sys_dim::Int, activation=tanh; include_bias=true)
+        if include_bias
+            return new{sys_dim, sys_dim, :bias, typeof(activation)}(activation)
+        else 
+            return new{sys_dim, sys_dim, :no_bias, typeof(activation)}(activation)
+        end
     end
 end 
 
-struct VolumePreservingUpperLayer{M, N, AT} <: VolumePreservingFeedForwardLayer{M, N}
+struct VolumePreservingUpperLayer{M, N, bias, AT} <: VolumePreservingFeedForwardLayer{M, N, bias}
     activation::AT
 
-    function VolumePreservingUpperLayer(sys_dim::Int, activation=tanh)
-        new{sys_dim, sys_dim, typeof(activation)}(activation)
+    function VolumePreservingUpperLayer(sys_dim::Int, activation=tanh; include_bias=true)
+        if include_bias
+            return new{sys_dim, sys_dim, :bias, typeof(activation)}(activation)
+        else 
+            return new{sys_dim, sys_dim, :no_bias, typeof(activation)}(activation)
+        end
     end
 end 
 
-parameterlength(::VolumePreservingFeedForwardLayer{M, M}) where M = M * (M - 1) ÷ 2 
+parameterlength(::VolumePreservingFeedForwardLayer{M, M, :no_bias}) where M = M * (M - 1) ÷ 2 
+parameterlength(::VolumePreservingFeedForwardLayer{M, M, :bias}) where M = M * (M - 1) ÷ 2 + M
 
-function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingLowerLayer{M, M}; rng::AbstractRNG = Random.default_rng(), init_weight! = GlorotUniform(), init_bias! = init_weight!) where {M, T}
+function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingLowerLayer{M, M, :bias}; rng::AbstractRNG = Random.default_rng(), init_weight! = GlorotUniform(), init_bias! = ZeroInitializer()) where {M, T}
     S = KernelAbstractions.allocate(backend, T, parameterlength(d))
     b = KernelAbstractions.allocate(backend, T, M)
     init_weight!(rng, S)
@@ -27,7 +36,7 @@ function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingLower
     (weight = LowerTriangular(S, M), bias = b)
 end 
 
-function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingUpperLayer{M, M}; rng::AbstractRNG = Random.default_rng(), init_weight! = GlorotUniform(), init_bias! = init_weight!) where {M, T}
+function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingUpperLayer{M, M, :bias}; rng::AbstractRNG = Random.default_rng(), init_weight! = GlorotUniform(), init_bias! = ZeroInitializer()) where {M, T}
     S = KernelAbstractions.allocate(backend, T, parameterlength(d))
     b = KernelAbstractions.allocate(backend, T, M)
     init_weight!(rng, S)
@@ -36,10 +45,32 @@ function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingUpper
     (weight = UpperTriangular(S, M), bias = b)
 end 
 
-function (d::VolumePreservingFeedForwardLayer)(x::AbstractArray{T, 3}, ps) where T
+function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingLowerLayer{M, M, :no_bias}; rng::AbstractRNG = Random.default_rng(), init_weight! = GlorotUniform(), init_bias! = ZeroInitializer()) where {M, T}
+    S = KernelAbstractions.allocate(backend, T, parameterlength(d))
+    init_weight!(rng, S)
+
+    (weight = LowerTriangular(S, M), )
+end 
+
+function initialparameters(backend::Backend, ::Type{T}, d::VolumePreservingUpperLayer{M, M, :no_bias}; rng::AbstractRNG = Random.default_rng(), init_weight! = GlorotUniform(), init_bias! = ZeroInitializer()) where {M, T}
+    S = KernelAbstractions.allocate(backend, T, parameterlength(d))
+    init_weight!(rng, S)
+    
+    (weight = UpperTriangular(S, M), )
+end 
+
+function (d::VolumePreservingFeedForwardLayer{M, M, :bias})(x::AbstractArray{T, 3}, ps) where {T, M}
     x + d.activation.(mat_tensor_mul(ps.weight, x) .+ ps.bias)
 end
 
-function (d::VolumePreservingFeedForwardLayer)(x::AbstractVecOrMat{T}, ps) where T 
+function (d::VolumePreservingFeedForwardLayer{M, M, :no_bias})(x::AbstractArray{T, 3}, ps) where {T, M}
+    x + d.activation.(mat_tensor_mul(ps.weight, x))
+end
+
+function (d::VolumePreservingFeedForwardLayer{M, M, :bias})(x::AbstractVecOrMat{T}, ps) where {T, M} 
     x + d.activation.(ps.weight * x .+ ps.bias)
+end
+
+function (d::VolumePreservingFeedForwardLayer{M, M, :no_bias})(x::AbstractVecOrMat{T}, ps) where {T, M} 
+    x + d.activation.(ps.weight * x)
 end
