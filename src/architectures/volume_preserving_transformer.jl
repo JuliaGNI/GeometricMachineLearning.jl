b5d2b4ac527c126cde31dd6d2ab3d27092b2e587
benedict-96 <benedikt.brantner@icloud.com>
2024-03-06 14:24:48 +0100

    Add spaces.

--- src/architectures/volume_preserving_transformer.jl
+++ src/architectures/volume_preserving_transformer.jl
@@ -28,8 +28,8 @@ function Chain(arch::VolumePreservingTransformer)
     layers = ()
     
     for _ in 1:arch.L 
-        layers = (layers..., VolumePreservingAttention(arch.sys_dim, arch.seq_length; skew_sym=arch.skew_sym))
-        layers = (layers..., Chain(VolumePreservingFeedForward(arch.sys_dim, arch.n_blocks, arch.n_linear, arch.activation; init_upper=arch.init_upper)).layers...)
+        layers = (layers..., VolumePreservingAttention(arch.sys_dim, arch.seq_length; skew_sym = arch.skew_sym))
+        layers = (layers..., Chain(VolumePreservingFeedForward(arch.sys_dim, arch.n_blocks, arch.n_linear, arch.activation; init_upper = arch.init_upper)).layers...)
     end
 
     Chain(layers...)
