8a9503d19fa662bdd95cc17b15cb3bc28be92072
benedict-96 <benedikt.brantner@icloud.com>
2024-03-20 09:57:22 +0100

    Added add_connection option.

--- src/architectures/regular_transformer_integrator.jl
+++ src/architectures/regular_transformer_integrator.jl
@@ -8,6 +8,7 @@ The constructor is called with the following arguments:
 - `L::Int` the number of transformer blocks (default is `L = 2`).
 - `upscaling_activation`: by default identity
 - `resnet_activation`: by default tanh
+- `add_connection:Bool=true` (keyword argument): if the input should be added to the output.
 """
 struct RegularTransformerIntegrator{AT1, AT2} <: TransformerIntegrator
     sys_dim::Int 
@@ -16,9 +17,10 @@ struct RegularTransformerIntegrator{AT1, AT2} <: TransformerIntegrator
     L::Int
     upsacling_activation::AT1
     resnet_activation::AT2
+    add_connection::Bool
 
-    function RegularTransformerIntegrator(sys_dim::Int, transformer_dim::Int = sys_dim, n_heads::Int = sys_dim, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh)
-        new{typeof(upscaling_activation), typeof(resnet_activation)}(sys_dim, transformer_dim, n_heads, L, upscaling_activation, resnet_activation)
+    function RegularTransformerIntegrator(sys_dim::Int, transformer_dim::Int = sys_dim, n_heads::Int = sys_dim, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh; add_connection::Bool=true)
+        new{typeof(upscaling_activation), typeof(resnet_activation)}(sys_dim, transformer_dim, n_heads, L, upscaling_activation, resnet_activation, add_connection)
     end
 end
 
@@ -26,7 +28,7 @@ end
 function Chain(arch::RegularTransformerIntegrator)
     layers = (Dense(arch.sys_dim, arch.transformer_dim, arch.upsacling_activation), )
     for _ in 1:arch.L 
-        layers = (layers..., MultiHeadAttention(arch.transformer_dim, arch.n_heads))
+        layers = (layers..., MultiHeadAttention(arch.transformer_dim, arch.n_heads; add_connection = arch.add_connection))
         layers = (layers..., ResNet(arch.transformer_dim, arch.resnet_activation))
     end
     layers = (layers..., Dense(arch.transformer_dim, arch.sys_dim, identity))
