3fd6b4fc05193ec5b59c7d19cc633bcd5c51a0a2
benedict-96 <benedikt.brantner@icloud.com>
2024-03-25 12:13:06 +0100

    Added TransformerLoss constructor and put optimizatoin routines into extra file.

--- src/data_loader/batch.jl
+++ src/data_loader/batch.jl
@@ -44,6 +44,9 @@ struct Batch{seq_type <: Union{Nothing, Int}}
     prediction_window::seq_type
 end
 
+# add an additional constructor for `TransformerLoss` taking batch as input.
+TransformerLoss(batch::Batch{Int}) = TransformerLoss(seq_length, prediction_window)
+
 function Batch(batch_size, seq_length = nothing, prediction_window = nothing)
     new{typeof(seq_length)}(batch_size, seq_length, prediction_window)
 end
@@ -167,22 +170,22 @@ end
 end
 
 @kernel function assign_output_from_vector_of_tuples_kernel!(q_output::AT, p_output::AT, input::NamedTuple{(:q, :p), Tuple{AT, AT}}, indices::AbstractArray{Int, 2}, seq_length::Int) where {T, AT<:AbstractArray{T, 3}}
-    i, k = @index(Global, NTuple)
+    i, j, k = @index(Global, NTuple)
 
-    q_output[i, 1, k] = input.q[i, indices[1, k] + seq_length, indices[2, k]]
-    p_output[i, 1, k] = input.p[i, indices[1, k] + seq_length, indices[2, k]]
+    q_output[i, j, k] = input.q[i, indices[1, k] + seq_length + j - 1, indices[2, k]]
+    p_output[i, j, k] = input.p[i, indices[1, k] + seq_length + j - 1, indices[2, k]]
 end
 
 @kernel function assign_output_from_vector_of_tuples_kernel!(output::AT, data::AT, indices::AbstractArray{Int, 2}, seq_length::Int) where {T, AT<:AbstractArray{T, 3}}
-    i, k = @index(Global, NTuple)
+    i, j, k = @index(Global, NTuple)
 
-    output[i, 1, k] = data[i, indices[1, k] + seq_length, indices[2, k]]
+    output[i, j, k] = data[i, indices[1, k] + seq_length + j - 1, indices[2, k]]
 end
 
 """
 Takes the output of the batch functor and uses it to create the corresponding array (NamedTuples). 
 """
-function convert_input_and_batch_indices_to_array(dl::DataLoader{T, BT}, batch::Batch, batch_indices_tuple::Vector{Tuple{Int, Int}}) where {T, AT<:AbstractArray{T, 3}, BT<:NamedTuple{(:q, :p), Tuple{AT, AT}}}
+function convert_input_and_batch_indices_to_array(dl::DataLoader{T, BT}, batch::Batch{Int}, batch_indices_tuple::Vector{Tuple{Int, Int}}) where {T, AT<:AbstractArray{T, 3}, BT<:NamedTuple{(:q, :p), Tuple{AT, AT}}}
     backend = KernelAbstractions.get_backend(dl.input.q)
     q_input = KernelAbstractions.allocate(backend, T, dl.input_dim ÷ 2, batch.seq_length, length(batch_indices_tuple))
     p_input = similar(q_input)
@@ -196,13 +199,13 @@ function convert_input_and_batch_indices_to_array(dl::DataLoader{T, BT}, batch::
     batch_indices = typeof(batch_indices)(batch_indices_temp)
 
     assign_input_from_vector_of_tuples! = assign_input_from_vector_of_tuples_kernel!(backend)
-    assign_input_from_vector_of_tuples!(q_input, p_input, dl.input, batch_indices, ndrange=(dl.input_dim ÷ 2, batch.seq_length, length(batch_indices_tuple)))
+    assign_input_from_vector_of_tuples!(q_input, p_input, dl.input, batch_indices, ndrange=(dl.input_dim ÷ 2, batch.seq_length, batch.prediction_window, length(batch_indices_tuple)))
 
-    q_output = KernelAbstractions.allocate(backend, T, dl.input_dim ÷ 2, 1, length(batch_indices_tuple))
+    q_output = KernelAbstractions.allocate(backend, T, dl.input_dim ÷ 2, batch.prediction_window, length(batch_indices_tuple))
     p_output = similar(q_output)
 
     assign_output_from_vector_of_tuples! = assign_output_from_vector_of_tuples_kernel!(backend)
-    assign_output_from_vector_of_tuples!(q_output, p_output, dl.input, batch_indices, batch.seq_length, ndrange=(dl.input_dim ÷ 2, length(batch_indices_tuple)))
+    assign_output_from_vector_of_tuples!(q_output, p_output, dl.input, batch_indices, batch.seq_length, ndrange=(dl.input_dim ÷ 2, batch.prediction_window, length(batch_indices_tuple)))
 
     (q = q_input, p = p_input), (q = q_output, p = p_output)
 end
@@ -225,78 +228,10 @@ function convert_input_and_batch_indices_to_array(dl::DataLoader{T, BT}, batch::
     assign_input_from_vector_of_tuples! = assign_input_from_vector_of_tuples_kernel!(backend)
     assign_input_from_vector_of_tuples!(input, dl.input, batch_indices, ndrange=(dl.input_dim, batch.seq_length, length(batch_indices_tuple)))
 
-    output = KernelAbstractions.allocate(backend, T, dl.input_dim, 1, length(batch_indices_tuple))
+    output = KernelAbstractions.allocate(backend, T, dl.input_dim, batch.prediction_window, length(batch_indices_tuple))
 
     assign_output_from_vector_of_tuples! = assign_output_from_vector_of_tuples_kernel!(backend)
-    assign_output_from_vector_of_tuples!(output, dl.input, batch_indices, batch.seq_length, ndrange=(dl.input_dim, length(batch_indices_tuple)))
+    assign_output_from_vector_of_tuples!(output, dl.input, batch_indices, batch.seq_length, ndrange=(dl.input_dim, batch.prediction_window, length(batch_indices_tuple)))
 
     input, output
-end
-
-function optimize_for_one_epoch!(opt::Optimizer, model, ps::Union{Tuple, NamedTuple}, dl::DataLoader{T, CT, Nothing}, batch::Batch, loss) where {T, AT<:AbstractArray{T, 3}, BT<:NamedTuple{(:q, :p), Tuple{AT, AT}}, CT<:Union{AT, BT}}
-    count = 0
-    total_error = T(0)
-    batches = batch(dl)
-    @views for batch_indices in batches 
-        count += 1
-        # these `copy`s should not be necessary! coming from a Zygote problem!
-        input_nt, output_nt = convert_input_and_batch_indices_to_array(dl, batch, batch_indices)
-        loss_value, pullback = Zygote.pullback(ps -> loss(model, ps, input_nt, output_nt), ps)
-        total_error += loss_value
-        dp = pullback(one(loss_value))[1]
-        optimization_step!(opt, model, ps, dp)
-    end
-    total_error / count
-end
-
-function optimize_for_one_epoch!(opt::Optimizer, model, ps::Union{Tuple, NamedTuple}, dl::DataLoader{T, AT, BT}, batch::Batch) where {T, T1, AT<:AbstractArray{T, 3}, BT<:AbstractArray{T1, 3}}
-    optimize_for_one_epoch!(opt, model, ps, dl, batch, loss)
-end
-
-function optimize_for_one_epoch!(opt::Optimizer, nn::NeuralNetwork, dl::DataLoader, batch::Batch)
-    optimize_for_one_epoch!(opt, nn.model, nn.params, dl, batch)
-end
-
-"""
-This routine is called if a `DataLoader` storing *symplectic data* (i.e. a `NamedTuple`) is supplied.
-"""
-function optimize_for_one_epoch!(opt::Optimizer, model, ps::Union{Tuple, NamedTuple}, dl::DataLoader{T, AT}, batch::Batch, loss) where {T, AT<:NamedTuple}
-    count = 0 
-    total_error = T(0)
-    batches = batch(dl)
-    @views for batch_indices in batches 
-        count += 1
-        input_batch = (q=copy(dl.input.q[:,batch_indices]), p=copy(dl.input.p[:,batch_indices]))
-        # add +1 here !!!!
-        output_batch = (q=copy(dl.input.q[:,batch_indices.+1]), p=copy(dl.input.p[:,batch_indices.+1]))
-        loss_value, pullback = Zygote.pullback(ps -> loss(model, ps, input_batch, output_batch), ps)
-        total_error += loss_value 
-        dp = pullback(one(loss_value))[1]
-        optimization_step!(opt, model, ps, dp)
-    end
-    total_error / count
-end
-
-@doc raw"""
-A functor for `Optimizer`. It is called with:
-    - `nn::NeuralNetwork`
-    - `dl::DataLoader`
-    - `batch::Batch`
-    - `n_epochs::Int`
-    - `loss`
-
-The last argument is a function through which `Zygote` differentiates. This argument is optional; if it is not supplied `GeometricMachineLearning` defaults to an appropriate loss for the `DataLoader`.
-"""
-function (o::Optimizer)(nn::NeuralNetwork, dl::DataLoader, batch::Batch, n_epochs::Int, loss)
-    progress_object = ProgressMeter.Progress(n_epochs; enabled=true)
-    loss_array = zeros(n_epochs)
-    for i in 1:n_epochs
-        loss_array[i] = optimize_for_one_epoch!(o, nn.model, nn.params, dl, batch, loss)
-        ProgressMeter.next!(progress_object; showvalues = [(:TrainingLoss, loss_array[i])]) 
-    end
-    loss_array
-end
-
-function (o::Optimizer)(nn::NeuralNetwork, dl::DataLoader, batch::Batch, n_epochs::Int=1)
-    o(nn, dl, batch, n_epochs, loss)
 end
