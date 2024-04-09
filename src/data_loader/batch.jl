ce844840cab185b373c9b753793737808d88f0c4
benedict-96 <benedikt.brantner@icloud.com>
2024-04-09 11:56:09 +0200

    Moved routine to optimize.jl and shifted the order of inclusion of some files to make GML compile.

--- src/data_loader/batch.jl
+++ src/data_loader/batch.jl
@@ -84,40 +84,6 @@ function number_of_batches(dl::DataLoader{T, AT}, batch::Batch{<:Integer}) where
     Int(ceil((dl.input_time_steps - batch.seq_length) * dl.n_params / batch.batch_size))
 end
 
-@doc raw"""
-Optimize for an entire epoch. For this you have to supply: 
-- an instance of the optimizer.
-- the neural network model 
-- the parameters of the model 
-- the data (in form of `DataLoader`)
-- in instance of `Batch` that contains `batch_size` (and optionally `seq_length`)
-
-With the optional argument:
-- the loss, which takes the `model`, the parameters `ps` and an instance of `DataLoader` as input.
-
-The output of `optimize_for_one_epoch!` is the average loss over all batches of the epoch:
-```math
-output = \frac{1}{\mathtt{steps\_per\_epoch}}\sum_{t=1}^\mathtt{steps\_per\_epoch}loss(\theta^{(t-1)}).
-```
-This is done because any **reverse differentiation** routine always has two outputs: a pullback and the value of the function it is differentiating. In the case of zygote: `loss_value, pullback = Zygote.pullback(ps -> loss(ps), ps)` (if the loss only depends on the parameters).
-"""
-function optimize_for_one_epoch!(opt::Optimizer, model, ps::Union{Tuple, NamedTuple}, dl::DataLoader{T, AT, BT}, batch::Batch, loss) where {T, T1, AT<:AbstractArray{T, 3}, BT<:AbstractArray{T1, 3}}
-    count = 0
-    total_error = T(0)
-    batches = batch(dl)
-    @views for batch_indices in batches 
-        count += 1
-        # these `copy`s should not be necessary! coming from a Zygote problem!
-        input_batch = copy(dl.input[:, :, batch_indices])
-        output_batch = copy(dl.output[:, :, batch_indices])
-        loss_value, pullback = Zygote.pullback(ps -> loss(model, ps, input_batch, output_batch), ps)
-        total_error += loss_value
-        dp = pullback(one(loss_value))[1]
-        optimization_step!(opt, model, ps, dp)
-    end
-    total_error / count
-end
-
 @kernel function assign_input_from_vector_of_tuples_kernel!(q_input::AT, p_input::AT, input::NamedTuple{(:q, :p), Tuple{AT, AT}}, indices::AbstractArray{Int, 2}) where {T, AT<:AbstractArray{T, 3}}
     i, j, k = @index(Global, NTuple)
     
