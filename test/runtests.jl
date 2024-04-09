982ab0783219a59682d1f8b8767492ee7fef18a2
benedict-96 <benedikt.brantner@icloud.com>
2024-04-09 13:43:09 +0200

    Added new test.

--- test/runtests.jl
+++ test/runtests.jl
@@ -46,3 +46,5 @@ using SafeTestsets
 @safetestset "Test the data loader in combination with optimization_step!                     " begin include("data_loader/data_loader_optimization_step.jl") end
 @safetestset "Optimizer functor with data loader for Adam                                     " begin include("data_loader/optimizer_functor_with_adam.jl") end
 @safetestset "Test data loader for a tensor (q and p data)                                    " begin include("data_loader/draw_batch_for_tensor_test.jl") end
+
+@safetestset "Test NetworkLoss + Optimizer                                                    " begin include("network_losses/losses_and_optimization.jl") end
