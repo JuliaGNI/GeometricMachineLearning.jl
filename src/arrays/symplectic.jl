b8797a9d7658144c59e8884cfaaf6c1daf8a9ab6
benedict-96 <benedikt.brantner@icloud.com>
2024-04-12 14:25:00 +0200

    Updated the SymplecticPotential struct.

--- src/arrays/symplectic.jl
+++ src/arrays/symplectic.jl
@@ -12,23 +12,54 @@ Returns a symplectic matrix of size 2n x 2n
 \end{pmatrix}
 ```
 """
-function SymplecticPotential(backend, n2::Int, T::DataType=Float64)
+struct SymplecticPotential{T, AT} <: AbstractMatrix{T}
+    J::AT
+    n::Int
+end
+
+Base.getindex(𝕁::SymplecticPotential, i, j) = getindex(𝕁.J, i, j)
+
+Base.size(𝕁::SymplecticPotential) = size(𝕁.J)
+
+function SymplecticPotential(backend::Backend, n2::Int, T::DataType)
     @assert iseven(n2)
     n = n2÷2
     J = KernelAbstractions.zeros(backend, T, 2*n, 2*n)
     assign_ones_for_symplectic_potential! = assign_ones_for_symplectic_potential_kernel!(backend)
-    assign_ones_for_symplectic_potential!(J, n, ndrange=n)
-    J
+    assign_ones_for_symplectic_potential!(J, n, ndrange=n2)
+    
+    SymplecticPotential{T, typeof(J)}(J, n)
 end
 
-SymplecticPotential(n::Int, T::DataType=Float64) = SymplecticPotential(CPU(), n, T)
-SymplecticPotential(bakend, T::DataType, n::Int) = SymplecticPotential(backend, n, T)
+SymplecticPotential(n2::Int, T::DataType) = SymplecticPotential(CPU(), n2, T)
 
-SymplecticPotential(T::DataType, n::Int) = SymplecticPotential(n, T)
+SymplecticPotential(n2::Int) = SymplecticPotential(n2, Float64)
+
+SymplecticPotential(backend::Backend, n2::Int) = SymplecticPotential(backend, n2, Float32)
+
+SymplecticPotential(backend::CPU, n2::Int) = SymplecticPotential(backend, n2, Float64)
 
 @kernel function assign_ones_for_symplectic_potential_kernel!(J::AbstractMatrix{T}, n::Int) where T
     i = @index(Global)
     J[map_index_for_symplectic_potential(i, n)...] = i ≤ n ? one(T) : -one(T)
+end
+
+Base.:*(𝕁::SymplecticPotential{T}, v::NamedTuple{(:q, :p), Tuple{AT, AT}}) where {T, AT <: AbstractVecOrMat{T}} = (q = v.p, p = -v.q)
+
+function _vcat(v::NamedTuple{(:q, :p), Tuple{AT, AT}}) where {AT <: AbstractArray}
+    vcat(v.q, v.p)
+end
+
+Base.:*(𝕁::SymplecticPotential{T}, v::AbstractVector{T}) where T = _vcat(𝕁 * assign_q_and_p(v, 𝕁.n))
+Base.:*(𝕁::SymplecticPotential{T}, v::AbstractMatrix{T}) where T = _vcat(𝕁 * assign_q_and_p(v, 𝕁.n))
+
+
+function (𝕁::SymplecticPotential{T})(v₁::NT, v₂::NT) where {T, AT <: AbstractVector{T}, NT <: NamedTuple{(:q, :p), Tuple{AT, AT}}}
+    v₁.q' * v₂.p - v₁.p' * v₂.q
+end
+
+function (𝕁::SymplecticPotential{T})(v₁::AbstractVector{T}, v₂::AbstractVector{T}) where T 
+    𝕁(assign_q_and_p(v₁, 𝕁.n), assign_q_and_p(v₂, 𝕁.n))
 end
 
 """
@@ -36,8 +67,8 @@ This assigns the right index for the symplectic potential. To be used with `assi
 """
 function map_index_for_symplectic_potential(i::Int, n::Int)
     if i ≤ n
-        return (i, i+n)
+        return (i, i + n)
     else
-        return (i, i-n)
+        return (i, i - n)
     end
 end
