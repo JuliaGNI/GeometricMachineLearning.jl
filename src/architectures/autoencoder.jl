fde6055812e5a825a723433b82fc0b474b008a0b
benedict-96 <benedikt.brantner@icloud.com>
2024-04-24 18:04:12 +0200

    Moved more generic functionality to autoencoder.jl

--- src/architectures/autoencoder.jl
+++ src/architectures/autoencoder.jl
@@ -3,58 +3,83 @@ Abstract `AutoEncoder` type. If a custom `<:AutoEncoder` architecture is impleme
 """
 abstract type AutoEncoder <: Architecture end
 
+"""
+Abstract `Encoder` type. If a custom `<:Encoder` architecture is implemented it should have the fields `full_dim`, `reduced_dim` and `n_encoder_blocks`.
+"""
 abstract type Encoder <: Architecture end 
 
+"""
+Abstract `Decoder` type. If a custom `<:Decoder` architecture is implemented it should have the fields `full_dim`, `reduced_dim` and `n_decoder_blocks`.
+"""
 abstract type Decoder <: Architecture end
 
-struct UnknownEncoder <: Encoder end 
+struct UnknownEncoder <: Encoder 
+    full_dim::Int
+    reduced_dim::Int
+    n_encoder_blocks::Int
+end 
 
-struct UnknownDecoder <: Decoder end
-
-#=
-"""
-Returns a chain that contains the layers of the encoder. 
-"""
-get_encoder(::AutoEncoder) = error("`get_encoder` not implemented for this type of autoencoder.")
-"""
-Returns a chain that contains the layers of the decoder. 
-"""
-get_decoder(::AutoEncoder) = error("`get_decoder` not implemented for this type of autoencoder.")
+struct UnknownDecoder <: Decoder 
+    full_dim::Int 
+    reduced_dim::Int 
+    n_decoder_blocks::Int
+end
 
 """
-Returns the parameters of the encoder. 
+This function gives iterations from the full dimension to the reduced dimension (i.e. the intermediate steps). The iterations are given in ascending order. 
 """
-get_encoder_parameters(::NeuralNetwork{<:AutoEncoder}) = error("`get_encoder_parameters` not implemented for this type of autoencoder.")
-"""
-Returns the parameters of the decoder. 
-"""
-get_decoder_parameters(::NeuralNetwork{<:AutoEncoder}) = error("`get_decoder_parameters` not implemented for this type of autoencoder.")
-=#
+function compute_iterations(full_dim::Integer, reduced_dim::Integer, n_blocks::Integer)
+    iterations = Vector{Int}(reduced_dim : (full_dim - reduced_dim) ÷ (n_blocks - 1) : full_dim)
+    iterations[end] = full_dim
+    iterations
+end
 
-function get_encoder(arch::AutoEncoder)
-    encoder_iterations = reverse(compute_iterations(arch.full_dim, arch.reduced_dim, arch.n_encoder_blocks))
+function compute_encoder_iterations(arch::AutoEncoder)
+    compute_iterations(arch.full_dim, arch.reduced_dim, arch.n_encoder_blocks)
+end
+
+function compute_decoder_iterations(arch::AutoEncoder)
+    compute_iterations(arch.full_dim, arch.reduced_dim, arch.n_decoder_blocks)
+end
+
+"""
+Takes as input the autoencoder architecture and a vector of integers specifying the layer dimensions in the encoder. Has to return a tuple of `AbstractExplicitLayer`s.
+"""
+encoder_layers_from_iteration(::AutoEncoder, ::AbstractVector{<:Integer}) = error("You have to implement `encoder_layers_from_iteration` for this autoencoder architecture!")
+
+"""
+Takes as input the autoencoder architecture and a vector of integers specifying the layer dimensions in the decoder. Has to return a tuple of `AbstractExplicitLayer`s.
+"""
+decoder_layers_from_iteration(::AutoEncoder, ::AbstractVector{<:Integer}) = error("You have to implement `decoder_layers_from_iteration` for this autoencoder architecture!")
+
+function get_encoder_model(arch::AutoEncoder)
+    encoder_iterations = reverse(compute_encoder_iterations(arch))
     Chain(encoder_layers_from_iteration(arch, encoder_iterations)...)
 end
 
-function get_decoder(arch::AutoEncoder)
-    decoder_iterations = compute_iterations(arch.full_dim, arch.reduced_dim, arch.n_decoder_blocks)
+function get_decoder_model(arch::AutoEncoder)
+    decoder_iterations = compute_decoder_iterations(arch)
     Chain(decoder_layers_from_iteration(arch, decoder_iterations)...)
 end
 
 function get_encoder_parameters(nn::NeuralNetwork{<:AutoEncoder})
-    n_encoder_layers = length(get_encoder(nn.architecture).layers)
+    n_encoder_layers = length(get_encoder_model(nn.architecture).layers)
     nn.params[1:n_encoder_layers]
 end
 
 function get_decoder_parameters(nn::NeuralNetwork{<:AutoEncoder})
-    n_decoder_layers = length(get_decoder(nn.architecture).layers)
-    nn.params[(end - n_decoder_layers):end]
+    n_decoder_layers = length(get_decoder_model(nn.architecture).layers)
+    nn.params[(end - (n_decoder_layers - 1)):end]
+end
+
+function Chain(arch::AutoEncoder)
+    Chain(get_encoder_model(arch).layers..., get_decoder_model(arch).layers...)
 end
 
 function get_encoder(nn::NeuralNetwork{<:AutoEncoder})
-    NeuralNetwork(UnknownEncoder(), get_encoder(nn.architecture), get_encoder_parameters(nn), get_backend(nn))
+    NeuralNetwork(UnknownEncoder(nn.architecture.full_dim, nn.architecture.reduced_dim, nn.architecture.n_encoder_blocks), get_encoder_model(nn.architecture), get_encoder_parameters(nn), get_backend(nn))
 end
 
 function get_decoder(nn::NeuralNetwork{<:AutoEncoder})
-    NeuralNetwork(UnknownDecoder(), get_decoder(nn.architecture), get_decoder_parameters(nn), get_backend(nn))
+    NeuralNetwork(UnknownDecoder(nn.architecture.full_dim, nn.architecture.reduced_dim, nn.architecture.n_encoder_blocks), get_decoder_model(nn.architecture), get_decoder_parameters(nn), get_backend(nn))
 end
