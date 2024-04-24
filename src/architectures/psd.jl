dc05a272ec1a74f5b6948daa3a94f32de4dfd5d3
benedict-96 <benedikt.brantner@icloud.com>
2024-04-24 11:28:50 +0200

    PSD -> PSDArch.

--- src/architectures/psd.jl
+++ src/architectures/psd.jl
@@ -1,32 +1,32 @@
-struct PSD <: AutoEncoder 
+struct PSDArch <: AutoEncoder 
     full_dim::Int 
     reduced_dim::Int 
     n_encoder_blocks::Int 
     n_decoder_blocks::Int
 end
 
-function PSD(full_dim::Integer, reduced_dim::Integer)
-    PSD(full_dim, reduced_dim, 1, 1)
+function PSDArch(full_dim::Integer, reduced_dim::Integer)
+    PSDArch(full_dim, reduced_dim, 1, 1)
 end
 
-function Chain(arch::PSD)
+function Chain(arch::PSDArch)
     Chain(Linear(arch.full_dim, arch.reduced_dim; use_bias = false), Linear(arch.reduced_dim, arch.full_dim; use_bias = false))
 end
 
 #=
-get_ecoder(arch::PSD) = Linear(arch.full_dim, arch.reduced_dim)
+get_ecoder(arch::PSDArch) = Linear(arch.full_dim, arch.reduced_dim)
 
-get_decoder(arch::PSD) = Linear(arch.reduced_dim, arch.full_dim)
+get_decoder(arch::PSDArch) = Linear(arch.reduced_dim, arch.full_dim)
 
-get_encoder_parameters(nn::NeuralNetwork{<:PSD}) = nn.params[1]
+get_encoder_parameters(nn::NeuralNetwork{<:PSDArch}) = nn.params[1]
 
-get_decoder_parameters(nn::NeuralNetwork{<:PSD}) = nn.params[2]
+get_decoder_parameters(nn::NeuralNetwork{<:PSDArch}) = nn.params[2]
 =#
 
-function solve!(nn::NeuralNetwork{<:PSD}, dl::DataLoader{T, AT, <:Any, :RegularData}) where {T, AT <: AbstractArray{T}}
+function solve!(nn::NeuralNetwork{<:PSDArch}, dl::DataLoader{T, AT, <:Any, :RegularData}) where {T, AT <: AbstractArray{T}}
     #nn dl.input
 end
 
-function solve!(nn::NeuralNetwork{<:PSD}, dl::DataLoader{T, NT, <:Any, :RegularData}) where {T, AT <: AbstractArray{T}, NT <: NamedTuple{(:q, :p), {AT, AT}}}
+function solve!(nn::NeuralNetwork{<:PSDArch}, dl::DataLoader{T, NT, <:Any, :RegularData}) where {T, AT <: AbstractArray{T}, NT <: NamedTuple{(:q, :p), {AT, AT}}}
 
 end
