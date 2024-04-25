01f504109d146ac0dc25ac5fa3724e320d2544fd
benedict-96 <benedikt.brantner@icloud.com>
2024-04-25 10:35:22 +0200

    Added type stability for all fields in struct.

--- src/reduced_system/reduced_system.jl
+++ src/reduced_system/reduced_system.jl
@@ -1,69 +1,59 @@
 @doc raw"""
 ReducedSystem computes the reconstructed dynamics in the full system based on the reduced one. Optionally it can be compared to the FOM solution.
 
-It can be called using the following constructor: ReducedSystem(N, n, encoder, decoder, full_vector_field, reduced_vector_field, params, tspan, tstep, ics, projection_error) where 
-- encoder: a function $\mathbb{R}^{2N}\mapsto{}\mathbb{R}^{2n}$
-- decoder: a (differentiable) function $\mathbb{R}^{2n}\mapsto\mathbb{R}^{2N}$
-- full_vector_field: a (differentiable) mapping defined the same way as in GeometricIntegrators 
-- reduced_vector_field: a (differentiable) mapping defined the same way as in GeometricIntegrators 
-- params: a NamedTuple that parametrizes the vector fields (the same for full_vector_field and reduced_vector_field)
-- tspan: a tuple (t₀, tₗ) that specifies start and end point of the time interval over which integration is performed. 
-- tstep: the time step 
-- ics: the initial condition for the big system.
-- projection_error: the error $||M - \mathcal{R}\circ\mathcal{P}(M)||$ where $M$ is the snapshot matrix; $\mathcal{P}$ and $\mathcal{R}$ are the reduction and reconstruction respectively.
+It can be called using the following constructor: `ReducedSystem(N, n; encoder, decoder, full_vector_field, reduced_vector_field, parameters, tspan, tstep, ics, projection_error)` where 
+- `encoder`: a function ``\mathbb{R}^{2N}\mapsto{}\mathbb{R}^{2n}``
+- `decoder`: a (differentiable) function ``\mathbb{R}^{2n}\mapsto\mathbb{R}^{2N}``
+- `full_vector_field`: a (differentiable) mapping defined the same way as in GeometricIntegrators 
+- `reduced_vector_field`: a (differentiable) mapping defined the same way as in GeometricIntegrators 
+- `parameters`: a NamedTuple that parametrizes the vector fields (the same for full_vector_field and reduced_vector_field)
+- `tspan`: a tuple `(t₀, tₗ)` that specifies start and end point of the time interval over which integration is performed. 
+- `tstep`: the time step 
+- `ics`: the initial condition for the big system.
+- `projection_error`: the error ``||M - \mathcal{R}\circ\mathcal{P}(M)||`` where ``M`` is the snapshot matrix; ``\mathcal{P}$ and $\mathcal{R}`` are the reduction and reconstruction respectively.
 """
-struct ReducedSystem{T, ST<:SystemType, ET <: Encoder, DT <: Decoder, IT} 
-    N::Integer 
-    n::Integer
+struct ReducedSystem{T, ET <: Encoder, DT <: Decoder, FT, RT, InT, PT, IT <: Union{AT, NamedTuple{(:q, :p), Tuple{AT, AT}}}} where AT <: AbstractArray{T} 
+    N::Int 
+    n::Int
     encoder::ET
     decoder::DT
-    full_vector_field
-    reduced_vector_field 
-    integrator 
-    params
+    full_vector_field::FT
+    reduced_vector_field::RT 
+    integrator::InT 
+    parameters::PT
     tspan::Tuple{Int, Int} 
     tstep::T
     ics::IT
 
-    function ReducedSystem(N::Integer, n::Integer, encoder, decoder, full_vector_field, reduced_vector_field, params, tspan, tstep, ics; integrator=ImplicitMidpoint(), system_type=Symplectic(), T=Float64) 
-        new{T, typeof(system_type), typeof(encoder), typeof(decoder), typeof(ics)}(N, n, encoder, decoder, full_vector_field, reduced_vector_field, integrator, params, tspan, tstep, ics)
+    function ReducedSystem(N::Integer, n::Integer; encoder::Encoder, decoder::Decoder, full_vector_field, reduced_vector_field, parameters, tspan::Tuple, tstep::Real, ics, integrator=ImplicitMidpoint()) 
+        new{typeof(tstep), typeof(encoder), typeof(decoder), typeof(full_vector_field), typeof(reduced_vector_field), typeof(integrator), typeof(parameters), typeof(ics)}(N, n, encoder, decoder, full_vector_field, reduced_vector_field, integrator, parameters, tspan, tstep, ics)
     end
 end
 
-function ReducedSystem(N::Integer, n::Integer, encoder, decoder, full_vector_field, params, tspan, tstep, ics; integrator=ImplicitMidpoint(), system_type=Symplectic(), T=Float64) 
-    ReducedSystem{T, typeof(system_type), typeof(encoder), typeof(decoder), typeof(ics)}(
-        N, n, encoder, decoder, full_vector_field, build_reduced_vector_field(full_vector_field, decoder, N, n, T), integrator, params, tspan, tstep, ics
-    )
+function ReducedSystem(N::Integer, n::Integer; encoder::Encoder, decoder::Decoder, full_vector_field, parameters, tspan::Tuple, tstep::T, ics, integrator=ImplicitMidpoint()) where T
+    ReducedSystem(N, n; encoder = encoder, decoder = decoder, full_vector_field = full_vector_field, reduced_vector_fields = build_reduced_vector_field(full_vector_field, decoder, N, n, T), integrator = integrator, parameters = parameters, tspan = tspan, tstep = tstep, ics = ics)
 end
 
-function build_reduced_vector_field(full_vector_field, decoder, N, n, T=Float64)
-    function reduced_vector_field(v, t, ξ, params)
-        v_intermediate = zeros(T, 2*N)
-        full_vector_field(v_intermediate, t, decoder(ξ), params)
-        v .= -SymplecticPotential(n)*ForwardDiff.jacobian(decoder, ξ)'*SymplecticPotential(N)*v_intermediate
-    end
-    reduced_vector_field
-end
-
-@doc raw"""
-This function is needed if we obtain a GeometricIntegrators-like vector field from an explicit vector field $V:\mathbb{R}^{2N}\to\mathbb{R}^{2N}$. 
-We need this function because build_reduced_vector_field is not working in conjunction with implicit integrators.
-"""
-function reduced_vector_field_from_full_explicit_vector_field(full_explicit_vector_field, decoder, N::Integer, n::Integer)
-    function reduced_vector_field(v, t, ξ, params)
-        v .= -SymplecticPotential(n) * ForwardDiff.jacobian(decoder, ξ)' * SymplecticPotential(N) * full_explicit_vector_field(t, decoder(ξ), params)
+function build_reduced_vector_field(full_vector_field, decoder::SymplecticDecoder, N::Integer, n::Integer, T::DataType=Float64)
+    @assert iseven(N) && iseven(n) "Full dimension and reduced dimension must be even!"
+    N2 = N ÷ 2 
+    n2 = n ÷ 2
+    function reduced_vector_field(v, t, ξ, parameters)
+        v_intermediate = zeros(T, N)
+        full_vector_field(v_intermediate, t, decoder(ξ), parameters)
+        v .= -SymplecticPotential(n2) * ForwardDiff.jacobian(decoder, ξ)' * SymplecticPotential(N2) * v_intermediate
     end
     reduced_vector_field
 end
 
 function perform_integration_reduced(rs::ReducedSystem)
     ics_reduced = rs.encoder(rs.ics)
-    ode = ODEProblem(rs.reduced_vector_field, parameters=rs.params, rs.tspan, rs.tstep, ics_reduced)
+    ode = ODEProblem(rs.reduced_vector_field, parameters=rs.parameters, rs.tspan, rs.tstep, ics_reduced)
     integrate(ode, rs.integrator)
 end
 
 function perform_integration_full(rs::ReducedSystem)
-    ode = ODEProblem(rs.full_vector_field, parameters=rs.params, rs.tspan, rs.tstep, rs.ics)
+    ode = ODEProblem(rs.full_vector_field, parameters=rs.parameters, rs.tspan, rs.tstep, rs.ics)
     integrate(ode, rs.integrator)
 end
 
