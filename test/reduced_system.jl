d9fe5c69f5be6c0f0454914b29c41584292969ae
benedict-96 <benedikt.brantner@icloud.com>
2024-04-30 11:32:14 +0200

    Only the reduced dimension can be picked here. The actual system dimension must conform with the problem.

--- test/reduced_system.jl
+++ test/reduced_system.jl
@@ -2,31 +2,32 @@ using GeometricMachineLearning
 using GeometricProblems.CoupledHarmonicOscillator: hodeproblem
 using GeometricIntegrators: integrate, ImplicitMidpoint
 using Test
-import Random 
+import Random
 
 Random.seed!(123)
 
-function test_reduced_vector_fields(full_dim::Integer, reduced_dim::Integer, time_steps::Integer=100, n_parameters::Integer=5)
+function test_reduced_vector_fields(reduced_dim::Integer)
     pr = hodeproblem()
 
     sol = integrate(pr, ImplicitMidpoint())
 
     dl = DataLoader(sol)
 
-    model1 = PSDArch(full_dim, reduced_dim)
+    model1 = PSDArch(dl.input_dim, reduced_dim)
 
-    model2 = SymplecticAutoencoder(full_dim, reduced_dim)
+    model2 = SymplecticAutoencoder(dl.input_dim, reduced_dim)
 
     nn1 = NeuralNetwork(model1)
 
     nn2 = NeuralNetwork(model2)
 
-    rs1 = ReducedSystem(hodeproblem(); encoder = get_encoder(nn1), decoder = get_decoder(nn1))
+    rs1 = HRedSys(hodeproblem(), get_encoder(nn1), get_decoder(nn1))
 
-    rs2 = ReducedSystem(hodeproblem(); encoder = get_encoder(nn2), decoder = get_decoder(nn2))
+    rs2 = HRedSys(hodeproblem(), get_encoder(nn2), get_decoder(nn2))
 
-    # we expect the neural network reduction error to be higher than the PSD reduction error if we don't train
+    # we expect the neural network projection & reduction errors to be higher than the PSD projection & reduction errors if we don't train
+    @test compute_projection_error(rs1) < compute_projection_error(rs2)
     @test compute_reduction_error(rs1) < compute_reduction_error(rs2)
 end
 
-test_reduced_vector_fields(20, 10)
+test_reduced_vector_fields(2)
