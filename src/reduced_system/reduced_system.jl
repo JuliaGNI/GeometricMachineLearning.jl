dc85b4714cee29147926f63491b2ad9e6633a814
benedict-96 <benedikt.brantner@icloud.com>
2024-05-02 17:08:13 +0200

    Implemented some hacks to get around ForwardDiff limitations.

--- src/reduced_system/reduced_system.jl
+++ src/reduced_system/reduced_system.jl
@@ -53,10 +53,21 @@ end
 function HRedSys(odeproblem::Union{HODEProblem, HODEEnsemble}, encoder::NeuralNetwork{<:Encoder}, decoder::NeuralNetwork{<:Decoder}; integrator=ImplicitMidpoint()) 
     N = encoder.architecture.full_dim 
     n = encoder.architecture.reduced_dim
-    f_eq = odeproblem.equation.f
     v_eq = odeproblem.equation.v
+    f_eq = odeproblem.equation.f
     h_eq = odeproblem.equation.hamiltonian
-    HRedSys(N, n, encoder, decoder, f_eq, v_eq, h_eq, odeproblem.tspan, odeproblem.tstep, odeproblem.ics; parameters = odeproblem.parameters, integrator = integrator)
+    HRedSys(N, n, encoder, decoder, v_eq, f_eq, h_eq, odeproblem.tspan, odeproblem.tstep, odeproblem.ics; parameters = odeproblem.parameters, integrator = integrator)
+end
+
+# this is much more expensive than it has to be and is due to a problem with nested derivatives in ForwardDiff (should not be necessary to do this twice!)
+function evaluate_vf_and_compute_∇Ψ(t, q̃::AbstractVector{T}, p̃::AbstractVector{T}, parameters, v_full, f_full) where T
+    v_intermediate = zeros(T, N2)
+    f_intermediate = zeros(T, N2)
+    decoded_nt = decoder((q = q̃, p = p̃))
+    v_full(v_intermediate, t, decoded_nt..., parameters)
+    f_full(f_intermediate, t, decoded_nt..., parameters)
+    ∇Ψ = ForwardDiff.jacobian(qp -> decoder(qp), vcat(q̃, p̃)) 
+    v_intermediate, f_intermediate, ∇Ψ 
 end
 
 @doc raw"""
@@ -67,15 +78,12 @@ We then get
 ```
 """
 function build_v_reduced(v_full, f_full, decoder::NeuralNetwork{<:SymplecticDecoder})
-    T = _eltype(decoder.params)
     N2 = decoder.architecture.full_dim ÷ 2 
-    function v_reduced(v, t, q̃, p̃, parameters)
-        v_intermediate = zeros(T, N2)
-        f_intermediate = zeros(T, N2)
-        v_full(v_intermediate, t, decoder((q = q̃, p = p̃))..., parameters)
-        f_full(f_intermediate, t, decoder((q = q̃, p = p̃))..., parameters)
-        ∇₂Ψ₁ = Zygote.jacobian(p -> decoder(q̃, p)[1], p̃)[1]
-        ∇₂Ψ₂ = Zygote.jacobian(p -> decoder(q̃, p)[2], p̃)[1]
+    n2 = decoder.architecture.reduced_dim ÷ 2
+    function v_reduced(v, t, q̃::AbstractVector{T}, p̃::AbstractVector{T}, parameters) where T
+        v_intermediate, f_intermediate, ∇Ψ = evaluate_vf_and_compute_∇Ψ(t, q̃, p̃, parameters, v_full, f_full)
+        ∇₂Ψ₁ = @view ∇Ψ[1:N2, (n2 + 1):(2 * n2)]
+        ∇₂Ψ₂ = @view ∇Ψ[(N2 + 1):(2 * N2), (n2 + 1):(2 * n2)]
         v .= -∇₂Ψ₁' * f_intermediate + ∇₂Ψ₂' * v_intermediate
 
         nothing
@@ -84,15 +92,12 @@ function build_v_reduced(v_full, f_full, decoder::NeuralNetwork{<:SymplecticDeco
 end
 
 function build_f_reduced(v_full, f_full, decoder::NeuralNetwork{<:SymplecticDecoder})
-    T = _eltype(decoder.params)
     N2 = decoder.architecture.full_dim ÷ 2 
-    function f_reduced(f, t, q̃, p̃, parameters)
-        v_intermediate = zeros(T, N2)
-        f_intermediate = zeros(T, N2)
-        v_full(v_intermediate, t, decoder((q = q̃, p = p̃))..., parameters)
-        f_full(f_intermediate, t, decoder((q = q̃, p = p̃))..., parameters)
-        ∇₁Ψ₁ = Zygote.jacobian(q -> decoder(q, p̃)[1], q̃)[1]
-        ∇₁Ψ₂ = Zygote.jacobian(q -> decoder(q, p̃)[2], q̃)[1]
+    n2 = decoder.architecture.reduced_dim ÷ 2
+    function f_reduced(f, t, q̃::AbstractVector{T}, p̃::AbstractVector{T}, parameters) where T 
+        v_intermediate, f_intermediate, ∇Ψ = evaluate_vf_and_compute_∇Ψ(t, q̃, p̃, parameters, v_full, f_full)
+        ∇₁Ψ₁ = @view ∇Ψ[1:N2, 1:n2]
+        ∇₁Ψ₂ = @view ∇Ψ[(N2 + 1):(2 * N2), 1:n2]
         f .= ∇₁Ψ₁' * f_intermediate - ∇₁Ψ₂' * v_intermediate
 
         nothing
@@ -107,7 +112,7 @@ function build_h_reduced(h_full, decoder::NeuralNetwork{<:SymplecticDecoder})
     h_reduced
 end
 
-function perform_integration_reduced(rs::HRedSys)
+function integrate_reduced_system(rs::HRedSys)
     ics_reduced_nt = rs.encoder(rs.ics)
     # convert to StateVariable format 
     ics_reduced = (q = StateVariable(ics_reduced_nt.q), p = StateVariable(ics_reduced_nt.p))
@@ -115,18 +120,20 @@ function perform_integration_reduced(rs::HRedSys)
     integrate(hode, rs.integrator)
 end
 
-function perform_integration_full(rs::HRedSys)
+function integrate_full_system(rs::HRedSys)
     hode = HODEProblem(rs.v_full, rs.f_full, rs.h_full, rs.tspan, rs.tstep, rs.ics; parameters = rs.parameters)
     integrate(hode, rs.integrator)
 end
 
 # compute reduction error for the q part 
-function reduction_error(rs::HRedSys, sol_full=perform_integration_full(rs), sol_red=perform_integration_reduced(rs))
-    norm(rs.decoder(sol_red.q) - sol_full.q)/norm(sol_full.q)
+function reduction_error(rs::HRedSys, sol_full=integrate_full_system(rs), sol_reduced=integrate_reduced_system(rs))
+    sol_full_matrices = data_matrices_from_geometric_solution(sol_full)
+    sol_reduced_matrices = data_matrices_from_geometric_solution(sol_reduced)
+    _norm(_diff(rs.decoder(sol_reduced_matrices), sol_full_matrices)) / _norm(sol_full_matrices)
 end
 
 # compute projection error for the q part 
-function projection_error(rs::HRedSys, sol_full=perform_integration_full(rs))
+function projection_error(rs::HRedSys, sol_full=integrate_full_system(rs))
     sol_full_matrices = data_matrices_from_geometric_solution(sol_full)
     _norm(_diff(rs.decoder(rs.encoder(sol_full_matrices)), sol_full_matrices)) / _norm(sol_full_matrices)
 end
