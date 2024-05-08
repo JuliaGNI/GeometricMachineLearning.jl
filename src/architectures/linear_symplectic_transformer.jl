32d2bf603c7996129c707cd84a713efe3c410f5a
benedict-96 <benedikt.brantner@icloud.com>
2024-05-08 11:08:57 +0200

    Unified interfaces of LinearSymplecticTransformer and VolumePreservingTransformer (including docs).

--- src/architectures/linear_symplectic_transformer.jl
+++ src/architectures/linear_symplectic_transformer.jl
@@ -1,30 +1,30 @@
 @doc raw"""
 Realizes the linear Symplectic Transformer.
 
-Fields: 
-- `dim::Int`: System dimension 
-- `time_steps::Int`: Number of time steps that the transformer considers. 
-- `depth::Int`: The number of SympNet layers that is applied. 
-- `nhidden::Int`: The number of hidden layers in the transformer.
-- `upscaling_dimension::Int`: The upscaling that is done by the gradient layer. 
-- `activation`: The activation function for the SympNet layers. 
-- `init_upper::Bool`: Specifies if the first layer is a ``Q``-type layer (`init_upper=true`) or if it is a ``P``-type layer (`init_upper=false`).
+### Constructor: 
+
+The constructor is called with the following arguments
+1. `dim::Int`: System dimension 
+2. `seq_length::Int`: Number of time steps that the transformer considers. 
+
+Optional keyword arguments:
+- `n_sympnet::Int=2`: The number of sympnet layers in the transformer.
+- `upscaling_dimension::Int=2*dim`: The upscaling that is done by the gradient layer. 
+- `L::Int=1`: The number of transformer units. 
+- `activation=tanh`: The activation function for the SympNet layers. 
+- `init_upper::Bool=true`: Specifies if the first layer is a ``Q``-type layer (`init_upper=true`) or if it is a ``P``-type layer (`init_upper=false`).
 """
-struct LinearSymplecticTransformer{AT, InitUpper} <: Architecture where AT 
+struct LinearSymplecticTransformer{AT} <: TransformerIntegrator where AT 
     dim::Int
-    time_steps::Int
-    depth::Int
-    nhidden::Int
+    sew_length::Int
+    n_sympnet::Int
     upscaling_dimension::Int
+    L::Int
     activation::AT
-    seq_length::Int
+    init_upper::Bool
 
-    function LinearSymplecticTransformer(dim::Int, time_steps::Int; depth::Int=5, nhidden::Int=2, upscaling_dimension::Int=2*dim, activation=tanh, init_upper::Bool=true, seq_length::Int=5)
-        new{typeof(tanh), init_upper}(dim, time_steps, depth, nhidden, upscaling_dimension, activation, seq_length)
-    end
-
-    function LinearSymplecticTransformer(dl::DataLoader{T, BT}; depth::Int=5, nhidden::Int=4, upscaling_dimension::Int=2*dl.input_dim, activation=tanh, init_upper::Bool=true, seq_length::Int=5) where {T, AT<:AbstractArray{T}, BT<:NamedTuple{(:q, :p), Tuple{AT, AT}}}
-        new{typeof(tanh), init_upper}(dl.input_dim, dl.input_time_steps, depth, nhidden, upscaling_dimension, activation, seq_length)
+    function LinearSymplecticTransformer(dim::Int, seq_length::Int; n_sympnet::Int=5, upscaling_dimension::Int=2*dim, L::Int=1, activation=tanh, init_upper::Bool=true)
+        new{typeof(tanh)}(dim, seq_length, n_sympnet, upscaling_dimension, L, activation, init_upper)
     end
 end
 
@@ -56,36 +56,4 @@ function Chain(arch::LinearSymplecticTransformer{AT, false}) where AT
         end
     end
     Chain(layers...)
-end
-
-@doc raw"""
-This function computes a trajectory for a LinearSymplecticTransformer that has already been trained for valuation purposes.
-
-It takes as input: 
-- `nn`: a `NeuralNetwork` (that has been trained).
-- `ics`: initial conditions (a `NamedTuple` of two matrices)
-"""
-function iterate(nn::NeuralNetwork{<:LinearSymplecticTransformer}, ics::NamedTuple{(:q, :p), Tuple{AT, AT}}; n_points = 100) where {T, AT<:AbstractVector{T}}
-
-    seq_length = nn.model.seq_length
-    n_dim = length(ics.q)
-    backend = KernelAbstractions.get_backend(ics.q)
-
-    # Array to store the predictions
-    q_valuation = KernelAbstractions.allocate(backend, T, n_dim, n_points)
-    p_valuation = KernelAbstractions.allocate(backend, T, n_dim, n_points)
-    
-    # Initialisation
-    @views q_valuation[:,1:seq_length] = ics.q
-    @views p_valuation[:,1:seq_length] = ics.p
-    
-    #Computation of phase space
-    @views for i in (seq_length + 1):n_points
-        qp_temp = (q=q_valuation[:, (i - seq_length):(i - 1)], p=p_valuation[:, (i - seq_length):(i - 1)]) 
-        qp_prediction = nn(qp_temp)
-        q_valuation[:, i] = qp_prediction.q
-        p_valuation[:, i] = qp_prediction.p
-    end
-
-    (q=q_valuation, p=p_valuation)
 end
