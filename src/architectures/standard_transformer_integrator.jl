2ff0b515744c67aa5f241283f0cb9924d5f06147
benedict-96 <benedikt.brantner@icloud.com>
2024-05-11 16:07:29 +0200

    Renamed file.

--- src/architectures/standard_transformer_integrator.jl
+++ src/architectures/standard_transformer_integrator.jl
@@ -11,7 +11,7 @@ The constructor is called with the following arguments:
 - `resnet_activation`: by default tanh
 - `add_connection:Bool=true` (keyword argument): if the input should be added to the output.
 """
-struct RegularTransformerIntegrator{AT1, AT2} <: TransformerIntegrator
+struct StandardTransformerIntegrator{AT1, AT2} <: TransformerIntegrator
     sys_dim::Int 
     transformer_dim::Int 
     n_heads::Int
@@ -22,15 +22,15 @@ struct RegularTransformerIntegrator{AT1, AT2} <: TransformerIntegrator
     add_connection::Bool
 end
 
-# function RegularTransformerIntegrator(sys_dim::Int, transformer_dim::Int = sys_dim, n_heads::Int = sys_dim, n_blocks = 1, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh; add_connection::Bool = true)
-#     RegularTransformerIntegrator{typeof(upscaling_activation), typeof(resnet_activation)}(sys_dim, transformer_dim, n_heads, n_blocks, L, upscaling_activation, resnet_activation, add_connection)
+# function StandardTransformerIntegrator(sys_dim::Int, transformer_dim::Int = sys_dim, n_heads::Int = sys_dim, n_blocks = 1, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh; add_connection::Bool = true)
+#     StandardTransformerIntegrator{typeof(upscaling_activation), typeof(resnet_activation)}(sys_dim, transformer_dim, n_heads, n_blocks, L, upscaling_activation, resnet_activation, add_connection)
 # end
 
-function RegularTransformerIntegrator(sys_dim::Int, transformer_dim::Int = sys_dim, n_heads::Int = sys_dim; n_blocks = 1, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh, add_connection::Bool = true)
-    RegularTransformerIntegrator(sys_dim, transformer_dim, n_heads, n_blocks, L, upscaling_activation, resnet_activation, add_connection)
+function StandardTransformerIntegrator(sys_dim::Int, transformer_dim::Int = sys_dim, n_heads::Int = sys_dim; n_blocks = 1, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh, add_connection::Bool = true)
+    StandardTransformerIntegrator(sys_dim, transformer_dim, n_heads, n_blocks, L, upscaling_activation, resnet_activation, add_connection)
 end
 
-function Chain(arch::RegularTransformerIntegrator)
+function Chain(arch::StandardTransformerIntegrator)
     layers = arch.sys_dim == arch.transformer_dim ? () : (Dense(arch.sys_dim, arch.transformer_dim, arch.upsacling_activation), )
     for _ in 1:arch.L 
         layers = (layers..., MultiHeadAttention(arch.transformer_dim, arch.n_heads; add_connection = arch.add_connection))
