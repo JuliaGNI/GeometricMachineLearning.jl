207aefb99ec49fcca68a0b770294d3445c117f06
benedict-96 <benedikt.brantner@icloud.com>
2024-05-13 23:45:03 +0200

    Adjusted to new interface (GSympNet).

--- test/train!/test_training.jl
+++ test/train!/test_training.jl
@@ -9,7 +9,7 @@ include("../macro_testerror.jl")
 #########################################
 
 training_data = tra_ps_data
-nn = NeuralNetwork(GSympNet(2; nhidden=2), Float64)
+nn = NeuralNetwork(GSympNet(2; n_layers=2), Float64)
 mopt = GradientOptimizer()
 method = BasicSympNet()
 nruns = 1
