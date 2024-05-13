626906a800295bdf9937297c183fdb98d10a37f4
benedict-96 <benedikt.brantner@icloud.com>
2024-05-13 23:02:04 +0200

    Everything instead of the system dimension is now a keyword argument.

--- src/architectures/standard_transformer_integrator.jl
+++ src/architectures/standard_transformer_integrator.jl
@@ -1,15 +1,16 @@
 @doc raw"""
 The regular transformer used as an integrator (multi-step method). 
 
-The constructor is called with the following arguments: 
+The constructor is called with one argument: 
 - `sys_dim::Int`
+The following are keyword arguments:
 - `transformer_dim::Int`: the default is `transformer_dim = sys_dim`.
 - `n_blocks::Int`: The default is `1`.
 - `n_heads::Int`: the number of heads in the multihead attentio layer (default is `n_heads = sys_dim`)
 - `L::Int` the number of transformer blocks (default is `L = 2`).
 - `upscaling_activation`: by default identity
 - `resnet_activation`: by default tanh
-- `add_connection:Bool=true` (keyword argument): if the input should be added to the output.
+- `add_connection:Bool=true`: if the input should be added to the output.
 """
 struct StandardTransformerIntegrator{AT1, AT2} <: TransformerIntegrator
     sys_dim::Int 
@@ -26,7 +27,7 @@ end
 #     StandardTransformerIntegrator{typeof(upscaling_activation), typeof(resnet_activation)}(sys_dim, transformer_dim, n_heads, n_blocks, L, upscaling_activation, resnet_activation, add_connection)
 # end
 
-function StandardTransformerIntegrator(sys_dim::Int, transformer_dim::Int = sys_dim, n_heads::Int = sys_dim; n_blocks = 1, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh, add_connection::Bool = true)
+function StandardTransformerIntegrator(sys_dim::Int;transformer_dim::Int = sys_dim, n_heads::Int = sys_dim, n_blocks = 1, L::Int = 2, upscaling_activation = identity, resnet_activation = tanh, add_connection::Bool = true)
     StandardTransformerIntegrator(sys_dim, transformer_dim, n_heads, n_blocks, L, upscaling_activation, resnet_activation, add_connection)
 end
 
