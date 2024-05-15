d6b98b7b52c5d3420d90f4763bd69dbc32789fb4
benedict-96 <benedikt.brantner@icloud.com>
2024-05-15 10:54:29 +0200

    ps1 -> ps2.

--- test/linear_symplectic_attention.jl
+++ test/linear_symplectic_attention.jl
@@ -18,7 +18,7 @@ function test_application_of_lsa(n::Integer=4, seq_length::Integer=5, T=Float64)
     # test for Array as input
     arr = rand(T, 2 * n, seq_length)
     @test l₁(arr, ps₁) ≈ vcat(arr[1:n, :] + arr[(n + 1):(2 * n), :] * ps₁.A, arr[(n + 1):(2 * n), :])
-    @test l₂(arr, ps₂) ≈ vcat(arr[1:n, :], arr[(n + 1):(2 * n), :] + arr[1:n, :] * ps₁.A)
+    @test l₂(arr, ps₂) ≈ vcat(arr[1:n, :], arr[(n + 1):(2 * n), :] + arr[1:n, :] * ps₂.A)
 end
 
 test_application_of_lsa()
