27fa99106d750c87794d0a85d490197566a38bee
benedict-96 <benedikt.brantner@icloud.com>
2024-05-28 17:49:18 +0200

    Added docstrings.

--- src/optimizers/optimizer_method.jl
+++ src/optimizers/optimizer_method.jl
@@ -1,3 +1,11 @@
+@doc raw"""
+Each `Optimizer` has to be called with an `OptimizerMethod`. This specifies how the neural network weights are updated in each optimization step.
+"""
 abstract type OptimizerMethod end
 
+@doc raw"""
+    init_optimizer_cache(method, x)
+
+Initialize= the optimizer cache based on input `x` for the given `method`.
+"""
 function init_optimizer_cache(::OptimizerMethod, x) end
