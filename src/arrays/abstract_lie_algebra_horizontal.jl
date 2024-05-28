f6f6005841812528e7ef9d7d8ec29773276e0df5
benedict-96 <benedikt.brantner@icloud.com>
2024-05-28 11:16:06 +0200

    Added reference to StiefelLieAlgHorMatrix and GrassmannLieAlgHorMatrix.

--- src/arrays/abstract_lie_algebra_horizontal.jl
+++ src/arrays/abstract_lie_algebra_horizontal.jl
@@ -1,4 +1,6 @@
 @doc raw"""
 `AbstractLieAlgHorMatrix` is a supertype for various horizontal components of Lie algebras. We usually call this ``\mathfrak{g}^\mathrm{hor}``.
+
+See [`StiefelLieAlgHorMatrix`](@ref) and [`GrassmannLieAlgHorMatrix`](@ref).
 """
 abstract type AbstractLieAlgHorMatrix{T} <: AbstractMatrix{T} end
