a35fb2cd4f715e413a2d1e1ff5fb581da54f08f4
benedict-96 <benedikt.brantner@icloud.com>
2024-06-04 10:55:40 +0200

    Fixed problem with different matrices.

--- src/arrays/stiefel_projection.jl
+++ src/arrays/stiefel_projection.jl
@@ -1,7 +1,7 @@
 @doc raw"""
     StiefelProjection(backend, T, N, n)
 
-Make a matrix of the form ``\begin{bmatrix} \mathbb{I} & \mathbb{O} \end{pmatrix}^T`` for a specific backend and data type.
+Make a matrix of the form ``\begin{bmatrix} \mathbb{I} & \mathbb{O} \end{bmatrix}^T`` for a specific backend and data type.
 
 An array that essentially does `vcat(I(n), zeros(N-n, n))` with GPU support. 
 
