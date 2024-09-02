d85b38aa134c9e9c9aa46f2ab39edb009d18adf3
benedict-96 <benedikt.brantner@icloud.com>
2024-09-02 18:55:08 +0200

    Reworked docstrings.

--- src/layers/stiefel_layer.jl
+++ src/layers/stiefel_layer.jl
@@ -1,14 +1,13 @@
 # """
 # Defines a layer that performs simple multiplication with an element of the Stiefel manifold.
 # """
-struct StiefelLayer{M, N, Retraction} <: ManifoldLayer{M, N, Retraction} end
+struct StiefelLayer{M, N} <: ManifoldLayer{M, N} end
 
-default_retr = Geodesic()
-function StiefelLayer(n::Integer, N::Integer; retraction::AbstractRetraction=default_retr)
-    StiefelLayer{n, N, typeof(retraction)}()
+function StiefelLayer(n::Integer, N::Integer)
+    StiefelLayer{n, N}()
 end
 
-function AbstractNeuralNetworks.initialparameters(d::StiefelLayer{M,N}, backend::KernelAbstractions.Backend, ::Type{T}; rng::AbstractRNG=Random.default_rng()) where {M,N,T}
+function AbstractNeuralNetworks.initialparameters(::StiefelLayer{M,N}, backend::KernelAbstractions.Backend, ::Type{T}; rng::AbstractRNG=Random.default_rng()) where {M,N,T}
     (weight = N > M ? rand(backend, rng, StiefelManifold{T}, N, M) : rand(backend, rng, StiefelManifold{T}, M, N), )
 end
 
