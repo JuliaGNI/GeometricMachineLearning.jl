7660ca72dc4ab5dd04e083dda180df65eaa7c69f
benedict-96 <benedikt.brantner@icloud.com>
2024-12-06 10:15:51 +0100

    Fixed typo.

--- test/hamiltonian_neural_network_tests.jl
+++ test/hamiltonian_neural_network_tests.jl
@@ -3,16 +3,36 @@ using Test
 import Random 
 Random.seed!(1234)
 
+const GMLA = GeometricMachineLearning.Activation
+function allocate_network_and_data_loader(dim::Integer, width::Integer, nhidden::Integer, activation::GMLA)
+    arch = StandardHamiltonianArchitecture(dim, width, nhidden, activation)
+    loss = HNNLoss(arch)
+    input = rand(dim, 10)
+    output = rand(dim, 10)
+    dl = DataLoader(input, output)
+    NeuralNetwork(arch), loss, dl
+end
+
+"""
+This tests if we can call the loss
+"""
 function test_hnn_loss( dim::Integer = 2, 
                         width::Integer = dim, 
                         nhidden::Integer = 1, 
                         activation::GeometricMachineLearning.Activation = GeometricMachineLearning.SigmoidActivation())
-    arch = StandardHamiltonianArchitecture(dim, width, nhidden, activation)
-    loss = HNNLoss(arch)
-    ps = NeuralNetwork(arch).params
-    input = rand(dim, 10)
-    output = rand(dim, 10)
-    @test typeof(loss(ps), input, output) <: Real
+    nn, loss, dl = allocate_network_and_data_loader(dim, width, nhidden, activation)
+    @test typeof(loss(nn.params, dl.input, dl.output)) <: Real
+    @test typeof(loss) <: NetworkLoss
 end
 
 test_hnn_loss()
+
+"""
+This tests if we can differentiate the loss
+"""
+function test_hnn_loss_derivative(  dim::Integer = 2,
+                                    width::Integer = dim,
+                                    nhidden::Integer = 1,
+                                    activation::GeometricMachineLearning.Activation = GeometricMachineLearning.SigmoidActivation())
+    nn, loss, dl = allocate_network_and_data_loader(dim, width, nhidden, activation)
+end
