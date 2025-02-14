99fb92fe1976a0db84bc3c8da4708f2a50a2675a
benedict-96 <benedikt.brantner@icloud.com>
2025-02-14 10:39:57 +0100

    Added/modified script for double pendulum.

--- scripts/symplectic_transformer/double_pendulum.jl
+++ scripts/symplectic_transformer/double_pendulum.jl
@@ -1,60 +1,150 @@
-using GeometricMachineLearning: DataLoader, LinearSymplecticTransformer, NeuralNetwork, CPU, Batch, AdamOptimizer, Optimizer, transformer_loss, GSympNet
+using GeometricMachineLearning
+using GeometricMachineLearning: MatrixSoftmax, VectorSoftmax
 using GeometricProblems.DoublePendulum: tspan, tstep, default_parameters, hodeproblem
-using GeometricIntegrators: integrate, ImplicitMidpoint
 using GeometricEquations: EnsembleProblem
-using CUDA
+using GeometricIntegrators: ImplicitMidpoint, integrate
+using LaTeXStrings
+using CairoMakie
+CairoMakie.activate!()
+import Random
+Random.seed!(123)
 
+morange = RGBf(255 / 256, 127 / 256, 14 / 256)
+mred = RGBf(214 / 256, 39 / 256, 40 / 256)
+mpurple = RGBf(148 / 256, 103 / 256, 189 / 256)
+mblue = RGBf(31 / 256, 119 / 256, 180 / 256)
+mgreen = RGBf(44 / 256, 160 / 256, 44 / 256)
+
+# ensemble problem
 initial_conditions = [
-    (q = [π / i, π / j], p = [0.0, π / k]) for i=1:10, j=1:10, k=1:5
+    (q = [π / i, π / j], p = [0.0, π / k]) for i=1:1, j=1:1, k=1:1
 ]
 initial_conditions = reshape(initial_conditions, length(initial_conditions))
 
-ensemble_problem = EnsembleProblem(hodeproblem().equation, (tspan[1], 20*tspan[2]), tstep, initial_conditions, default_parameters)
+ensemble_problem = EnsembleProblem(hodeproblem().equation, (tspan[1], tspan[2]), tstep, initial_conditions, default_parameters)
 
 ensemble_solution = integrate(ensemble_problem, ImplicitMidpoint())
 
 dl = DataLoader(ensemble_solution)
 
-const seq_length = 5
-const nhidden = 4
-const depth = 1 
-const backend = CUDABackend()
-const T = backend == CUDABackend() ? Float32 : Float64 
-const batch_size = 1000
-const n_epochs = 100
-const opt_method = AdamOptimizer(T)
 
-dl = backend == CUDABackend() ? DataLoader(dl.input |> cu) : dl
+const seq_length = 2
+const transformer_dim = 4
 
-function train_linear_symplectic_transformer()
+act = MatrixSoftmax()
 
-    arch = LinearSymplecticTransformer(dl, nhidden = nhidden, depth = depth, seq_length = seq_length)
+arch1 = StandardTransformerIntegrator(dl.input_dim; transformer_dim = transformer_dim,
+                                                    n_heads = 1, 
+                                                    L = 1, 
+                                                    n_blocks = 4,
+                                                    attention_activation = act)
 
-    nn = NeuralNetwork(arch, backend, T)
+arch2 = SymplecticTransformer(dl.input_dim; transformer_dim = transformer_dim,
+                                            L = 1,
+                                            n_sympnet = 6,
+                                            attention_activation = act,
+                                            symmetric = true)
 
-    opt = Optimizer(opt_method, nn)
+arch3 = SymplecticTransformer(dl.input_dim; transformer_dim = transformer_dim,
+                                            L =1,
+                                            n_sympnet = 6,
+                                            attention_activation = act,
+                                            symmetric = false)
 
-    batch = Batch(batch_size, seq_length)
 
-    loss_array = opt(nn, dl, batch, n_epochs, transformer_loss)
+arch4 = GSympNet(dl.input_dim; n_layers = 6)
 
-    loss_array, nn
-end 
+nn1 = NeuralNetwork(arch1)
+nn2 = NeuralNetwork(arch2)
+nn3 = NeuralNetwork(arch3)
+nn4 = NeuralNetwork(arch4)
 
-function train_sympnet()
+const batch_size = 1024
+const n_epochs = 3000
 
-    arch = GSympNet(dl, nhidden = nhidden * depth)
+o_method = AdamOptimizer()
 
-    nn = NeuralNetwork(arch, backend, T)
+o1 = Optimizer(o_method, nn1)
+o2 = Optimizer(o_method, nn2)
+o3 = Optimizer(o_method, nn3)
+o4 = Optimizer(o_method, nn4)
 
-    opt = Optimizer(opt_method, nn)
+batch = Batch(batch_size, seq_length)
 
-    batch = Batch(batch_size, 1)
+loss_array1 = o1(nn1, dl, batch, n_epochs)
+loss_array2 = o2(nn2, dl, batch, n_epochs)
+loss_array3 = o3(nn3, dl, batch, n_epochs)
+loss_array4 = o4(nn4, dl, Batch(batch_size), n_epochs)
 
-    loss_array = opt(nn, dl, batch, n_epochs)
+function make_training_error_plot(; theme = :dark, symplectic = true)
+    textcolor = theme == :dark ? :white : :black
+    fig = Figure(; backgroundcolor = :transparent)
+    ax = Axis(fig[1, 1]; 
+        backgroundcolor = :transparent,
+        bottomspinecolor = textcolor, 
+        topspinecolor = textcolor,
+        leftspinecolor = textcolor,
+        rightspinecolor = textcolor,
+        xtickcolor = textcolor, 
+        ytickcolor = textcolor,
+        xticklabelcolor = textcolor,
+        yticklabelcolor = textcolor,
+        xlabel=L"t", 
+        ylabel=L"q_1",
+        xlabelcolor = textcolor,
+        ylabelcolor = textcolor,
+    )
 
-    loss_array, nn
+    # we use linewidth  = 2
+    lines!(ax, loss_array1; color = mpurple, label = "Transformer", linewidth = 2)
+    symplectic ? lines!(ax, loss_array2; color = mred, label = "SymplecticTransformerA", linewidth = 2) : nothing
+    symplectic ? lines!(ax, loss_array3; color = mgreen, label = "SymplecticTransformerA", linewidth = 2) : nothing
+    axislegend(; position = (.55, .75), backgroundcolor = :transparent, labelcolor = textcolor)
+
+    fig, ax
 end
 
-sympnet_loss_array, sympnet = train_sympnet()
-transformer_loss_array, transformer = train_linear_symplectic_transformer()
+training_fig_light, training_ax_light = make_training_error_plot(; theme = :light)
+training_fig_dark, training_ax_dark = make_training_error_plot(; theme = :dark)
+
+const index = 1
+init_con = (q = dl.input.q[:, 1:seq_length, index], p = dl.input.p[:, 1:seq_length, index])
+
+const n_steps = 300
+
+function make_validation_plot(n_steps = n_steps; theme = :dark, symplectic = true)
+    textcolor = theme == :dark ? :white : :black
+    fig = Figure(; backgroundcolor = :transparent)
+    ax = Axis(fig[1, 1]; 
+        backgroundcolor = :transparent,
+        bottomspinecolor = textcolor, 
+        topspinecolor = textcolor,
+        leftspinecolor = textcolor,
+        rightspinecolor = textcolor,
+        xtickcolor = textcolor, 
+        ytickcolor = textcolor,
+        xticklabelcolor = textcolor,
+        yticklabelcolor = textcolor,
+        xlabel=L"t", 
+        ylabel=L"q_1",
+        xlabelcolor = textcolor,
+        ylabelcolor = textcolor,
+    )
+    prediction1 = iterate(nn1, init_con; n_points = n_steps, prediction_window = seq_length)
+    prediction2 = iterate(nn2, init_con; n_points = n_steps, prediction_window = seq_length)
+    prediction3 = iterate(nn3, init_con; n_points = n_steps, prediction_window = seq_length)
+
+    # we use linewidth  = 2
+    lines!(ax, n_steps-(50):n_steps, dl.input.q[1, n_steps-(50):n_steps, index]; color = mblue, label = "Implicit midpoint", linewidth = 2)
+    lines!(ax, n_steps-(50):n_steps, prediction1.q[1, n_steps-(50):n_steps]; color = mpurple, label = "Transformer", linewidth = 2)
+    symplectic ? lines!(ax, n_steps-(50):n_steps, prediction2.q[1, n_steps-(50):n_steps]; color = mred, label = "SymplecticTransformerS", linewidth = 2) : false
+    symplectic ? lines!(ax, n_steps-(50):n_steps, prediction3.q[1, n_steps-(50):n_steps]; color = mgreen, label = "SymplecticTransformerA", linewidth = 2) : false
+    axislegend(; position = (.55, .75), backgroundcolor = :transparent, labelcolor = textcolor)
+
+    fig, ax
+end
+
+fig_light, ax_light = make_validation_plot(n_steps; theme = :light)
+fig_dark, ax_dark = make_validation_plot(n_steps; theme = :dark)
+
+save("Matrix-Softmax-Validation.png", fig_light)
