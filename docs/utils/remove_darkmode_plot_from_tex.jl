52910510cfc4b60bd7fc631948b072bfb89ce2c4
benedict-96 <benedikt.brantner@icloud.com>
2025-02-17 23:35:27 +0100

    Made script work (up to a point).

--- docs/utils/remove_darkmode_plot_from_tex.jl
+++ docs/utils/remove_darkmode_plot_from_tex.jl
@@ -1,15 +1,25 @@
 file = open("build/GeometricMachineLearning.jl.tex")
 
-collection_of_lines = (line for line in eachline(file))
+collection_of_lines = Tuple(line for line in eachline(file))
+close(file)
 
 new_contents = ""
 
 for (i, line) in zip(1:length(collection_of_lines), collection_of_lines)
     if line == raw" \begin{figure}"
-        new_contents *= raw"\begin{comment}"
-    elseif line == raw"\end{figure}" || collection_of_lines[i + 1] == ""
-        new_contents *= raw"\end{comment}"
+        global new_contents *= raw"\begin{comment}"
+    elseif line == raw"\end{figure}"
+            if collection_of_lines[i + 1] == ""
+                global new_contents *= raw"\end{figure}"
+            else
+                global new_contents *= raw"\end{comment}"
+            end
     else
-        new_contents *= line
+        global new_contents *= line
     end
+    global new_contents *= "\n"
 end
+
+new_file = open("build/GeometricMachineLearning.jl-copy.tex", "a")
+write(new_file, new_contents)
+close(new_file)
